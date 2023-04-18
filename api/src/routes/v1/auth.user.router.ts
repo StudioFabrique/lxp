3b7d13a1585437318cd67bd97a04ba436e8e1c6e
CyrilPonsan <cyril.ponsan@gmail.com>
2023-04-18 10:35:15 +0200

    création du endpoint logout valable pour tous les utilisateurs +  tests

--- api/src/routes/v1/auth.user.router.ts
+++ api/src/routes/v1/auth.user.router.ts
@@ -2,9 +2,11 @@ import express from "express";
 import { body } from "express-validator";
 
 import httpUserLogin from "../../controllers/auth/user-login";
+import httpLogout from "../../controllers/auth/httpLogout";
 
 const userAuth = express.Router();
 
 userAuth.post("/login", body("email").trim().escape().isEmail(), httpUserLogin);
+userAuth.get("/logout", httpLogout);
 
 export default userAuth;
