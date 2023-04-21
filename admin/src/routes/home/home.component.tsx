31a9eb46f9274ae48cf0665900c736d39fa7792f
CyrilPonsan <cyril.ponsan@gmail.com>
2023-04-21 11:15:00 +0200

    écriture d'un test end to end pour login

--- admin/src/routes/home/home.component.tsx
+++ admin/src/routes/home/home.component.tsx
@@ -5,7 +5,7 @@ const Home = () => {
   const { logout } = useContext(Context);
 
   return (
-    <div className="bg-red h-screen w-screen">
+    <div className="home bg-red h-screen w-screen">
       Hey je suis la page Home <br />
       <button onClick={logout}>logout</button>
     </div>
