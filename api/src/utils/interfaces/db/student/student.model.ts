021509decda53397528a4af5610c8b43ea40aa6b
CyrilPonsan <cyril.ponsan@gmail.com>
2023-05-02 13:43:55 +0200

    modifications des modeles, fixtures users

--- api/src/utils/interfaces/db/student/student.model.ts
+++ api/src/utils/interfaces/db/student/student.model.ts
@@ -3,8 +3,11 @@ import mongoose, { Schema, Document } from "mongoose";
 export interface IStudent extends Document {
   email: string;
   password: string;
-  //lastname: string;
-  //firstname: string;
+  firstname: string;
+  lastname: string;
+  address: string;
+  postCode: string;
+  city: string;
   roles: Array<string>;
   avatar?: string;
   createdAt: Date;
@@ -15,8 +18,11 @@ const studentSchema: Schema = new Schema(
   {
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    //lastname: { type: String, required: true },
-    //firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    firstname: { type: String, required: true },
+    address: { type: String, required: true },
+    postCode: { type: String, required: true },
+    city: { type: String, required: true },
     roles: { type: [String], required: true },
     avatar: { type: String, required: false },
   },
--- api/src/utils/interfaces/db/teacher-admin/teacher.model.ts
+++ api/src/utils/interfaces/db/teacher-admin/teacher.model.ts
@@ -3,6 +3,11 @@ import mongoose, { Schema, Document } from "mongoose";
 export interface IUser extends Document {
   email: string;
   password: string;
+  firstname: string;
+  lastname: string;
+  address: string;
+  postCode: string;
+  city: string;
   roles: Array<string>;
   avatar?: string;
   createdAt: Date;
@@ -13,6 +18,11 @@ const userSchema: Schema = new Schema(
   {
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    lastname: { type: String, required: true },
+    firstname: { type: String, required: true },
+    address: { type: String, required: true },
+    postCode: { type: String, required: true },
+    city: { type: String, required: true },
     roles: { type: [String], required: true },
     avatar: { type: String, required: false },
   },
