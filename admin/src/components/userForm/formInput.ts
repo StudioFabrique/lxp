6b2b504f12ed2dd4dab7e5828fb970dee50e8d08
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2023-05-03 11:43:03 +0200

    ajout de la partie visuelle + un peu de logique de la partie formulaire 'création utilisateur' + préparation logique code pour un formulaire 'modification utilisateur'

---
+++ admin/src/components/userForm/formInput.ts
@@ -0,0 +1,11 @@
+const formInput = [
+  { label: "Email", type: "email" },
+  { label: "Mot de passe", type: "password" },
+  { label: "Prénom", type: "text" },
+  { label: "Nom", type: "text" },
+  { label: "Adresse", type: "text" },
+  { label: "Code Postal", type: "text" },
+  { label: "Ville", type: "text" },
+  { label: "createdAt", type: "text" },
+];
+export default formInput;
