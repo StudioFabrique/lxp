eeceb304e80dfcda53f110139ad161b0f8d6c2c5
CyrilPonsan <cyril.ponsan@gmail.com>
2023-05-04 10:55:19 +0200

    implémentation de la fonction de tri du tableau

--- admin/src/routes/home/home.component.tsx
+++ admin/src/routes/home/home.component.tsx
@@ -7,79 +7,12 @@ import Pagination from "../../components/UI/pagination/pagination";
 
 const Home = () => {
   const { logout } = useContext(Context);
-  const [userList, setUserList] = useState<Array<any>>([]);
-  const [role, setRole] = useState("admin");
-  const { isLoading, error, sendRequest } = useHttp();
-  const { page, perPage, totalPages, setTotalPages, initPagination, setPage } =
-    usePagination();
-
-  useEffect(() => {
-    const applyData = (data: any) => {
-      let index = (page - 1) * perPage + 1;
-      data.users.forEach((item: any) => {
-        item.index = index++ + ".";
-        item.createdAt =
-          item?.createdAt && new Date(item.createdAt).toLocaleDateString();
-        item.updatedAt =
-          item?.updatedAt && new Date(item.updatedAt).toLocaleDateString();
-      });
-      setTotalPages(data.total);
-      setUserList(data.users);
-    };
-    sendRequest(
-      {
-        path: `/user/${role}?page=${page}&limit=${perPage}`,
-      },
-      applyData
-    );
-  }, [sendRequest, page, perPage, setTotalPages, role]);
-
-  const handleTabClick = (newRole: string) => {
-    initPagination();
-    setRole(newRole);
-  };
 
   return (
     <div className="home bg-red h-screen w-screen flex flex-col items-center">
       <p>Hey je suis la page Home</p>
-      <button onClick={logout}>logout</button>
-      <div className="mt-8">
-        <div className="tabs">
-          <p
-            className={
-              role === "admin" ? "tab tab-lifted tab-active" : "tab tab-lifted"
-            }
-            onClick={() => handleTabClick("admin")}
-          >
-            Admins
-          </p>
-          <p
-            className={
-              role === "teacher"
-                ? "tab tab-lifted tab-active"
-                : "tab tab-lifted"
-            }
-            onClick={() => handleTabClick("teacher")}
-          >
-            Formateurs
-          </p>
-          <p
-            className={
-              role === "student"
-                ? "tab tab-lifted tab-active"
-                : "tab tab-lifted"
-            }
-            onClick={() => handleTabClick("student")}
-          >
-            Apprenants
-          </p>
-        </div>
-        {userList ? <UserList userList={userList} /> : null}
-        <Pagination page={page} totalPages={totalPages} setPage={setPage} />
-        <div className="w-full mt-8 flex justify-center">
-          <p>{error}</p>
-        </div>
-      </div>
+      <button onClick={logout}>logout</button>      
+       <UserList  />
     </div>
   );
 };
--- api/src/models/user/admin-teacher/getAllUsers.ts
+++ api/src/models/user/admin-teacher/getAllUsers.ts
@@ -2,17 +2,25 @@ import Student from "../../../utils/interfaces/db/student/student.model";
 import User from "../../../utils/interfaces/db/teacher-admin/teacher.model";
 import { getPagination } from "../../../utils/services/getPagination";
 
-async function getAllUsers(page: number, limit: number, role: string) {
-  console.log({ limit });
+async function getAllUsers(
+  page: number,
+  limit: number,
+  role: string,
+  stype: string,
+  sdir: string
+) {
+  const dir = sdir === "asc" ? 1 : -1;
 
   if (role === "admin" || role === "teacher") {
     const users = await User.find({ roles: role })
+      .sort({ [stype]: dir })
       .skip(getPagination(page, limit))
       .limit(limit);
     const total = await User.count({ roles: role });
     return { total, users };
   } else if (role === "student") {
     const users = await Student.find({ roles: role })
+      .sort({ [stype]: dir })
       .skip(getPagination(page, limit))
       .limit(limit);
     const total = await Student.count();
