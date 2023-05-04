2b731c2624d613f48b8bd97710c77f16d4c5b867
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2023-05-04 13:26:50 +0200

    modification constantes + modification json response

--- api/src/controllers/userTest/make-user.ts
+++ api/src/controllers/userTest/make-user.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from "express";
 import { validationResult } from "express-validator";
 import { IUser } from "../../utils/interfaces/db/teacher-admin/teacher.model";
-import { alreadyExist, badQuery } from "../../utils/constantes";
+import {
+  alreadyExist,
+  badQuery,
+  creationSuccessfull,
+} from "../../utils/constantes";
 import make from "../../models/userTest/make";
 
 export default async function MakeUser(req: Request, res: Response) {
@@ -15,7 +19,7 @@ export default async function MakeUser(req: Request, res: Response) {
 
   make(user).then((response) =>
     response != null
-      ? res.status(201).json(user) /* or .send(SuccessConstante) */
-      : res.status(409).send(alreadyExist)
+      ? res.status(201).json({ message: creationSuccessfull })
+      : res.status(409).json({ message: alreadyExist })
   );
 }
