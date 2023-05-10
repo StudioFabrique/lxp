a9f60238b141b8abcb7f749d010700cd34c44745
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2023-05-10 13:47:12 +0200

    ajout de la redirection

--- front/src/views/user/user-add.component.tsx
+++ front/src/views/user/user-add.component.tsx
@@ -1,16 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import UserAddForm from "../../components/UserForm/add/user-add-form.component";
 import useHttp from "../../hooks/use-http";
 import User from "../../utils/interfaces/user";
 
 const UserAdd = () => {
   const { error, isLoading, sendRequest } = useHttp();
+  const navigate = useNavigate();
 
   const handleSubmit = (user: User) => {
     console.log("event emit (form)");
-
     sendRequest(
       { method: "post", path: "/userTest", body: user },
-      (data: any) => console.log(data)
+      (data: any) => {
+        if (data) return navigate(-1);
+      }
     );
   };
 
