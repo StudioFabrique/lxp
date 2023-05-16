a7c1b495edce2e8985e59ebbece00c77cf3faf28
CyrilPonsan <cyril.ponsan@gmail.com>
2023-05-16 09:40:55 +0200

    mise à jour des users (non apprenants)

--- api/src/controllers/auth/http-get-roles.ts
+++ api/src/controllers/auth/http-get-roles.ts
@@ -3,8 +3,6 @@ import { serverIssue } from "../../utils/constantes";
 import getRoles from "../../models/auth/get-roles";
 
 async function httpGetRoles(req: Request, res: Response) {
-  console.log("fetching roles");
-
   try {
     const roles = await getRoles();
     return res.status(200).json(roles);
