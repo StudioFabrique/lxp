9ea031d2cd86eaaa9607b5b85faff25e2b58ae23
CyrilPonsan <cyril.ponsan@gmail.com>
2023-05-19 14:27:52 +0200

    correction tri des résultats de la recherche

--- api/src/controllers/user/http-search-user.ts
+++ api/src/controllers/user/http-search-user.ts
@@ -3,13 +3,21 @@ import searchUser from "../../models/user/search-user";
 import { badQuery, serverIssue } from "../../utils/constantes";
 
 async function httpSearchUser(req: Request, res: Response) {
-  const { entity, role, value, sdir } = req.params;
+  const { entity, role, value, stype, sdir } = req.params;
   const { page, limit } = req.query;
 
   console.log({ role });
 
   try {
-    const result = await searchUser(entity, value, role, +page!, +limit!, sdir);
+    const result = await searchUser(
+      entity,
+      value,
+      role,
+      +page!,
+      +limit!,
+      stype,
+      sdir
+    );
 
     if (!result) {
       return res.status(400).json({ message: badQuery });
