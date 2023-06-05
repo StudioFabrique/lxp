d6d9037ea35f3ce57c5be42b3a50557d96294cbd
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2023-06-05 16:27:55 +0200

    préaparation ajout composant

--- front/src/components/lists/group-add-user-list/user-group-list.component.tsx
+++ front/src/components/lists/group-add-user-list/user-group-list.component.tsx
@@ -1,16 +1,21 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import User from "../../../utils/interfaces/user";
 
 const GroupUserList: FC<{
   users: User[];
   AddSelectedUser: (userId: number) => void;
 }> = (props) => {
+  /* const [selectButton, setSelectButton] = useState<React.ReactElement>(
+    <button onClick={}></button>
+  ); */
+
   return (
+    // map dans un autre composant
     <>
       {props.users.map((user, i) => (
         <div key={i} className="flex flex-row justify-between">
           <div>{user.firstname}</div>
-          <div></div>
+          <div>{/* {selectButton} */}</div>
         </div>
       ))}
     </>
