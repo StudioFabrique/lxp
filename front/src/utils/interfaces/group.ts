d9a864b5efa48e7cc80b02b427d8a9dcf10794de
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2023-06-05 11:06:01 +0200

    correction bugs ajout groupe api

--- front/src/utils/interfaces/group.ts
+++ front/src/utils/interfaces/group.ts
@@ -5,8 +5,8 @@ import User from "./user";
 export default interface Group extends Document {
   name: string;
   desc: string;
-  teachers?: User["_id"];
-  users?: Student["_id"];
+  users?: User["_id"];
+  students?: Student["_id"];
   role: Role["_id"];
   createdAt: Date;
   updatedAt: Date;
