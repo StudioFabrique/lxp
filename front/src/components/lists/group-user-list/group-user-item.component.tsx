a591cf261966b60c7f33f3d6b827f96e7ffc193c
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2023-06-07 15:05:03 +0200

    finalisation tableau

--- front/src/components/lists/group-user-list/group-user-item.component.tsx
+++ front/src/components/lists/group-user-list/group-user-item.component.tsx
@@ -4,8 +4,19 @@ import User from "../../../utils/interfaces/user";
 const GroupUserItem: FC<{ user: User }> = ({ user }) => {
   return (
     <tr className="">
-      <td className="bg-slate-200"></td>
-      <td className="bg-slate-200"></td>
+      <td className="bg-slate-200">
+        <input type="checkbox" className="checkbox checkbox-primary" />
+      </td>
+      <td className="bg-slate-200">
+        <div className="avatar">
+          <div className="w-10 rounded-full">
+            <img
+              src="https://www.leparisien.fr/resizer/fGXimQvLycC2XjTOb9nran3rDcU=/1248x782/filters:focal(1184x745:1194x755)/cloudfront-eu-central-1.images.arcpublishing.com/leparisien/NJE5TPKX7NDY3AL7MWIJJEXZOA.jpg"
+              alt="image de chat"
+            />
+          </div>
+        </div>
+      </td>
       <td className="bg-slate-200">{user.firstname}</td>
       <td className="bg-slate-200">{user.lastname}</td>
       <td className="bg-slate-200">{user.email}</td>
