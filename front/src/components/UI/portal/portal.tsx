5e375c6eb13ec6c470f5e81b1595e1f5b3c73c7b
CyrilPonsan <cyril.ponsan@gmail.com>
2023-06-08 16:08:57 +0200

    correction petit bug lié au portal

--- front/src/components/UI/portal/portal.tsx
+++ front/src/components/UI/portal/portal.tsx
@@ -15,12 +15,7 @@ const Portal = (props: PortalProps) => {
   }, []);
 
   return mounted && ref.current
-    ? createPortal(
-        <div className="fixed z-10 top-0 left-0 w-screen h-screen">
-          {props.children}
-        </div>,
-        ref.current
-      )
+    ? createPortal(<div>{props.children}</div>, ref.current)
     : null;
 };
 
