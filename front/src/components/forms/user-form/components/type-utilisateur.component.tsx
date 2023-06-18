43588b647ab4400dda9b9ef16f30e31c4609c405
martin-dhlle <mdhollande@fabriquenumerique.fr>
2023-06-18 11:35:07 +0200

    last adds

--- front/src/components/forms/user-form/components/type-utilisateur.component.tsx
+++ front/src/components/forms/user-form/components/type-utilisateur.component.tsx
@@ -4,14 +4,40 @@ const TypeUtilisateur = () => {
   return (
     <Wrapper>
       <h2 className="font-bold text-xl">Type d'utilisateur</h2>
-      <span>
-        <select>
-          <option value="">Etudiant</option>
-          <option value="">Formateur</option>
-          <option value="">Administrateur</option>
-          <option value="">Visiteur</option>
-        </select>
-      </span>
+      <div className="flex flex-col">
+        <span>
+          <input
+            name="etudiant"
+            type="checkbox"
+            className="checkbox checkbox-primary"
+          />
+          <label htmlFor="etudiant">Etudiant</label>
+        </span>
+        <span>
+          <input
+            name="formateur"
+            type="checkbox"
+            className="checkbox checkbox-primary"
+          />
+          <label htmlFor="formateur">Formateur</label>
+        </span>
+        <span>
+          <input
+            name="administrateur"
+            type="checkbox"
+            className="checkbox checkbox-primary"
+          />
+          <label htmlFor="administrateur">Administrateur</label>
+        </span>
+        <span>
+          <input
+            name="visiteur"
+            type="checkbox"
+            className="checkbox checkbox-primary"
+          />
+          <label htmlFor="visiteur">Visiteur</label>
+        </span>
+      </div>
     </Wrapper>
   );
 };
