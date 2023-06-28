f2b06a6e1b75bdb61949113640d8177da590be9c
CyrilPonsan <cyril.ponsan@gmail.com>
2023-06-28 16:29:23 +0200

    compétences

---
+++ front/src/components/skills/skills.component.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SkillsHeader from "./skills-header";
+import SkillsList from "./skills-list";
+
+const Skills = () => {
+  return (
+    <div className="w-full">
+      <SkillsHeader />
+      <SkillsList />
+    </div>
+  );
+};
+
+export default Skills;
