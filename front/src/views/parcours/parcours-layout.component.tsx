8fe1454aaf3eacdf3650e41b88a41e9786a593a5
CyrilPonsan <cyril.ponsan@gmail.com>
2023-06-28 10:53:00 +0200

    creer un parcours limité aux admins

--- front/src/views/parcours/parcours-layout.component.tsx
+++ front/src/views/parcours/parcours-layout.component.tsx
@@ -1,9 +1,25 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, Outlet } from "react-router-dom";
+import { casbinAuthorizer } from "../../config/rbac";
+import Can from "../../components/UI/can/can.component";
 
 const ParcoursLayout = () => {
+  const [hasAccess, setHasAccess] = useState(false);
+
+  useEffect(() => {
+    (async function () {
+      const result = await casbinAuthorizer.can("write", "parcours");
+      setHasAccess(result);
+    })();
+  }, []);
+
   return (
     <div>
+      {hasAccess ? (
+        <Link to="/parcours/créer-un-parcours">Créer un parcours</Link>
+      ) : (
+        <p className="text-base-content/50">Créer un parcours</p>
+      )}
       <Outlet />
     </div>
   );
