efb445f46e93b10a992ff181f6d2a1aea3859d46
CyrilPonsan <cyril.ponsan@gmail.com>
2023-07-24 13:16:50 +0200

    modele bdd sql avec prisma

--- api/src/controllers/parcours/parcours.spec.ts
+++ api/src/controllers/parcours/parcours.spec.ts
@@ -1,17 +1,45 @@
 import request from "supertest";
 import app from "../../app";
 import dotenv from "dotenv";
-dotenv.config();
 import mongoConnect from "../../utils/services/db/mongo-connect";
+import { PrismaClient } from "@prisma/client";
+import { blobParcours } from "../../utils/tests/blob-parcours";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const prisma = new PrismaClient();
+
+// Méthode pour fermer la connexion
+const disconnect = async () => {
+  if (mongoose.connection.readyState !== 0) {
+    await mongoose.connection.close();
+  }
+};
+
+let tags: any;
+let contacts: any;
+const image = blobParcours;
+
+async function getTags() {
+  return await prisma.tag.findMany();
+}
+
+async function getContacts() {
+  return await prisma.teacher.findMany();
+}
 
 describe("HTTP Handshake", () => {
   let authToken = {}; // Store the authentication token
 
   beforeAll(async () => {
     await mongoConnect();
+    tags = (await getTags()).map((item) => item.id);
+    contacts = (await getContacts()).map((item: any) => item.id);
+
     // Perform any setup before running the tests, such as logging in and obtaining the authentication token
     const loginResponse = await request(app)
-      .post("/v1/auth/login/user")
+      .post("/v1/auth/login")
       .send({ email: "toto@toto.fr", password: "Abcdef@123456" });
 
     authToken = loginResponse.headers["set-cookie"][0];
@@ -22,12 +50,184 @@ describe("HTTP Handshake", () => {
     // ...
   });
 
-  describe("Test GET /parcours/", () => {
+  describe("Test GET /parcours/contacts", () => {
     test("It should respond with 200 success", async () => {
       await request(app)
         .get("/v1/parcours/contacts")
         .set("Cookie", [`${authToken}`])
         .expect(200);
     });
+  });
+
+  /**
+   * test POST-CREATE-PARCOURS
+   */
+
+  //  jeu de données valides
+  describe("Test POST /parcours", () => {
+    test("It should respond with 201 success", async () => {
+      await request(app)
+        .post("/v1/parcours")
+        .set("Cookie", [`${authToken}`])
+        .send({
+          id: 1,
+          title: "testToto",
+          description: "testDescription",
+          degree: "testDegree",
+          startDate: new Date(),
+          endDate: new Date(),
+          tags,
+          contacts: [1],
+          image,
+        })
+        .expect(201);
+    });
+  });
+
+  // jeux de données non valides
+
+  describe("Test POST /parcours", () => {
+    test("It should respond with 400 failure", async () => {
+      await request(app)
+        .post("/v1/parcours")
+        .set("Cookie", [`${authToken}`])
+        .send({
+          id: 1,
+          title: 0,
+          description: "testDescription",
+          degree: "testDegree",
+          startDate: new Date(),
+          endDate: new Date(),
+          tags,
+          contacts: [1],
+          image,
+        })
+        .expect(400);
+    });
+  });
+
+  describe("Test POST /parcours", () => {
+    test("It should respond with 400 failure", async () => {
+      await request(app)
+        .post("/v1/parcours")
+        .set("Cookie", [`${authToken}`])
+        .send({
+          id: 1,
+          title: "totoTest",
+          description: 0,
+          degree: "testDegree",
+          startDate: new Date(),
+          endDate: new Date(),
+          tags,
+          contacts: [1],
+          image,
+        })
+        .expect(400);
+    });
+  });
+
+  describe("Test POST /parcours", () => {
+    test("It should respond with 400 failure", async () => {
+      await request(app)
+        .post("/v1/parcours")
+        .set("Cookie", [`${authToken}`])
+        .send({
+          id: 1,
+          title: "totoTest",
+          description: "testDescription",
+          degree: 0,
+          startDate: new Date(),
+          endDate: new Date(),
+          tags,
+          contacts: [1],
+          image,
+        })
+        .expect(400);
+    });
+  });
+
+  describe("Test POST /parcours", () => {
+    test("It should respond with 400 failure", async () => {
+      await request(app)
+        .post("/v1/parcours")
+        .set("Cookie", [`${authToken}`])
+        .send({
+          id: 1,
+          title: "totoTest",
+          description: "testDescription",
+          degree: "testDegree",
+          startDate: 0,
+          endDate: new Date(),
+          tags,
+          contacts: [1],
+          image,
+        })
+        .expect(400);
+    });
+  });
+
+  describe("Test POST /parcours", () => {
+    test("It should respond with 400 failure", async () => {
+      await request(app)
+        .post("/v1/parcours")
+        .set("Cookie", [`${authToken}`])
+        .send({
+          id: 1,
+          title: "totoTest",
+          description: "testDescription",
+          degree: "testDegree",
+          startDate: new Date(),
+          endDate: 0,
+          tags: [0],
+          contacts: [1],
+          image,
+        })
+        .expect(400);
+    });
+  });
+
+  describe("Test POST /parcours", () => {
+    test("It should respond with 400 failure", async () => {
+      await request(app)
+        .post("/v1/parcours")
+        .set("Cookie", [`${authToken}`])
+        .send({
+          id: 1,
+          title: "totoTest",
+          description: "testDescription",
+          degree: "testDegree",
+          startDate: new Date(),
+          endDate: 0,
+          tags,
+          contacts: [0],
+          image,
+        })
+        .expect(400);
+    });
+  });
+
+  describe("Test POST /parcours", () => {
+    test("It should respond with 400 failure", async () => {
+      await request(app)
+        .post("/v1/parcours")
+        .set("Cookie", [`${authToken}`])
+        .send({
+          id: 1,
+          title: "totoTest",
+          description: "testDescription",
+          degree: "testDegree",
+          startDate: new Date(),
+          endDate: 0,
+          tags,
+          contacts: [1],
+          image: 0,
+        })
+        .expect(400);
+    });
+  });
+
+  afterAll(async () => {
+    // Fermer la connexion à MongoDB
+    await disconnect();
   });
 });
--- api/src/models/parcours/get-contacts.ts
+++ api/src/models/parcours/get-contacts.ts
@@ -4,14 +4,10 @@ import User from "../../utils/interfaces/db/user.model";
 async function getContacts() {
   const teacherRoles = await Role.find({ rank: 2 }, { _id: 1 });
 
-  console.log(teacherRoles);
-
   const contacts = await User.find(
     { roles: { $in: teacherRoles } },
     { _id: 1, firstname: 1, lastname: 1, email: 1, roles: 1 }
   ).populate("roles");
-
-  console.log(contacts);
 
   return contacts;
 }
