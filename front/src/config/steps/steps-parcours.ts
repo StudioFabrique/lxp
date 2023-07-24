cbb020c2ad2afd92ccf835319fbdbaf6aa1fa019
CyrilPonsan <cyril.ponsan@gmail.com>
2023-07-24 16:00:20 +0200

    composant stepper créé, custom hook useStep implémenté

---
+++ front/src/config/steps/steps-parcours.ts
@@ -0,0 +1,30 @@
+import { addIdToObject } from "../../utils/add-id-to-objects";
+
+const steps = [
+  {
+    label: "Informations",
+  },
+  {
+    label: "Objectifs",
+  },
+  {
+    label: "Compétences",
+  },
+  {
+    label: "Modules",
+  },
+  {
+    label: "Calendrier",
+  },
+  {
+    label: "Etudiants",
+  },
+  {
+    label: "",
+  },
+];
+
+export const stepsParcours = addIdToObject(steps).map((item: any) => ({
+  ...item,
+  saved: false,
+}));
---
+++ front/src/utils/interfaces/step.ts
@@ -0,0 +1,5 @@
+export default interface Step {
+  id: number;
+  label: string;
+  saved: boolean;
+}
