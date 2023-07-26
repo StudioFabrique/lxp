2b94d7d45f672678e0ad27dab7252ee9255d0589
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2023-07-26 11:39:25 +0200

    réalisation des bases du formulaire

---
+++ front/src/components/parcours-modules/modules-form/modules-form.tsx
@@ -0,0 +1,81 @@
+import { FC, FormEvent, FormEventHandler, useState } from "react";
+import ImageFileUpload from "../../UI/image-file-upload/image-file-upload";
+import Module from "../../../utils/interfaces/module";
+import useInput from "../../../hooks/use-input";
+import { regexGeneric, regexNumber } from "../../../utils/constantes";
+import User from "../../../utils/interfaces/user";
+import Skill from "../../../utils/interfaces/skill";
+import AddTeachers from "./add-teacher.component";
+
+const ModulesForm: FC<{ onSubmit: (module: Module) => void }> = () => {
+  const { value: title } = useInput((value) => regexGeneric.test(value));
+  const { value: description } = useInput((value) => regexGeneric.test(value));
+  const { value: nbHours } = useInput((value) => regexNumber.test(value));
+  const [imageFile, setImageFile] = useState<File | null>(null);
+  const [teachers, setTeachers] = useState<User[]>([]);
+  const [skills, setSkills] = useState<Skill[]>([]);
+
+  const handleSetImage = (file: File) => {
+    setImageFile(file);
+  };
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (
+    event: FormEvent<HTMLFormElement>
+  ) => {};
+
+  return (
+    <form className="flex flex-col gap-y-4 p-4" onSubmit={handleSubmit}>
+      <div className="flex flex-col">
+        <label htmlFor="title">Titre de module</label>
+        <input
+          className="input input-sm w-full"
+          name="title"
+          type="text"
+          value={title.value.value}
+          onChange={title.valueChangeHandler}
+          onBlur={title.valueBlurHandler}
+        />
+      </div>
+
+      <div className="flex flex-col">
+        <label htmlFor="description">Description de module</label>
+        <input
+          className="input input-sm w-full"
+          name="description"
+          type="text"
+          value={description.value}
+          onChange={description.valueChangeHandler}
+          onBlur={description.valueBlurHandler}
+        />
+      </div>
+
+      {/* formateurs compo */}
+      {/* <AddTeachers /> */}
+
+      <div className="flex flex-col">
+        <label htmlFor="skill">Compétences de module</label>
+        {/* skill compo */}
+      </div>
+
+      <div className="flex flex-col">
+        <label htmlFor="nbHours">Nombre d'heures</label>
+        <input
+          className="input input-sm w-full"
+          name="nbHours"
+          type="number"
+          value={nbHours.value}
+          onChange={nbHours.valueChangeHandler}
+          onBlur={nbHours.valueBlurHandler}
+        />
+      </div>
+
+      <ImageFileUpload maxSize={5} onSetFile={handleSetImage} />
+
+      <button type="submit" className="btn mt-10">
+        Ajouter le module
+      </button>
+    </form>
+  );
+};
+
+export default ModulesForm;
