369d206fde0d8a7a73c6c75e36d5a0c983b1a3d2
martin-dhlle <mdhollande@fabriquenumerique.fr>
2023-07-27 14:59:00 +0200

    ajout des listes de modules ajoutés + stockage temporaire de l'image dans le cache avant envoi bdd

--- front/src/components/parcours-modules/parcours-modules.component.tsx
+++ front/src/components/parcours-modules/parcours-modules.component.tsx
@@ -2,18 +2,22 @@ import { FC, useState } from "react";
 import ModulesForm from "./modules-form/modules-form";
 import Module from "../../utils/interfaces/module";
 import Wrapper from "../UI/wrapper/wrapper.component";
+import ModulesList from "./modules-list/modules-list.component";
 
 interface IParcoursModules {}
 
 const ParcoursModules: FC<IParcoursModules> = (props) => {
   const [modules, setModules] = useState<Module[]>([]);
 
-  const handleAddModule = (module: Module) => {};
+  const handleAddModule = (module: Module) => {
+    setModules((currentModules) => [...currentModules, module]);
+  };
 
   return (
     <div className="p-5 grid grid-cols-3">
       <Wrapper>
         <ModulesForm onSubmit={handleAddModule} />
+        <ModulesList modules={modules} />
       </Wrapper>
     </div>
   );
