70176362d8251c24aa3c74ac3811bdde2a343192
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2023-07-28 11:14:40 +0200

    ajout bouton delete pour la liste de formateur et de compétences

--- front/src/components/parcours-modules/modules-form/add-skill/skills-list.component.tsx
+++ front/src/components/parcours-modules/modules-form/add-skill/skills-list.component.tsx
@@ -1,13 +1,15 @@
 import { FC, useEffect } from "react";
 import Skill from "../../../../utils/interfaces/skill";
+import SkillItem from "./skill-item.component";
 
-const SkillsList: FC<{ skills: Skill[] }> = ({ skills }) => {
+const SkillsList: FC<{ skills: Skill[]; onDelete: (id: number) => void }> = ({
+  skills,
+  onDelete,
+}) => {
   return (
     <div className="flex flex-col gap-y-2 mt-5">
       {skills.map((skill) => (
-        <p className="bg-secondary-content p-2 rounded-lg" key={skill.id}>
-          {skill.description}
-        </p>
+        <SkillItem skill={skill} onDelete={onDelete} key={skill.id} />
       ))}
     </div>
   );
