dec9fef78a987c3ebd7ba77580e4c554a89329cf
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2023-07-31 16:07:02 +0200

    ajout modules prisma

--- api/src/models/module/create-modules.ts
+++ api/src/models/module/create-modules.ts
@@ -1,3 +1,33 @@
-import { IModule } from "../../utils/interfaces/db/module";
+import { PrismaClient } from "@prisma/client";
 
-export default async function createModules(modules: IModule[]) {}
+const prisma = new PrismaClient();
+
+export default async function createModulesOnParcours(
+  modules: {
+    title: string;
+    description: string;
+    image: Buffer;
+    duration: number;
+    rating: number;
+  }[]
+) {
+  try {
+    const createdModulesOnParcours = await prisma.modulesOnParcours.createMany({
+      data: modules.map((module) => ({
+        moduleId: 0,
+        parcoursId: 0,
+        module: module,
+      })),
+    });
+
+    console.log(
+      "Modules associés au parcours avec succès:",
+      createdModulesOnParcours
+    );
+  } catch (error) {
+    console.error(
+      "Erreur lors de l'association des modules au parcours:",
+      error
+    );
+  }
+}
