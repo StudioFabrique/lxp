26edd47f22d89e403a2564dd2daa105216ae4d07
CyrilPonsan <cyril.ponsan@gmail.com>
2023-08-04 10:26:15 +0200

    edition parcours

--- front/src/components/edit-parcours/parcours-informations-form.tsx
+++ front/src/components/edit-parcours/parcours-informations-form.tsx
@@ -1,220 +1,128 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-
-import { FC, useCallback, useEffect, useState } from "react";
+import { FC, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Toaster, toast } from "react-hot-toast";
 
+import ParcoursInformationsForm from "./parcours-informations-form";
 import useHttp from "../../hooks/use-http";
-import Formation from "../../utils/interfaces/formation";
-import useInput from "../../hooks/use-input";
-import { autoSubmitTimer } from "../../config/auto-submit-timer";
-import { regexGeneric, regexOptionalGeneric } from "../../utils/constantes";
-import { parcoursAction } from "../../store/redux-toolkit/parcours/parcours";
+import { toast } from "react-hot-toast";
+import Contact from "../../utils/interfaces/contact";
+import { Link } from "react-router-dom";
 import { parcoursInformationsAction } from "../../store/redux-toolkit/parcours/parcours-informations";
-import Selecter from "../UI/selecter/selecter.component";
+import Wrapper from "../UI/wrapper/wrapper.component";
+import DatesSelecter from "../UI/dates-selecter/dates-selecter.component";
+import Contacts from "./contacts";
+import Tags from "../UI/tags/tags.component";
+import Tag from "../../utils/interfaces/tag";
 
 type Props = {
   parcoursId?: string;
 };
 
-let notInitialState = false;
-
-const ParcoursInformationsForm: FC<Props> = ({ parcoursId = "12" }) => {
-  const formation = useSelector((state: any) => state.parcours.formation);
-  const parcoursInfos = useSelector(
-    (state: any) => state.parcoursInformations.infos
+const ParcoursInformations: FC<Props> = ({ parcoursId = "1" }) => {
+  const parcoursStartDate = useSelector(
+    (state: any) => state.parcoursInformations.infos.startDate
   );
-  const [formations, setFormations] = useState<Array<Formation>>([]);
-  const { sendRequest } = useHttp();
+  const parcoursEndDate = useSelector(
+    (state: any) => state.parcoursInformations.infos.endDate
+  );
   const dispatch = useDispatch();
-  const { value: title } = useInput(
-    (value) => regexGeneric.test(value),
-    parcoursInfos.title
-  );
-  const { value: description } = useInput(
-    (value) => regexOptionalGeneric.test(value),
-    parcoursInfos.description
-  );
+  const { sendRequest } = useHttp();
 
-  /**
-   * requête pour récupérer la liste des formations dans la bdd
-   */
-  useEffect(() => {
-    const processData = (data: Array<Formation>) => {
-      setFormations(data);
-    };
-    sendRequest(
-      {
-        path: "/formation",
-      },
-      processData
-    );
-  }, [sendRequest]);
-
-  /**
-   * gestion de la sélection d'une nouvelle formation et mise à jour du state global
-   * @param id number
-   */
-  const handleFormation = (id: number) => {
-    const form = formations.find((item) => item.id === id);
-    if (form) {
-      dispatch(parcoursAction.setParcoursFormation(form));
-    }
-  };
-
-  // vérification des champs du formulaire
-  const formIsValid = title.isValid && description.isValid;
-
-  /**
-   * envoi d'une requête http pour mettre à jour les informations du formulaire
-   */
-  const updateInfos = useCallback(() => {
-    const processData = (data: { message: string }) => {
-      toast.success(data.message);
-    };
-    if (formIsValid) {
+  const updateDates = useCallback(
+    (startDate: string, endDate: string) => {
+      const processData = (data: { success: boolean; message: string }) => {
+        if (data.success) {
+          toast.success(data.message);
+        }
+      };
       sendRequest(
         {
-          path: "/parcours/update-infos",
+          path: "/parcours/update-dates",
           method: "put",
-          body: {
-            parcoursId,
-            title: title.value,
-            description: description.value,
-            formation: formation.id,
-          },
+          body: { parcoursId, startDate, endDate },
         },
         processData
       );
-    }
-  }, [
-    description.value,
-    title.value,
-    formation,
-    formIsValid,
-    sendRequest,
-    parcoursId,
-  ]);
+    },
+    [parcoursId, sendRequest]
+  );
 
-  /**
-   * déclenchement avec un délai configuré dans le fichier auto-submit-timer.ts de l'envoi d'une requête http pour la mise à jour des informations du parcours
-   */
-  useEffect(() => {
-    if (notInitialState) {
-      console.log("youhou");
-
-      const setInfos = async () => {
-        dispatch(
-          parcoursInformationsAction.updateParcoursInfos({
-            title: title.value,
-            description: description.value,
-          })
-        );
-        updateInfos();
+  const updateTags = useCallback(
+    (tags: Array<Tag>) => {
+      const processData = (data: { success: boolean; message: string }) => {
+        if (data.success) {
+          toast.success(data.message);
+        }
       };
-      const timer = setTimeout(() => {
-        setInfos();
-      }, autoSubmitTimer);
-      return () => {
-        clearTimeout(timer);
+      sendRequest(
+        {
+          path: "/parcours/update-tags",
+          method: "put",
+          body: { parcoursId, tags: tags.map((item: Tag) => item.id) },
+        },
+        processData
+      );
+    },
+    [parcoursId, sendRequest]
+  );
+
+  // Callback pour soumettre les dates du parcours
+  const submitDates = useCallback(
+    (dates: { startDate: string; endDate: string }) => {
+      dispatch(parcoursInformationsAction.updateParcoursDates(dates));
+      updateDates(dates.startDate, dates.endDate);
+    },
+    [updateDates, dispatch]
+  );
+
+  const submitContacts = useCallback(
+    (contacts: Array<Contact>) => {
+      const processData = (data: { success: boolean; message: string }) => {
+        if (data.success) {
+          toast.success(data.message);
+        }
       };
-    }
-    notInitialState = true;
-  }, [updateInfos, description.value, title.value, dispatch]);
-
-  /**
-   * définit le style du champ formulaire en fonction de sa validité
-   * @param hasError boolean
-   * @returns string
-   */
-  const setInputStyle = (hasError: boolean) => {
-    return hasError
-      ? "input input-error text-error input-sm input-bordered focus:outline-none w-full"
-      : "input input-sm input-bordered focus:outline-none w-full";
-  };
-
-  /**
-   * définit le style du champ formulaire en fonction de sa validité
-   * @param hasError boolean
-   * @returns string
-   */
-  const setAreaStyle = (hasError: boolean) => {
-    return hasError
-      ? "textarea textarea-error text-error textarea-sm textarea-bordered focus:outline-none w-full"
-      : "textarea textarea-sm textarea-bordered focus:outline-none w-full";
-  };
+      sendRequest(
+        {
+          path: "/parcours/update-contacts",
+          method: "put",
+          body: { parcoursId, contacts },
+        },
+        processData
+      );
+    },
+    [parcoursId, sendRequest]
+  );
 
   return (
-    <>
-      <Toaster />
-      <div>
-        {formation && formations.length > 0 ? (
-          <>
-            <div className="flex flex-col gap-y-4">
-              {/* menu déroulant pour modifier la formation à laquelle est rattaché le parcours */}
-              <h2 className="font-bold">Formation</h2>
-              <Selecter
-                onSelectItem={handleFormation}
-                list={formations.map((item) => ({
-                  id: item.id!,
-                  title: item.title,
-                }))}
-                title="Sélectionner une formation"
-                formation={{ id: formation.id, title: formation.title }}
-              />
-            </div>
-            <form className="w-full flex flex-col gap-y-8 mt-8">
-              <div className="flex flex-col gap-y-4">
-                <label className="font-bold" htmlFor="title">
-                  Titre du parcours
-                </label>
-                <input
-                  className={setInputStyle(title.hasError)}
-                  id="title"
-                  name="title"
-                  type="text"
-                  defaultValue={title.value}
-                  onChange={title.valueChangeHandler}
-                  onBlur={title.valueBlurHandler}
-                  placeholder="Exemple: CDA - Promo 2023"
-                />
-              </div>
-
-              <div className="flex flex-col gap-y-4">
-                <label className="font-bold" htmlFor="level">
-                  Description
-                </label>
-                <textarea
-                  className={setAreaStyle(description.hasError)}
-                  id="description"
-                  name="description"
-                  rows={5}
-                  defaultValue={description.value}
-                  onChange={description.textAreaChangeHandler}
-                  onBlur={description.valueBlurHandler}
-                />
-              </div>
-
-              <div className="flex flex-col gap-y-4">
-                <label className="font-bold" htmlFor="level">
-                  Niveau du parcours
-                </label>
-                <input
-                  className="input input-sm input-bordered focus:outline-none w-full"
-                  id="level"
-                  name="level"
-                  type="level"
-                  readOnly={true}
-                  value={formation.level}
-                  disabled={true}
-                />
-              </div>
-            </form>
-          </>
-        ) : null}
+    <div className="w-full">
+      <h2 className="text-xl font-bold mt-16 mb-8">Informations</h2>
+      <div className="w-full grid grid-cols-1 lg:grid-cols-2 gap-x-16">
+        <Wrapper>
+          <div className="flex flex-col gap-y-8">
+            <ParcoursInformationsForm parcoursId={parcoursId} />
+            <DatesSelecter
+              startDateProp={parcoursStartDate}
+              endDateProp={parcoursEndDate}
+              label="Dates de parcours"
+              onSubmitDates={submitDates}
+            />
+          </div>
+          <Link className="mt-4 mb-8 font-bold hover:underline" to="#">
+            Classe Virtuelle
+          </Link>
+        </Wrapper>
+        <div className="grid grid-rows-2 gap-8">
+          <Wrapper>
+            <Contacts onSubmitContacts={submitContacts} />
+          </Wrapper>
+          <Wrapper>
+            <Tags onSubmitTags={updateTags} />
+          </Wrapper>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
-export default ParcoursInformationsForm;
+export default ParcoursInformations;
