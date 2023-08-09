3d30cfdf7bfe662a5f70040e33f6bd7a440a1d07
martin-dhlle <mdhollande@fabriquenumerique.fr>
2023-08-09 15:06:18 +0200

    be able to update isActive state of each user

--- front/src/components/lists/group-add-user-list/group-user-item.component.tsx
+++ front/src/components/lists/group-add-user-list/group-user-item.component.tsx
@@ -1,8 +1,19 @@
-import { FC } from "react";
+import { ChangeEvent, ChangeEventHandler, FC } from "react";
 import User from "../../../utils/interfaces/user";
 import { AvatarSmall } from "../../UI/avatar/avatar.component";
 
-const GroupUserItem: FC<{ user: User }> = ({ user }) => {
+const GroupUserItem: FC<{
+  user: User;
+  onUpdateUser: (user: User) => void;
+}> = ({ user, onUpdateUser }) => {
+  const handleToggleActiveState: ChangeEventHandler<HTMLInputElement> = (
+    event: ChangeEvent<HTMLInputElement>
+  ) => {
+    const userUpdated = user;
+    userUpdated.isActive = event.target.checked;
+    onUpdateUser(userUpdated);
+  };
+
   return (
     <tr className="bg-secondary/10 hover:bg-blue-800 hover:text-white">
       <td className="bg-transparent rounded-l-xl py-5">
@@ -19,7 +30,12 @@ const GroupUserItem: FC<{ user: User }> = ({ user }) => {
         {user.isActive ? "Actif" : "Inactif"}
       </td>
       <td className="bg-transparent">
-        <input type="checkbox" className="toggle" />
+        <input
+          type="checkbox"
+          className="toggle"
+          onChange={handleToggleActiveState}
+          defaultChecked={user.isActive}
+        />
       </td>
       <td className="bg-transparent rounded-r-xl">act1 act2</td>
     </tr>
