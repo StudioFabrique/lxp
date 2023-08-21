bca098ab2d66b800fd328a9de95e018040955dad
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2023-08-21 11:38:51 +0200

    style btn modifié

--- front/src/components/create-user-header/create-user-header.component.tsx
+++ front/src/components/create-user-header/create-user-header.component.tsx
@@ -10,27 +10,11 @@ const CreateUserHeader = () => (
       </p>
     </div>
     <div className="flex items-center gap-x-2 justify-center md:justify-end">
-      <button type="button" className="btn btn-primary btn-sm md:w-32">
-        Aperçu
+      <button type="button" className="btn btn-outline md:w-32">
+        Annuler
       </button>
-      <button type="submit" className="btn btn-primary btn-sm md:w-32">
-        Publier
-      </button>
-      <button className="btn btn-outline btn-sm btn-primary">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
-          />
-        </svg>
+      <button type="submit" className="btn btn-primary md:w-32">
+        Sauvegarder
       </button>
     </div>
   </div>
