e49e9a85466c77cdeb79fe6614daebb7401ae6e0
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2023-09-19 14:29:11 +0200

    correstions post merge API

--- api/src/routes/v1/v1.router.ts
+++ api/src/routes/v1/v1.router.ts
@@ -7,10 +7,9 @@ import parcoursRouter from "./parcours/parcours.router";
 import skillsRouter from "./skills.router";
 import tagRouter from "./tag.router";
 import formationRouter from "./formation.router";
-import bonusSkillRouter from "./bonus-skill.router";
 import objectiveRouter from "./objective.router";
 import moduleRouter from "./module.router";
-import bonusSkillRouter from "./bonus-skill/bonus-skill.router";
+import bonusSkillRouter from "./bonus-skill.router";
 
 const v1Router = express.Router();
 
