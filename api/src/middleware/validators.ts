671e18759ddbbb27ca3e0b26107005481073ac50
martin-dhlle <mdhollande@fabriquenumerique.fr>
2023-09-26 19:38:17 +0200

    correction complète des validators 2

--- api/src/middleware/validators.ts
+++ api/src/middleware/validators.ts
@@ -17,6 +17,8 @@ export const checkValidatorResult = (
 ) => {
   const checkValues = validationResult(req);
 
+  console.log(checkValues);
+
   if (!checkValues.isEmpty()) {
     const error = {
       message: checkValues.array()[0].msg ?? badQuery,
@@ -29,65 +31,70 @@ export const checkValidatorResult = (
   next();
 };
 
-const customEmptyValueValidation = (field: ValidationChain) => {
-  return (value: any, { req }: { req: any }) => {
-    if (value === undefined || value === null || value === "") {
-      return true; // Empty value is allowed
-    }
+const customPostalCodeValidation = (value: string) => {
+  const postCodePattern = /^\d{5}$/;
 
-    return field.run(req); // Run the original validation rules for non-empty value
-  };
+  if (
+    value === undefined ||
+    value === null ||
+    value === "" ||
+    postCodePattern.test(value)
+  ) {
+    return true; // Empty value is allowed
+  }
+
+  return false; // Run the original validation rules for non-empty value
 };
 
 export const userValidator = [
-  // body("email")
-  //   .exists()
-  //   .isEmail()
-  //   .trim()
-  //   .escape()
-  //   .withMessage("email non conforme"),
-  // body(["firstname", "lastname"])
-  //   .exists()
-  //   .notEmpty()
-  //   .isString()
-  //   .trim()
-  //   .escape()
-  //   .withMessage("firstname ou lastname non conforme"),
-  // body([
-  //   "nickname",
-  //   "description",
-  //   "address",
-  //   "city",
-  //   "links.*.url",
-  //   "links.*.alias",
-  //   "hobbies.*.title",
-  //   "graduations.*.title",
-  //   "graduations.*.degree",
-  // ])
-  //   .custom(customEmptyValueValidation(body().isString().trim().escape()))
-  //   .trim()
-  //   .escape()
-  //   .withMessage(
-  //     "nickname, description, address, city, links, hobbies ou graduations non conforme"
-  //   ),
-  // body("userType")
-  //   .exists()
-  //   .notEmpty()
-  //   .isNumeric()
-  //   .withMessage("userType non conforme"),
-  // body("postCode")
-  //   .custom(
-  //     customEmptyValueValidation(body().isPostalCode("FR").trim().escape())
-  //   )
-  //   .trim()
-  //   .escape()
-  //   .withMessage("postCode non conforme"),
+  body("email")
+    .exists()
+    .isEmail()
+    .trim()
+    .escape()
+    .withMessage("email non conforme"),
+  body(["firstname", "lastname"])
+    .exists()
+    .notEmpty()
+    .isString()
+    .trim()
+    .escape()
+    .withMessage("firstname ou lastname non conforme"),
+  body([
+    "nickname",
+    "description",
+    "address",
+    "city",
+    "links.*.url",
+    "links.*.alias",
+    "hobbies.*.title",
+    "graduations.*.title",
+    "graduations.*.degree",
+  ])
+    .isString()
+    .trim()
+    .escape()
+    .withMessage(
+      "nickname, description, address, city, links, hobbies ou graduations non conforme"
+    ),
+  body("userType")
+    .exists()
+    .notEmpty()
+    .isNumeric()
+    .withMessage("userType non conforme"),
+  body("postCode")
+    .custom(customPostalCodeValidation)
+    .trim()
+    .escape()
+    .withMessage("postCode non conforme"),
   // body(["graduations.*.date", "birthDate"])
-  //   .custom(customEmptyValueValidation(body().isDate().trim().escape()))
+  //   .isDate()
+  //   .trim()
+  //   .escape()
   //   .withMessage("graduations ou birthDate non conforme"),
-  // body(["hobbies", "graduations", "links"])
-  //   .isArray()
-  //   .withMessage("hobbies, graduations ou links non conforme"),
+  body(["hobbies", "graduations", "links"])
+    .isArray()
+    .withMessage("hobbies, graduations ou links non conforme"),
   checkValidatorResult,
 ];
 
