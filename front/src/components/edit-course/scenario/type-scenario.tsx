7ec6636efbf0e5605b719ed80fd43be06946f64d
CyrilPonsan <cyril.ponsan@gmail.com>
2023-10-16 16:32:22 +0200

    type scenario

--- front/src/components/edit-course/scenario/type-scenario.tsx
+++ front/src/components/edit-course/scenario/type-scenario.tsx
@@ -3,22 +3,28 @@ import ExclamationMarkIcon from "../../UI/svg/exclamation-mark-icon";
 interface TypeScenarioProps {
   scenario: boolean;
   label: string;
+  description: string;
+  onChangeScenario: () => void;
 }
 
 const TypeScenario = (props: TypeScenarioProps) => {
+  const baseStyle = "flex flex-col gap-y-4 p-8 rounded-xl border ";
+  const style = props.scenario
+    ? baseStyle + "border-secondary/50 bg-primary/10"
+    : "border-primary/50 bg-secondary/10";
+
   return (
-    <div className="flex flex-col gap-y-4 p-8 rounded-xl border border-primary bg-secondary/10">
+    <div className={style}>
       <label className="flex gap-x-4" htmlFor="scenario">
-        <input type="radio" checked={props.scenario} />
+        <input
+          type="radio"
+          checked={props.scenario}
+          onChange={props.onChangeScenario}
+        />
         <p className="text-lg font-bold">{props.label}</p>
       </label>
-      <div className="border border-primary p-4 rounded-xl flex gap-x-2">
-        <p className="flex-1">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sit
-          amet mattis duin elit nisl, vestibulum quis sagittis feugiat,
-          tincidunt ac orci. Suspendisse rhoncus tempor enim, ut aliquam est
-          lacinia vel. Proin ante velit, feugiat at sem ac.
-        </p>
+      <div className="border border-primary/50 p-4 rounded-xl flex gap-x-2">
+        <p className="flex-1">{props.description}</p>
         <div className="h-full flex items-start">
           <div className="w-8 h-8 text-primary">
             <ExclamationMarkIcon />
