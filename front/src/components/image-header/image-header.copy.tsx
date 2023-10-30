2d3490170b2cc8292a3f6114d15fff27f4f1e741
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2023-10-30 11:18:53 +0100

    amélioration après push

--- front/src/components/image-header/image-header.copy.tsx
+++ front/src/components/image-header/image-header.copy.tsx
@@ -21,8 +21,8 @@ const ImageHeaderCopy = (props: ImageHeaderProps) => {
 
   return (
     <div style={classImage}>
-      <div className="rounded-xl w-full h-full flex flex-col justify-end items-start relative">
-        <div className="absolute top-0 left-0 w-full h-full bg-neutral/50 z-0" />
+      <div className="w-full h-full flex flex-col justify-end items-start relative">
+        <div className="rounded-xl absolute top-0 left-0 w-full h-full bg-neutral/50 z-0" />
         {props.children[1]}
         <div className="w-full flex gap-x-2 px-4 py-2 items-end justify-between font-bold absolute z-10">
           <span className="flex gap-x-2 items-center">
