fa019debf67b50ce3f843e3b260fcf6cc589a3f0
Martin Dhollande <martin.dhollande@icloud.com>
2023-10-31 14:27:10 +0100

    correction bug affichage motion

--- front/src/components/view-parcours/parcours-view-header-menu.tsx
+++ front/src/components/view-parcours/parcours-view-header-menu.tsx
@@ -16,21 +16,6 @@ const ParcoursViewHeaderMenu = () => {
     inputRef.current?.focus();
   };
 
-  const MotionInput = () => (
-    <motion.input
-      ref={inputRef}
-      animate={{
-        width: inputVisible ? "auto" : 0,
-      }}
-      className={`h-full ${
-        inputVisible ? "input input-primary visible" : "hidden"
-      }`}
-      type="text"
-      name="search"
-      id="search"
-    />
-  );
-
   return (
     <div className="absolute flex justify-end w-full h-full p-5 px-10">
       <div className="flex flex-col gap-4 justify-between z-20">
@@ -42,7 +27,16 @@ const ParcoursViewHeaderMenu = () => {
           >
             <span className="flex w-full h-full gap-2">
               <MagnifyIcon />
-              <MotionInput />
+              <motion.input
+                ref={inputRef}
+                animate={{
+                  width: inputVisible ? "auto" : 0,
+                }}
+                type="text"
+                name="search"
+                id="search"
+                className="w-0"
+              />
             </span>
           </button>
           <button type="button" className="btn btn-primary btn-sm py-1">
