4619fd61751d6892eb436ddc894a56cb21673268
CyrilPonsan <cyril.ponsan@gmail.com>
2023-11-20 15:22:35 +0100

    optimisation form infos edit parcours

--- front/src/lib/validation/parcours-edit/infos-parcours-schema.ts
+++ front/src/lib/validation/parcours-edit/infos-parcours-schema.ts
@@ -1,16 +1,16 @@
 import { z } from "zod";
 import { regexGeneric, regexOptionalGeneric } from "../../../utils/constantes";
 
-export const infosSchema = z.object({
+export const infosParCoursSchema = z.object({
   title: z
-    .string({ required_error: "Le titre du cours est obligatoire" })
+    .string({ required_error: "Le titre du parcours est obligatoire" })
     .regex(regexGeneric, {
       message: "Le titre du cours contient des caractères invalides",
     }),
   description: z
     .string()
     .regex(regexOptionalGeneric, {
-      message: "La description du cours contient des caractères invalides",
+      message: "La description du parcours contient des caractères invalides",
     })
     .optional(),
 });
