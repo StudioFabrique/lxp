e25ea3273c7338bf16940fc20ee1a0af96d424cc
CyrilPonsan <cyril.ponsan@gmail.com>
2023-11-23 09:46:29 +0100

    nouvelles icone liste parcours

--- front/src/components/UI/svg/course-icon.tsx
+++ front/src/components/UI/svg/course-icon.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const CourseIcon = () => {
   return (
     <svg
--- front/src/components/parcours-home/parcours-card.tsx
+++ front/src/components/parcours-home/parcours-card.tsx
@@ -6,6 +6,10 @@ import EditIcon from "../UI/svg/edit-icon";
 import EyeIcon from "../UI/svg/eye-icon";
 import Can from "../UI/can/can.component";
 import { useEffect, useState } from "react";
+import PublishedIcon from "../UI/svg/published-icon";
+import DraftIcon from "../UI/svg/draft-icon";
+import EyeSlash from "../UI/svg/eyse-slash";
+import ArrowTopRightIcon from "../UI/svg/arrow-top-right-icon";
 
 interface ParcoursCardProps {
   parcours: Parcours;
@@ -15,7 +19,8 @@ const ParcoursCard = ({ parcours }: ParcoursCardProps) => {
   const nav = useNavigate();
   const [image, setImage] = useState<string | null>(null);
 
-  const handleDeleteParcours = (id: number) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const handleDeleteParcours = (_id: number) => {
     nav(``);
   };
 
@@ -49,8 +54,8 @@ const ParcoursCard = ({ parcours }: ParcoursCardProps) => {
                 to={`view/${parcours.id}`}
                 aria-label="Aperçu du parcours"
               >
-                <div className="w-6 h-6 ">
-                  <EyeIcon />
+                <div className="w-5 h-5 ">
+                  <ArrowTopRightIcon />
                 </div>
               </Link>
             </div>
@@ -60,21 +65,34 @@ const ParcoursCard = ({ parcours }: ParcoursCardProps) => {
 
       <div className="card-body w-full flex flex-col justify-between pt-4">
         <div className="flex flex-col gap-y-2 mb-4">
-          <h2 className="card-title text-primary">
+          <span className="flex justify-between items-center">
+            <h2 className="card-title text-primary">{parcours.title}</h2>
+
+            {parcours.isPublished ? (
+              <div className="w-7 h-7 text-primary">
+                <div
+                  className="tooltip tooltip-bottom"
+                  data-tip="Le parcours est publié"
+                >
+                  <PublishedIcon />
+                </div>
+              </div>
+            ) : (
+              <div className="w-7 h-7 text-warning">
+                <div className="tooltip tooltip-bottom" data-tip="Brouillon">
+                  <DraftIcon />
+                </div>
+              </div>
+            )}
+          </span>
+          <h2 className="card-title text-sm font-normal">
             {parcours.formation.title}
           </h2>
-          <h2 className="card-title text-sm font-normal">{parcours.title}</h2>
         </div>
         <div className="flex flex-col items-start gap-y-1 mb-4 w-full">
           <span className="flex justify-between gap-x-4 items-center w-full">
             <p className="font-bold w-24">Niveau :</p>
             <p className="flex justify-end">{parcours.formation.level}</p>
-          </span>
-          <span className="flex justify-between gap-x-4 items-center w-full">
-            <p className="font-bold w-24">Créé le :</p>
-            <p className="flex justify-end">
-              {localeDate(parcours.createdAt!)}
-            </p>
           </span>
           <span className="flex justify-between gap-x-4 items-center w-full">
             <p className="font-bold w-24">Dernière màj :</p>
@@ -87,19 +105,10 @@ const ParcoursCard = ({ parcours }: ParcoursCardProps) => {
             <p className="flex justify-end capitalize">{parcours.author}</p>
           </span>
           <span className="flex justify-between gap-x-4 items-center w-full">
-            <p className="font-bold w-24">Status :</p>
-            <p className="flex justify-end">
-              {parcours.isPublished ? "Publié" : "Brouillon"}
-            </p>
-          </span>
-          <span className="flex justify-between gap-x-4 items-center w-full">
             <p className="font-bold w-24">Visibilité :</p>
-            <input
-              type="checkbox"
-              className="toggle toggle-primary disabled:toggle-primary disabled:opacity-100 disabled:cursor-default"
-              disabled
-              checked={parcours.visibility}
-            />
+            <div className="w-6 h-6">
+              {parcours.visibility ? <EyeIcon /> : <EyeSlash />}
+            </div>
           </span>
         </div>
 
