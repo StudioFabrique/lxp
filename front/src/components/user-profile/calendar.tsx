396ac4984b7a84e2ee7bdd29d3e36ce8c4809c4b
Martin Dlle <mdhollande@fabriquenumerique.fr>
2023-12-28 15:37:51 +0100

    ajout calendrier fixe

--- front/src/components/user-profile/calendar.tsx
+++ front/src/components/user-profile/calendar.tsx
@@ -1,5 +1,14 @@
+import moment from "moment";
+import { Calendar as Cal } from "react-big-calendar";
+import { momentLocalizer } from "react-big-calendar";
+
 const Calendar = () => {
-  return <div>Section vide</div>;
+  const localizer = momentLocalizer(moment);
+  return (
+    <div className="h-[50vh] flex justify-center">
+      <Cal localizer={localizer} />
+    </div>
+  );
 };
 
 export default Calendar;
