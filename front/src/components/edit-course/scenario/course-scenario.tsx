19af09a56267785b8ce9537a808a75dfa13e5fcb
CyrilPonsan <cyril.ponsan@gmail.com>
2024-01-10 11:03:55 +0100

    rearrangement de la liste des leçons ok

--- front/src/components/edit-course/scenario/course-scenario.tsx
+++ front/src/components/edit-course/scenario/course-scenario.tsx
@@ -9,7 +9,7 @@ import {
 import { useDispatch } from "react-redux";
 import { courseScenarioActions } from "../../../store/redux-toolkit/course/course-scenario";
 import LinearScenarioLessons from "./linear-scenario-lessons";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import useHttp from "../../../hooks/use-http";
 import { useParams } from "react-router-dom";
 import courseScenarioFromHttp from "../../../helpers/course/course-scenario-from-http";
@@ -17,6 +17,7 @@ import Lesson from "../../../utils/interfaces/lesson";
 import toast from "react-hot-toast";
 import ButtonAdd from "../../UI/button-add/button-add";
 import LessonsInDrawer from "./lessons-in-drawer";
+import { autoSubmitTimer } from "../../../config/auto-submit-timer";
 
 const CourseScenario = () => {
   const { courseId } = useParams();
@@ -28,7 +29,11 @@ const CourseScenario = () => {
   const lessons = useSelector(
     (state: any) => state.courseScenario.courseLessons
   ) as Lesson[];
+  const submit = useSelector(
+    (state: any) => state.courseScenario.submit
+  ) as boolean;
   const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false); // déclenche l'affichage d'un icone check-circle dans le composant qui affiche la liste des leçons pour signifier le succes d'une maj de la liste
 
   // switch entre les différents types de scénarios
   const handleChangeScenario = () => {
@@ -62,6 +67,36 @@ const CourseScenario = () => {
     );
   };
 
+  // envoie le nouvel arrangement des leçons au backend
+  const reorderLessons = useCallback(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const applyData = (_data: any) => {
+      setLoading(false);
+      setSuccess(true);
+    };
+    sendRequest(
+      {
+        path: `/lesson/reorder/${courseId}`,
+        method: "put",
+        body: lessons.map((item) => item.id),
+      },
+      applyData
+    );
+    dispatch(courseScenarioActions.resetSubmit());
+  }, [courseId, dispatch, lessons, sendRequest]);
+
+  // déclenche l'envoi du nouvel arrangement de la liste des leçons au backend
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (submit) {
+      timer = setTimeout(() => {
+        reorderLessons();
+        setLoading(true);
+      }, autoSubmitTimer);
+    }
+    return () => clearTimeout(timer);
+  }, [submit, reorderLessons]);
+
   /**
    * récupère le type de scénario et les leçons qui lui sont associées
    */
@@ -77,10 +112,25 @@ const CourseScenario = () => {
     );
   }, [courseId, dispatch, sendRequest]);
 
+  /**
+   * déclenche l'affichage de l'icône check-circle pour signifier la réussite de la maj de la liste des leçons
+   * fait disparaître l'icône au bout de trois secondes
+   */
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (success) {
+      timer = setTimeout(() => {
+        setSuccess(false);
+      }, 3000);
+    }
+    return () => clearTimeout(timer);
+  }, [success]);
+
   // gère les erreurs HTTP
   useEffect(() => {
     if (error.length > 0) {
       toast.error(error);
+      setLoading(false);
       setLoading(false);
     }
   }, [error]);
@@ -119,7 +169,11 @@ const CourseScenario = () => {
                 }
               />
             </div>
-            <LinearScenarioLessons lessons={lessons} loading={loading} />
+            <LinearScenarioLessons
+              lessons={lessons}
+              loading={loading}
+              success={success}
+            />
           </>
         ) : (
           <p>
--- front/src/components/edit-course/scenario/lessons-list.tsx
+++ front/src/components/edit-course/scenario/lessons-list.tsx
@@ -3,20 +3,22 @@ import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { useDispatch } from "react-redux";
 
 import Lesson from "../../../utils/interfaces/lesson";
-import Loader from "../../UI/loader";
 import Wrapper from "../../UI/wrapper/wrapper.component";
 import LessonItem from "./lesson-item";
 import { courseScenarioActions } from "../../../store/redux-toolkit/course/course-scenario";
+import { CheckCircle, Loader2 } from "lucide-react";
 
 interface LessonsListProps {
   lessonsList: Lesson[];
   loading: boolean;
+  success: boolean;
   onEdit: (lesson: Lesson) => void;
   onDelete: (lesson: Lesson) => void;
 }
 
 const LessonsList = (props: LessonsListProps) => {
   const dispatch = useDispatch();
+
   const onDragEnd = (result: any) => {
     // Logique pour réorganiser la liste après le drag and drop
     if (!result.destination) {
@@ -29,7 +31,6 @@ const LessonsList = (props: LessonsListProps) => {
     dispatch(courseScenarioActions.reorderLessons(items));
     // Mettre à jour l'ordre des leçons dans votre état ou appeler une fonction de mise à jour appropriée
     // Exemple :
-    // updateLessonsOrder(items);
   };
 
   return (
@@ -37,50 +38,59 @@ const LessonsList = (props: LessonsListProps) => {
       <main className="max-h-[35rem] w-full pr-4 overflow-auto">
         <section className="flex items-center gap-x-2">
           <h2 className="font-bold text-xl mb-8">
-            Liste des Leçons
+            <span className="flex items-center gap-x-2">
+              <p>Liste des Leçons</p>
+              {props.loading ? (
+                <Loader2 className="w-4 h-4 text-primary animate animate-spin" />
+              ) : null}
+              {props.success ? (
+                <CheckCircle className="w-4 h-4 text-success" />
+              ) : null}
+            </span>
             {props.lessonsList && props.lessonsList.length > 0 ? (
               <p className="text-sm font-normal">{` ${props.lessonsList.length} leçon(s)`}</p>
             ) : (
               <p className="text-sm font-normal">aucune leçon</p>
             )}
           </h2>
-          {props.loading ? <Loader /> : null}
         </section>
         <section>
-          <DragDropContext onDragEnd={onDragEnd}>
-            <Droppable droppableId="lessons">
-              {(provided: any) => (
-                <ul
-                  className="flex flex-col gap-y-4"
-                  {...provided.droppableProps}
-                  ref={provided.innerRef}
-                >
-                  {props.lessonsList.map((lesson: Lesson, index: number) => (
-                    <Draggable
-                      key={lesson.id}
-                      draggableId={lesson.id!.toString()}
-                      index={index}
-                    >
-                      {(provided: any) => (
-                        <li
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                        >
-                          <LessonItem
-                            lesson={lesson}
-                            onEdit={props.onEdit}
-                            onDelete={props.onDelete}
-                          />
-                        </li>
-                      )}
-                    </Draggable>
-                  ))}
-                  {provided.placeholder}
-                </ul>
-              )}
-            </Droppable>
-          </DragDropContext>
+          {props.lessonsList && props.lessonsList.length > 0 ? (
+            <DragDropContext onDragEnd={onDragEnd}>
+              <Droppable droppableId="lessons">
+                {(provided: any) => (
+                  <ul
+                    className="flex flex-col gap-y-4"
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                  >
+                    {props.lessonsList.map((lesson: Lesson, index: number) => (
+                      <Draggable
+                        key={lesson.id}
+                        draggableId={lesson.id!.toString()}
+                        index={index}
+                      >
+                        {(provided: any) => (
+                          <li
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                          >
+                            <LessonItem
+                              lesson={lesson}
+                              onEdit={props.onEdit}
+                              onDelete={props.onDelete}
+                            />
+                          </li>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                  </ul>
+                )}
+              </Droppable>
+            </DragDropContext>
+          ) : null}
         </section>
       </main>
     </Wrapper>
