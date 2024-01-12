c5658fecd88195ce6db0a897a503daa690f7935f
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2024-01-12 11:40:20 +0100

    correction bug parcours affichage des cours du module dans l'aperçu

--- api/src/controllers/course/http-get-courses-by-module.ts
+++ api/src/controllers/course/http-get-courses-by-module.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from "express";
 import { serverIssue } from "../../utils/constantes";
 import getCourses from "../../models/course/get-courses";
+import getCoursesByModule from "../../models/course/get-courses-by-module";
 
 async function httpGetCoursesByModule(req: Request, res: Response) {
   const { moduleId } = req.params;
 
   try {
-    const response = await getCourses();
+    const response = await getCoursesByModule(parseInt(moduleId));
     return res.status(200).json({
       success: true,
       message:
