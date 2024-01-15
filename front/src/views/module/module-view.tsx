e5f51f8c3a2ad925ba4f9752809c229f18dd6121
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2024-01-15 10:15:51 +0100

    ajout header

--- front/src/views/module/module-view.tsx
+++ front/src/views/module/module-view.tsx
@@ -1,5 +1,18 @@
+import FadeWrapper from "../../components/UI/fade-wrapper/fade-wrapper";
+import ImageHeader from "../../components/image-header";
+import HeaderMenu from "../../components/UI/header-menu";
+
 const ModuleView = () => {
-  return <div></div>;
+  return (
+    <FadeWrapper>
+      <ImageHeader
+        imageUrl={/* image */ "test"}
+        title={/* parcoursInfos.title */ "test"}
+        subTitle={/* parcours.formation?.title */ "test"}
+        children={[<></>, <HeaderMenu />]}
+      />
+    </FadeWrapper>
+  );
 };
 
 export default ModuleView;
