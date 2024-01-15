278d3aebfb2ed66b376c1df7e642bdf7e8eed329
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2024-01-15 12:01:49 +0100

    récupération de la liste des cours et detail module depuis API

---
+++ front/src/components/module-view/progression/course-item.tsx
@@ -0,0 +1,9 @@
+import Course from "../../../utils/interfaces/course";
+
+type CourseItemProps = { course: Course };
+
+const CourseItem = ({ course }: CourseItemProps) => {
+  return <div></div>;
+};
+
+export default CourseItem;
