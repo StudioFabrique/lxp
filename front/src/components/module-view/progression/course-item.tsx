1f3d34d69fefc15019516703408f6280eb534c94
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2024-01-15 14:21:01 +0100

    amélioration interface + modification API

--- front/src/components/module-view/progression/course-item.tsx
+++ front/src/components/module-view/progression/course-item.tsx
@@ -1,9 +1,59 @@
+import { ArrowDown, ArrowRight } from "lucide-react";
 import Course from "../../../utils/interfaces/course";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 type CourseItemProps = { course: Course };
 
 const CourseItem = ({ course }: CourseItemProps) => {
-  return <div></div>;
+  const [isCourseOpen, setCourseOpen] = useState(false);
+
+  return (
+    <div className="flex flex-col">
+      <div
+        className="flex flex-col w-full cursor-pointer"
+        onClick={() => setCourseOpen(!isCourseOpen)}
+      >
+        <div className="bg-secondary p-4 rounded-xl flex flex-col gap-2">
+          {/* Titre du cours + tooltip */}
+          <td className="capitalize">
+            <div className="relative">
+              <span
+                data-tip={`Titre : ${course.title}`}
+                className="w-full h-10 tooltip tooltip-right absolute"
+              />
+            </div>
+            <div className="truncate">
+              <h3 className="text-secondary-content/70">{course.title}</h3>
+            </div>
+          </td>
+          <div className="flex justify-between gap-5">
+            <p className="text-secondary-content font-semibold text-sm w-[80%] max-h-10 break-words overflow-y-clip">
+              {course.description}
+            </p>
+            {isCourseOpen ? (
+              <ArrowDown className="stroke-primary" />
+            ) : (
+              <ArrowRight className="stroke-primary" />
+            )}
+          </div>
+        </div>
+        <progress
+          className="w-full progress progress-primary -mt-[8px] rounded-b-full"
+          value={0.5}
+        />
+      </div>
+      <motion.div
+        className="bg-secondary/80 -mt-2 rounded-b-xl p-2 flex flex-col items-center"
+        animate={{
+          display: isCourseOpen ? "block" : "none",
+          height: isCourseOpen ? 200 : 0,
+        }}
+      >
+        <p>Aucune leçons</p>
+      </motion.div>
+    </div>
+  );
 };
 
 export default CourseItem;
--- front/src/components/module-view/progression/progression.tsx
+++ front/src/components/module-view/progression/progression.tsx
@@ -1,17 +1,24 @@
+import { CSSProperties } from "react";
 import Course from "../../../utils/interfaces/course";
 import Wrapper from "../../UI/wrapper/wrapper.component";
 import CourseItem from "./course-item";
 
 type ProgressionProps = { courses: Course[] };
 
+const radialStyle = {
+  "--value": 25,
+} as CSSProperties;
+
 const Progression = ({ courses }: ProgressionProps) => {
   return (
     <Wrapper>
-      <div className="flex">
-        <h2>Progression du module</h2>
-        <progress className="progress" />
+      <div className="flex justify-between">
+        <h2 className="w-28">Progression du module</h2>
+        <div className="radial-progress" style={radialStyle}>
+          25%
+        </div>
       </div>
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center gap-5">
         {courses.length > 0 ? (
           courses.map((course) => <CourseItem course={course} />)
         ) : (
