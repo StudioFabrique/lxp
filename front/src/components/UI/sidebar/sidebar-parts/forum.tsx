4674ea69d5a1d974731283abe3c77c4db040f450
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2024-01-19 13:32:59 +0100

    modification sidebar

--- front/src/components/UI/sidebar/sidebar-parts/forum.tsx
+++ front/src/components/UI/sidebar/sidebar-parts/forum.tsx
@@ -4,7 +4,7 @@ import { MessageCircleIcon } from "lucide-react";
 const Forum = ({ interfaceType }: { interfaceType: string }) => (
   <li>
     <Link to={`/${interfaceType}`}>
-      <div className="tooltip tooltip-right w-6 h-6" data-tip="Accueil LXP">
+      <div className="tooltip tooltip-right w-6 h-6" data-tip="Forum">
         <MessageCircleIcon />
       </div>
     </Link>
