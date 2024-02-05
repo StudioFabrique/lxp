735dfc5e4ae628d4512582e32a10176a8cce1d96
CyrilPonsan <cyril.ponsan@gmail.com>
2024-02-05 10:59:59 +0100

    test

--- api/src/controllers/parcours/http-get-parcours-by-formation.ts
+++ api/src/controllers/parcours/http-get-parcours-by-formation.ts
@@ -3,20 +3,17 @@ import { Request, Response } from "express";
 import { serverIssue } from "../../utils/constantes";
 import getParcoursByFormation from "../../models/parcours/get-parcours-by-formation";
 
+//  retourne la liste des parcours associés à une formation
 async function httpGetParcoursByFormation(req: Request, res: Response) {
   try {
     const { formationId } = req.params;
     const response = await getParcoursByFormation(+formationId);
-    return res
-      .status(200)
-      .json({
-        success: true,
-        message:
-          response.length === 0
-            ? "Liste vide."
-            : "Liste des parcours récupérée.",
-        data: response,
-      });
+    return res.status(200).json({
+      success: true,
+      message:
+        response.length === 0 ? "Liste vide." : "Liste des parcours récupérée.",
+      data: response,
+    });
   } catch (error) {
     return res.status(500).json({ message: serverIssue });
   }
