46d882f5185797c08c7870253ea1a88847266519
Martin Dhollande <martindhollande@Air-de-Martin.lan>
2024-02-06 20:59:35 +0100

    préparation api dashboard

---
+++ api/src/models/lesson/get-last-lessons-read.ts
@@ -0,0 +1,7 @@
+export default async function getLastLessonsRead(userIdMdb: string) {
+  const lessons = prisma?.lessonRead.findMany({
+    where: { student: { idMdb: userIdMdb } },
+    orderBy: { beganAt: "desc" },
+    take: 4,
+  });
+}
