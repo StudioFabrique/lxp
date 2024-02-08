3ac7b2aa8fc518ca707a298d9a21a45276f15a21
Martin Dhollande <martindhollande@Air-de-Martin.lan>
2024-02-08 17:38:02 +0100

    mise en place complète de la liste des cours les plus plus

--- front/src/components/student-home/right-side/most-read-courses.tsx
+++ front/src/components/student-home/right-side/most-read-courses.tsx
@@ -2,25 +2,40 @@ import { useEffect, useState } from "react";
 import useHttp from "../../../hooks/use-http";
 import Course from "../../../utils/interfaces/course";
 import { ArrowUpRightSquare } from "lucide-react";
+import { Link, useLocation } from "react-router-dom";
 
 const MostReadCourses = () => {
   const { sendRequest } = useHttp(true);
+  const { pathname } = useLocation();
+  const currentRoute = pathname.split("/").slice(1) ?? [];
 
   const [courses, setCourses] = useState<Course[]>();
 
   useEffect(() => {
-    sendRequest({ path: "/course/most-read" });
+    const applyData = (data: { data: Course[] }) => {
+      setCourses(data.data);
+    };
+
+    sendRequest({ path: "/course/most-read" }, applyData);
   }, [sendRequest]);
 
   return (
     <div className="flex flex-col items-center justify-between bg-secondary rounded-lg p-5 gap-5">
-      <p className="font-bold">Les cours les plus consultés</p>
+      <p className="font-bold self-start">Les cours les plus consultés</p>
       {courses && courses?.length > 0 ? (
         courses?.map((course) => (
-          <span className="w-full bg-indigo-200 rounded-lg p-2">
-            <p className="font-semibold"></p>
+          <span
+            key={course.id}
+            className="flex justify-between gap-5 bg-indigo-200 rounded-lg p-2"
+          >
             <p>{course.title}</p>
-            <ArrowUpRightSquare />
+            <Link
+              to={`/${currentRoute}/parcours/module/${course.module.id}`}
+              state={{ lessonId: course.lessons[0].id }}
+              className="self-end"
+            >
+              <ArrowUpRightSquare />
+            </Link>
           </span>
         ))
       ) : (
