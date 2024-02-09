0d681adcc60474e196d5592a881736841f085a0e
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2024-02-09 15:21:49 +0100

    correction style accueil apprenant

--- front/src/components/student-home/right-side/feedback-apprenant.tsx
+++ front/src/components/student-home/right-side/feedback-apprenant.tsx
@@ -1,19 +1,42 @@
+import imageProfileReplacement from "../../../config/image-profile-replacement";
+
 const FeedbackApprenant = () => {
   return (
     <div className="flex flex-col items-center justify-between bg-secondary rounded-lg p-5 gap-5">
       <p className="font-bold self-start">Derniers feedback des apprenants</p>
-      <span className="w-full bg-indigo-200 rounded-lg p-2">
-        <p className="font-semibold">Jean Marc DUPONT</p>
-        <p>vient de terminer le cours 5</p>
-      </span>
-      <span className="w-full bg-indigo-200 rounded-lg p-2">
-        <p className="font-semibold">Stanley FROGATE</p>
-        <p>vient d'ajouter un commentaire</p>
-      </span>
-      <span className="w-full bg-indigo-200 rounded-lg p-2">
-        <p className="font-semibold">Clementine ORANGER</p>
-        <p>a obtenu 10/10 dans le quizz</p>
-      </span>
+      <div className="flex gap-4 w-full bg-indigo-200 rounded-lg p-2 tooltip tooltip-right">
+        <img
+          className="rounded-lg h-[35px] w-[35px] object-cover"
+          src={`data:image/jpeg;base64,${imageProfileReplacement}`}
+          alt="User Avatar"
+        />
+        <span className="flex flex-col items-start">
+          <p className="font-semibold">Stanley FROGATE</p>
+          <p>vient d'ajouter un commentaire</p>
+        </span>
+      </div>
+      <div className="flex gap-4 w-full bg-indigo-200 rounded-lg p-2 tooltip tooltip-right">
+        <img
+          className="rounded-lg h-[35px] w-[35px] object-cover"
+          src={`data:image/jpeg;base64,${imageProfileReplacement}`}
+          alt="User Avatar"
+        />
+        <span className="flex flex-col items-start">
+          <p className="font-semibold">Stanley FROGATE</p>
+          <p>vient d'ajouter un commentaire</p>
+        </span>
+      </div>
+      <div className="flex gap-4 w-full bg-indigo-200 rounded-lg p-2 tooltip tooltip-right">
+        <img
+          className="rounded-lg h-[35px] w-[35px] object-cover"
+          src={`data:image/jpeg;base64,${imageProfileReplacement}`}
+          alt="User Avatar"
+        />
+        <span className="flex flex-col items-start">
+          <p className="font-semibold">Stanley FROGATE</p>
+          <p>vient d'ajouter un commentaire</p>
+        </span>
+      </div>
     </div>
   );
 };
