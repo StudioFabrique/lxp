8226b70b1bbbe4fe4468f02c16a5a6bb9f4dbe46
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2024-02-09 09:49:41 +0100

    correction bugs graphiques

--- front/src/components/UI/sidebar/sidebar-parts/library.tsx
+++ front/src/components/UI/sidebar/sidebar-parts/library.tsx
@@ -1,14 +1,23 @@
 import { Link } from "react-router-dom";
 import { LibraryBigIcon } from "lucide-react";
 
-const Library = ({ currentRoute }: { currentRoute: string[] }) => (
-  <li>
-    <Link to={`/${currentRoute[0]}/library`}>
-      <div className="tooltip tooltip-right w-6 h-6" data-tip="Bibliothèque">
-        <LibraryBigIcon />
-      </div>
-    </Link>
-  </li>
-);
+const Library = ({ currentRoute }: { currentRoute: string[] }) => {
+  const isCurrentPathActive = currentRoute[1] === "library";
+
+  return (
+    <li>
+      <Link to={`/${currentRoute[0]}/library`}>
+        <div
+          className={`tooltip tooltip-right w-6 h-6 ${
+            isCurrentPathActive && "text-primary"
+          }`}
+          data-tip="Bibliothèque"
+        >
+          <LibraryBigIcon />
+        </div>
+      </Link>
+    </li>
+  );
+};
 
 export default Library;
