68ae24985bb5749f8cd68634a74a1072a976293b
CyrilPonsan <cyril.ponsan@gmail.com>
2024-02-16 16:19:56 +0100

    socket url

--- front/src/config/urls.ts
+++ front/src/config/urls.ts
@@ -7,4 +7,5 @@ export const DOWNLOAD_URL = url;
 
 export const ACTIVITIES_VIDEOS = url + "activities/videos/";
 
-export const SOCKET_URL = "http://localhost:5001";
+export const SOCKET_URL =
+  process.env.NODE_ENV === "development" ? "http://localhost:5001" : "/";
