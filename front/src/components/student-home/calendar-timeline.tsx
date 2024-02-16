dfc33a3ff3034921fd3997de636fb3864ea5b2a1
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2024-02-16 14:27:32 +0100

    mise en place timeline complété + correction bugs

--- front/src/components/student-home/calendar-timeline.tsx
+++ front/src/components/student-home/calendar-timeline.tsx
@@ -1,60 +1,82 @@
+import { useEffect, useState } from "react";
 import ReactApexChart from "react-apexcharts";
+import useHttp from "../../hooks/use-http";
+import Module from "../../utils/interfaces/module";
+import { getRandomHexColor } from "../../utils/randomColor";
+
+type TimelineData =
+  | (number | null)[]
+  | {
+      x: any;
+      y: any;
+      fill?: ApexFill;
+      fillColor?: string;
+      strokeColor?: string;
+      meta?: any;
+      goals?: any;
+      barHeightOffset?: number;
+      columnWidthOffset?: number;
+    }[]
+  | [number, number | null][]
+  | [number, (number | null)[]][]
+  | number[][];
 
 const CalendarTimeline = () => {
+  const { sendRequest } = useHttp();
+
+  const [timelineData, setTimelineData] = useState<TimelineData>();
+
+  useEffect(() => {
+    const applyData = (data: { data: Module[] }) => {
+      setTimelineData(
+        data.data
+          .filter((module) => module.minDate && module.maxDate)
+          .map((module) => {
+            return {
+              x: module.title,
+              y: [
+                module.minDate && new Date(module.minDate).getTime(),
+                module.maxDate && new Date(module.maxDate).getTime(),
+              ],
+              fillColor: getRandomHexColor(),
+            };
+          })
+      );
+    };
+
+    sendRequest({ path: "/modules/timeline" }, applyData);
+  }, [sendRequest]);
+
   return (
     <div>
       <h2 className="font-bold text-xl">Mon emploi du temps</h2>
       <div className="">
-        <ReactApexChart
-          type={"rangeBar"}
-          height={220}
-          series={[
-            {
-              data: [
-                {
-                  x: "Code",
-                  y: [
-                    new Date("2019-03-02").getTime(),
-                    new Date("2019-03-04").getTime(),
-                  ],
-                  fillColor: "#F44336",
-                },
-                {
-                  x: "Test",
-                  y: [
-                    new Date("2019-03-04").getTime(),
-                    new Date("2019-03-08").getTime(),
-                  ],
-                },
-                {
-                  x: "Validation",
-                  y: [
-                    new Date("2019-03-08").getTime(),
-                    new Date("2019-03-12").getTime(),
-                  ],
-                },
-                {
-                  x: "Deployment",
-                  y: [
-                    new Date("2019-03-12").getTime(),
-                    new Date("2019-03-18").getTime(),
-                  ],
-                },
-              ],
-            },
-          ]}
-          options={{
-            plotOptions: {
-              bar: {
-                horizontal: true,
-                borderRadius: 5,
+        {timelineData && (
+          <ReactApexChart
+            type={"rangeBar"}
+            height={400}
+            series={[
+              {
+                data: timelineData,
               },
-            },
-            xaxis: {
-              type: "datetime",
-            },
-          }}
-        />
+            ]}
+            options={{
+              plotOptions: {
+                bar: {
+                  horizontal: true,
+                  borderRadius: 5,
+                  barHeight: 50,
+                },
+              },
+              xaxis: {
+                labels: {
+                  format: "MMMM",
+                },
+                type: "datetime",
+              },
+            }}
+          />
+        )}
       </div>
     </div>
   );
