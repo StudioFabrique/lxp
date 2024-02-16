0713063f0a2ab76afeb650d4ab6a909f867364fe
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2024-02-16 10:19:08 +0100

    correction padding

--- front/src/components/student-home/right-side/feedback-apprenant.tsx
+++ front/src/components/student-home/right-side/feedback-apprenant.tsx
@@ -4,8 +4,8 @@ const FeedbackApprenant = () => {
   return (
     <div className="flex flex-col items-center bg-secondary text-secondary-content rounded-lg p-5 gap-5 h-[350px]">
       <p className="font-bold self-start">Derniers feedback des apprenants</p>
-      <div className="flex flex-col gap-5 carousel carousel-vertical">
-        <div className="flex gap-4 items-center w-full  bg-primary text-primary-content rounded-lg p-2 tooltip tooltip-right">
+      <div className="flex flex-col w-full gap-5 carousel carousel-vertical">
+        <div className="flex gap-4 items-center w-full bg-primary text-primary-content rounded-lg p-2 tooltip tooltip-right">
           <img
             className="rounded-lg h-[35px] w-[35px] object-cover"
             src={`data:image/jpeg;base64,${imageProfileReplacement}`}
