e7c45749cd754f305047ca2ead02ffd565387b7d
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2024-02-19 13:41:47 +0100

    correction style sidebar

--- front/src/components/UI/sidebar/sidebar-parts/forum.tsx
+++ front/src/components/UI/sidebar/sidebar-parts/forum.tsx
@@ -7,12 +7,7 @@ const Forum = ({ currentRoute }: { currentRoute: string[] }) => {
   return (
     <li>
       <Link to={`/${currentRoute[0]}/forum`}>
-        <div
-          className={`tooltip tooltip-right w-6 h-6 ${
-            isCurrentPathActive && "text-primary"
-          }`}
-          data-tip="Forum"
-        >
+        <div className="tooltip tooltip-right w-6 h-6" data-tip="Forum">
           <div className="flex hover justify-center items-center">
             <MessageCircleIcon className="z-10 pointer-events-none" />
             <span
