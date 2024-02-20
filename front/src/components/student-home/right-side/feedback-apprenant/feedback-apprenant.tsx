8e252628d865f7d3d5054cf13f6d6f3dffbe97d0
Martin Dhollande <martindhollande@Air-de-Martin.lan>
2024-02-20 12:57:28 +0100

    intégration changements et donnees au composant

--- front/src/components/student-home/right-side/feedback-apprenant/feedback-apprenant.tsx
+++ front/src/components/student-home/right-side/feedback-apprenant/feedback-apprenant.tsx
@@ -1,14 +1,23 @@
 import { useEffect, useState } from "react";
 import useHttp from "../../../../hooks/use-http";
 import Loader from "../../../UI/loader";
+import Student from "../../../../utils/interfaces/student";
+import Item from "./item";
+
+export interface Accomplishment {
+  description: stirng;
+  id: number;
+  name: string;
+  student: Student;
+}
 
 const FeedbackApprenant = () => {
   const { sendRequest, isLoading } = useHttp();
-  const [feedbacks, setFeedbacks] = useState();
+  const [accomplishments, setAccomplishments] = useState<Accomplishment[]>();
 
   useEffect(() => {
     const applyData = (data: { data: any }) => {
-      console.log({ data });
+      setAccomplishments(data.data);
     };
 
     sendRequest({ path: "/user/accomplishment" }, applyData);
@@ -18,9 +27,13 @@ const FeedbackApprenant = () => {
     <div className="flex flex-col items-center bg-secondary text-secondary-content rounded-lg p-5 gap-5 h-[350px]">
       <p className="font-bold self-start">Derniers feedback des apprenants</p>
       <div className="flex flex-col w-full gap-5 carousel carousel-vertical">
-        {/* map item */}
-        {isLoading ? <Loader /> : <></>}
-        <p>Aucun feedback</p>
+        {isLoading ? (
+          <Loader />
+        ) : accomplishments && accomplishments?.length > 0 ? (
+          accomplishments.map((item) => <Item accomplishment={item} />)
+        ) : (
+          <p>Aucun feedback</p>
+        )}
       </div>
     </div>
   );
