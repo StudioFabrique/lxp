405a3ce8e5a244c8f7d689b140e2d2aa3dbfce7e
Martin Dhollande <martindhollande@MacBook-Air-de-Martin.local>
2024-02-23 10:37:22 +0100

    ajout tests unitaires

--- front/src/views/profile/profile.test.tsx
+++ front/src/views/profile/profile.test.tsx
@@ -1,4 +1,7 @@
-import { describe, it } from "vitest";
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { describe, expect, it } from "vitest";
+import Profile from "./profile";
 
 describe(
   "Conditional tests about components render along with the router state",
@@ -6,5 +9,27 @@ describe(
 );
 
 function Tests() {
-  it("Should not show 'skill' and 'links' components when admin is connected", () => {});
+  it("Should not show 'skill' and 'links' components when admin is connected", () => {
+    // Render the Profile component within a MemoryRouter
+    const { container } = render(
+      <MemoryRouter initialEntries={["/admin", "/formateur"]}>
+        <Profile />
+      </MemoryRouter>
+    );
+
+    // Check if Profile does not contain any skills or links
+    expect(container.innerHTML).not.toContain("Mes centres d'intérêts");
+  });
+
+  it("Should not show 'skill' and 'links' components when admin is connected", () => {
+    // Render the Profile component within a MemoryRouter
+    const { container } = render(
+      <MemoryRouter initialEntries={["/student"]}>
+        <Profile />
+      </MemoryRouter>
+    );
+
+    // Check if Profile contain skills or links component
+    expect(container.innerHTML).toContain("Mes centres d'intérêts");
+  });
 }
