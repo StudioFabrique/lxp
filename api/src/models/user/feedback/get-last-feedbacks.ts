ba8b9cb311db883914d7685756aedddbd2068218
CyrilPonsan <cyril.ponsan@gmail.com>
2024-02-23 10:25:48 +0100

    fix get-last-feedbacks

--- api/src/models/user/feedback/get-last-feedbacks.ts
+++ api/src/models/user/feedback/get-last-feedbacks.ts
@@ -1,12 +1,43 @@
+import { prisma } from "../../../utils/db";
+import Group from "../../../utils/interfaces/db/group";
 import StudentFeedback from "../../../utils/interfaces/db/student-feedback";
 
 export default async function getLastFeedbacks(teacherId: string) {
-  const result = await StudentFeedback.find({ teacher: new Object(teacherId) })
+  const groupsSql = await prisma.group.findMany({
+    where: {
+      parcours: {
+        some: {
+          parcours: {
+            contacts: {
+              some: {
+                contact: {
+                  idMdb: teacherId,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+
+  const groupsIds = groupsSql.map((item) => new Object(item.idMdb));
+
+  console.log({ groupsIds });
+
+  const studentsIds = await Group.find(
+    { _id: { $in: groupsIds } },
+    { _id: 1 }
+  ).populate("users", { _id: 1 });
+
+  const ids = studentsIds.map((item) =>
+    item.users.map((elem: any) => elem._id)
+  );
+
+  const result = await StudentFeedback.find({ user: { $in: ids[0] } })
     .sort({ feedbackAt: "desc" })
     .limit(5)
     .populate("user", { _id: 1, firstname: 1, lastname: 1, avatar: 1 });
-
-  console.log({ result });
 
   const feedbacks = result.map((item) => ({
     _id: item._id,
