20712eec35a3837ffe710909c83b47042d93ebf7
CyrilPonsan <cyril.ponsan@gmail.com>
2024-02-26 13:34:52 +0100

    corrections diverses et commentaires

--- api/src/app.ts
+++ api/src/app.ts
@@ -37,11 +37,9 @@ app
   .use(
     cors({
       origin: [
-        "http://localhost:3000",
         "http://localhost:5173",
         "http://localhost:5174",
         "http://localhost:5175",
-        "http://localhost:4173",
       ],
       credentials: true,
     })
