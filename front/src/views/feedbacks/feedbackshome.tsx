08481e1007ac9541fdb333c4fcd374c7aef2bfca
CyrilPonsan <cyril.ponsan@gmail.com>
2024-03-06 09:07:15 +0100

    fix permissions editions parcours

--- front/src/views/feedbacks/feedbackshome.tsx
+++ front/src/views/feedbacks/feedbackshome.tsx
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useState } from "react";
 import StudentFeedback from "../../utils/interfaces/student-feedback";
 import useHttp from "../../hooks/use-http";
 import FeedbacksList from "../../components/feedbacks-home/feedbacks-list";
+import Wrapper from "../../components/UI/wrapper/wrapper.component";
 
 export default function FeedbacksHome() {
   const [feedbacks, setFeedbacks] = useState<StudentFeedback[]>([]);
@@ -33,7 +34,17 @@ export default function FeedbacksHome() {
     <div>
       <h1 className="text-2xl font-bold">Feedbacks des apprenants</h1>
       <section>
-        {feedbacks.length > 0 ? <FeedbacksList feedbacks={feedbacks} /> : null}
+        {feedbacks.length > 0 ? (
+          <FeedbacksList feedbacks={feedbacks} />
+        ) : (
+          <div className="w-fit mt-2">
+            <Wrapper>
+              <p>
+                Vous n'avez reçu aucun feedback de la part de vos apprenants.
+              </p>
+            </Wrapper>
+          </div>
+        )}
       </section>
     </div>
   );
