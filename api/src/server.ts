14efc9fb581fed42363f3506a2b3c3f0c2af926a
CyrilPonsan <cyril.ponsan@gmail.com>
2024-03-10 11:39:41 +0100

    exceptions socket io

--- api/src/server.ts
+++ api/src/server.ts
@@ -12,7 +12,7 @@ let server!: any;
 
 server = http.createServer(app);
 
-const io = new Server(server, {
+export const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
