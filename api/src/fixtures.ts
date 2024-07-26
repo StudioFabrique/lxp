42b2b046a295b85919f17871993caf942438665b
CyrilPonsan <cyril.ponsan@gmail.com>
2024-07-26 16:20:50 +0200

    tests

--- api/src/fixtures.ts
+++ api/src/fixtures.ts
@@ -23,7 +23,7 @@ import ConnectionInfos from "./utils/interfaces/db/connection-infos";
 dotenv.config();
 
 const MONGO_URL = process.env.FIXTURES_MONGO_URL;
-console.log("coucou les gens !");
+console.log(MONGO_URL, process.env.FIXTURES_MONGO_URL);
 
 mongoose.connection.once("open", () => {
   console.log("MongoDB connection ready!");
