d1c29c05533d22386eae692b3a147129b2c413bd
CyrilPonsan <cyril.ponsan@gmail.com>
2024-07-26 15:52:48 +0200

    tests

--- api/src/fixtures.ts
+++ api/src/fixtures.ts
@@ -22,7 +22,7 @@ import IConnectionInfos from "./utils/interfaces/db/connection-infos";
 import ConnectionInfos from "./utils/interfaces/db/connection-infos";
 dotenv.config();
 
-const MONGO_URL = process.env.MONGO_LOCAL_URL;
+const MONGO_URL = process.env.FIXTURES_MONGO_URL;
 console.log({ MONGO_URL });
 
 mongoose.connection.once("open", () => {
