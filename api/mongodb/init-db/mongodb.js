0f05e32eef02661a4c370c56dabdaed4faf81d11
CyrilPonsan <cyril.ponsan@gmail.com>
2024-07-29 15:42:05 +0200

    test

--- api/mongodb/init-db/mongodb.js
+++ api/mongodb/init-db/mongodb.js
@@ -1,3 +1,8 @@
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "../../../.env") });
+
+console.log(process.env.POSTGRES_PASSWORD);
+
 db.createUser({
   user: "studio-lxp",
   pwd: "Tata1234",
