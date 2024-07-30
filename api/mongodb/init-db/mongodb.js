658834747ae7f34c0b891f6a796f3bad4c1dfa48
CyrilPonsan <cyril.ponsan@gmail.com>
2024-07-30 09:03:36 +0200

    test

--- api/mongodb/init-db/mongodb.js
+++ api/mongodb/init-db/mongodb.js
@@ -1,16 +1,10 @@
-const path = require("path");
-require("dotenv").config();
-
-console.log("hello mongodb");
-console.log(process.env.POSTGRES_PASSWORD);
-
 db.createUser({
-  user: process.env.MONGO_USERNAME,
-  pwd: process.env.MONGO_PASSWORD,
+  user: "studio-lxp",
+  pwd: "Tata1234",
   roles: [
     {
       role: "readWrite",
-      db: process.env.MONGO_DATABASE,
+      db: "lxp",
     },
   ],
 });
