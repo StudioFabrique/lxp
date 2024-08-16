4fb5cc270e3694f6acd5f0941c1e7455c36af5b3
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2024-08-16 11:32:08 +0200

    correction du infinite render

--- front/src/components/lists/group-add-user-list/group-manage-user-list/group-manage-user-item/group-manage-user-item.component.tsx
+++ front/src/components/lists/group-add-user-list/group-manage-user-list/group-manage-user-item/group-manage-user-item.component.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useRef, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import User from "../../../../../utils/interfaces/user";
 import { AvatarSmall } from "../../../../UI/avatar/avatar.component";
 import toTitleCase from "../../../../../utils/toTitleCase";
