1965d0d99c97bbcfca4ae1c71e914ac7d89939c4
CyrilPonsan <cyril.ponsan@gmail.com>
2024-08-30 14:00:23 +0200

    tags wip

--- front/src/components/edit-parcours/informations/tags-with-drawer.tsx
+++ front/src/components/edit-parcours/informations/tags-with-drawer.tsx
@@ -3,28 +3,39 @@ import { useDispatch, useSelector } from "react-redux";
 import Tag from "../../../utils/interfaces/tag";
 import CurrentTags from "../../inherited-items/current-tags";
 import InheritedItems from "../../inherited-items/inherited-items";
-import NotSelectedTags from "../../inherited-items/not-selected-tags";
 import { useCallback, useEffect, useRef, useState } from "react";
-import useHttp from "../../../hooks/use-http";
 import { tagsAction } from "../../../store/redux-toolkit/tags";
 import { autoSubmitTimer } from "../../../config/auto-submit-timer";
+import { Link } from "react-router-dom";
+import useHttp from "../../../hooks/use-http";
+import ParcoursTagsSelecter from "./parcours-tags-selecter";
 
 interface TagsWithDrawerProps {
   loading: boolean;
   onSubmit: (items: any[]) => void;
 }
 
+type Item = {
+  title: string;
+  formationId?: number;
+};
+
 const TagsWithDrawer = (props: TagsWithDrawerProps) => {
+  const formation = useSelector((state: any) => state.parcours.formation);
   const currentTags = useSelector(
-    (state: any) => state.tags.currentTags
+    (state: any) => state.tags.currentTags,
   ) as Tag[];
   const initialTags = useSelector(
-    (state: any) => state.tags.initialTags
+    (state: any) => state.tags.initialTags,
   ) as Tag[];
-  const isInitialRender = useRef(true);
-  const { sendRequest } = useHttp();
   const dispatch = useDispatch();
   const [submit, setSubmit] = useState<boolean>(false);
+  const isInitialRender = useRef(true);
+  const { sendRequest } = useHttp();
+  const [formationTags, setFormationTags] = useState<Tag[]>([]);
+  const [currentFormation, setCurrentFormation] = useState<Item | null>(null);
+
+  console.log({ formation });
 
   /**
    * met à jour la liste des tags sélectionnés dans le state partagé et
@@ -34,7 +45,7 @@ const TagsWithDrawer = (props: TagsWithDrawerProps) => {
       setSubmit(true);
       dispatch(tagsAction.setCurrentTags(tags));
     },
-    [dispatch]
+    [dispatch],
   );
 
   /**
@@ -64,10 +75,16 @@ const TagsWithDrawer = (props: TagsWithDrawerProps) => {
         {
           path: "/tag",
         },
-        processData
+        processData,
       );
     }
   }, [dispatch, sendRequest]);
+
+  useEffect(() => {
+    if (formation) {
+      setFormationTags(formation.tags.map((item: any) => item.tag));
+    }
+  }, [formation]);
 
   return (
     <InheritedItems
@@ -75,13 +92,39 @@ const TagsWithDrawer = (props: TagsWithDrawerProps) => {
       drawerTitle="Ajouter des Tags"
       title="Tags"
       loading={props.loading}
-      initialList={initialTags}
+      initialList={formationTags}
       selectedItems={currentTags}
       property="name"
       onSubmit={handleUpdateTags}
     >
       <CurrentTags />
-      <NotSelectedTags />
+      <ParcoursTagsSelecter
+        formations={[]}
+        formationId={1}
+        onSearchTag={() => {}}
+      >
+        <div className="max-w-96">
+          <p>
+            Tous les tags en lien avec la formation ont été ajoutés au parcours.
+            Si vous souhaitez créer de nouveau tags pour ce parcours il faut
+            d'abord les ajouter à la formation en suivant ce{" "}
+            {formation ? (
+              <Link
+                className="text-primary underline"
+                to={`/admin/formation?formationId=${formation.id ?? ""}`}
+              >
+                lien
+              </Link>
+            ) : null}
+            .
+          </p>
+
+          <p>
+            Vous pouvez également ajouter des tags en lien avec d'autres
+            formations en utilisant le menu déroulant.
+          </p>
+        </div>
+      </ParcoursTagsSelecter>
     </InheritedItems>
   );
 };
