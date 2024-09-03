186798673d900bf96e89b505a59e0ae07ee086ce
CyrilPonsan <cyril.ponsan@gmail.com>
2024-09-03 11:08:07 +0200

    message d'erreur lors de la suppression d'un module lié à la formation fix

--- api/src/controllers/module/http-delete-formation-module.ts
+++ api/src/controllers/module/http-delete-formation-module.ts
@@ -4,13 +4,18 @@ import { serverIssue } from "../../utils/constantes";
 
 export default async function httpDeleteFormationModule(
   req: Request,
-  res: Response
+  res: Response,
 ) {
   try {
     const { moduleId } = req.params;
     await deleteFormationModule(+moduleId);
     return res.status(200).json({ message: "Module supprimé avec succès" });
   } catch (error: any) {
-    return res.status(500).json({ message: error.message });
+    return res
+      .status(500)
+      .json({
+        message:
+          "Le module n'a pas pu être effacé car un ou plusieurs parcours l'utilisent.",
+      });
   }
 }
