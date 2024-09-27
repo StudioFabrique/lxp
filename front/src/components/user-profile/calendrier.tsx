df8fa56cd4c1f3fc10e43e6f0541d0b6e148901f
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2024-09-27 09:40:48 +0200

    rendu conditionnel ajusté dans le profil + correction du calendrier

--- front/src/components/user-profile/calendrier.tsx
+++ front/src/components/user-profile/calendrier.tsx
@@ -1,18 +1,7 @@
-import moment from "moment";
-import { Calendar } from "react-big-calendar";
-import { momentLocalizer } from "react-big-calendar";
-
-const localizer = momentLocalizer(moment);
+import CalendarTimeline from "../student-home/calendar-timeline";
 
 const Calendrier = () => {
-  return (
-    <div className="h-[50vh] flex justify-center">
-      <Calendar
-        className="col-span-2 bg-white rounded-lg p-5"
-        localizer={localizer}
-      />
-    </div>
-  );
+  return <CalendarTimeline />;
 };
 
 export default Calendrier;
