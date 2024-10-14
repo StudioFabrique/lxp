3e53caa69d3cf7939f629066c0e21113c4f20714
CyrilPonsan <cyril.ponsan@gmail.com>
2024-10-14 10:37:13 +0200

    put password wip

--- api/src/middleware/activate-account.ts
+++ api/src/middleware/activate-account.ts
@@ -13,19 +13,17 @@ export default function activateAccount(
     const { token } = req.body;
     console.log({ token });
     if (!token) throw { satusCode: 400, message: badQuery };
-    const decodedToken = jwt.verify(
-      token,
-      process.env.REGISTER_SECRET!,
-      (err: any, data: any) => {
-        if (err) console.log(err);
-        if (data) {
-          req.auth = { userId: data.userId, userRoles: data.userRoles };
-        }
-        next();
-      },
-    );
+    jwt.verify(token, process.env.REGISTER_SECRET!, (err: any, data: any) => {
+      if (err) console.log(err.status);
+      if (data) {
+        console.log("token", data);
+        console.log("role", data.userRoles[0].label);
+        req.auth = { userId: data.userId, userRoles: data.userRoles };
+      }
+      next();
+    });
   } catch (error: any) {
     console.log({ error });
-    return res.status(500).json({ message: "dans le cul lulu" });
+    return res.status(500).json({ message: "Le lien n'est plus valide." });
   }
 }
--- api/src/models/user/put-password.ts
+++ api/src/models/user/put-password.ts
@@ -1,5 +1,20 @@
 import { hash } from "bcrypt";
+import User from "../../utils/interfaces/db/user";
+import mongoose from "mongoose";
 
 export default async function putPassword(userId: string, password: string) {
+  console.log("id", userId);
+  const existingUser = await User.findOne({
+    _id: new mongoose.Types.ObjectId(userId),
+  });
+
+  if (!existingUser)
+    throw { statusCode: 404, message: "L'utilisateur n'existe pas" };
+
   const hashedPassword = await hash(password, 10);
+  const updatedUser = await User.updateOne(
+    { _id: new mongoose.Types.ObjectId(userId) },
+    { ...existingUser, password: hashedPassword },
+  );
+  return updatedUser;
 }
