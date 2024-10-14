6432ac423dc2d0cecab623fe5306adfab981d506
CyrilPonsan <cyril.ponsan@gmail.com>
2024-10-14 11:54:23 +0200

    middleware d'activation du compte ok

--- api/src/middleware/activate-account.ts
+++ api/src/middleware/activate-account.ts
@@ -11,19 +11,19 @@ export default function activateAccount(
 ) {
   try {
     const { token } = req.body;
-    console.log({ token });
     if (!token) throw { satusCode: 400, message: badQuery };
     jwt.verify(token, process.env.REGISTER_SECRET!, (err: any, data: any) => {
-      if (err) console.log(err.status);
+      if (err)
+        throw { statusCode: 401, message: "erreur en vérifiant le token" };
       if (data) {
-        console.log("token", data);
-        console.log("role", data.userRoles[0].label);
         req.auth = { userId: data.userId, userRoles: data.userRoles };
       }
       next();
     });
   } catch (error: any) {
     console.log({ error });
-    return res.status(500).json({ message: "Le lien n'est plus valide." });
+    return res
+      .status(error.statusCode ?? 500)
+      .json({ message: "Le lien n'est plus valide." });
   }
 }
--- api/src/models/user/put-password.ts
+++ api/src/models/user/put-password.ts
@@ -3,20 +3,18 @@ import User from "../../utils/interfaces/db/user";
 import mongoose from "mongoose";
 
 export default async function putPassword(userId: string, password: string) {
-  console.log("id", userId);
   const existingUser = await User.findOne({
     _id: new mongoose.Types.ObjectId(userId),
   });
 
   if (!existingUser)
     throw { statusCode: 404, message: "L'utilisateur n'existe pas" };
-  /*
+
   const hashedPassword = await hash(password, 10);
-  const updatedUser = await User.updateOne(
-    { _id: new mongoose.Types.ObjectId(userId) },
+  const updatedUser = await User.findOneAndUpdate(
+    { _id: userId },
     { ...existingUser, password: hashedPassword },
+    { new: false },
   );
   return updatedUser;
-  */
-  return true;
 }
