c78f960b16281fa8af8cb153dc48ec18cbd3b515
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2024-10-14 09:55:40 +0200

    correction d'un probleme de modification de groupe

--- api/src/models/group/put-group.ts
+++ api/src/models/group/put-group.ts
@@ -1,12 +1,13 @@
 import Group, { IGroup } from "../../utils/interfaces/db/group";
-import { IUser } from "../../utils/interfaces/db/user";
+import User, { IUser } from "../../utils/interfaces/db/user";
+import activateMultipleUsers from "../user/activate-multiple-users";
 
 export default async function putGroup(
   id: string,
   group: IGroup,
   users: IUser[],
   image: Buffer | undefined,
-  parcoursId?: number
+  parcoursId?: number,
 ) {
   // Find the group by id
   const groupToFind = await Group.findOne({ _id: id });
@@ -14,19 +15,21 @@ export default async function putGroup(
     return null;
   }
 
+  await activateMultipleUsers(users);
+
   const updateData: any = { ...group };
 
   if (image) {
     updateData.image = image;
   }
 
-  const ids = users.map((user) => user._id);
-  updateData.users = ids;
+  const usersId = users.map((user) => user._id);
+  updateData.users = usersId;
 
   try {
     const updatedGroup = await Group.findOneAndUpdate(
       { _id: id },
-      { $set: updateData }
+      { $set: updateData },
     );
 
     if (parcoursId) {
@@ -35,6 +38,11 @@ export default async function putGroup(
         data: { parcoursId },
       });
     }
+
+    await User.updateMany(
+      { _id: { $in: usersId } },
+      { $push: { group: updatedGroup?._id } },
+    );
 
     return updatedGroup;
   } catch (error) {
