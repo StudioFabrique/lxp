e699a8a42addf008022a5c679d73bdf398ace899
CyrilPonsan <cyril.ponsan@gmail.com>
2024-10-16 10:23:42 +0200

    test smtp port

--- api/src/services/mailer.ts
+++ api/src/services/mailer.ts
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP!,
-  port: +process.env.SMTP_PORT!,
+  port: 587,
   secure: true,
   auth: {
     user: process.env.EMAIL!,
