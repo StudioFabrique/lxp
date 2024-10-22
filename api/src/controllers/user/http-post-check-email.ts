04ea19e945c1d92f89c5cec4f159ef22cb3d8aff
CyrilPonsan <cyril.ponsan@gmail.com>
2024-10-22 16:27:46 +0200

    endpoint check mail wip

---
+++ api/src/controllers/user/http-post-check-email.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from "express";
+import postCheckEmail from "../../models/user/post-check-email";
+
+export default async function httpPostCheckEmail(
+  req: Request,
+  _res: Response,
+  next: NextFunction,
+) {
+  try {
+    const { email } = req.body;
+    await postCheckEmail(email);
+    const result = {
+      statusCode: 200,
+      data: { success: true, message: "Le compte utilisateur existe !" },
+    };
+    next(result);
+  } catch (error: any) {
+    const err = {
+      statusCode: error.statusCode ?? 500,
+      message: error.message,
+    };
+    next(err);
+  }
+}
