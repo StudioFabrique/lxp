70218a257a075e170caa4044d3dc16ad18eeb006
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2024-10-23 18:18:31 +0200

    modificatiom style selectors

--- front/src/components/UI/dropdown-selector/dropdown-selector.tsx
+++ front/src/components/UI/dropdown-selector/dropdown-selector.tsx
@@ -23,62 +23,40 @@ const DropdownSelector = ({
   const dropdownRef = useRef<HTMLUListElement>(null);
 
   const handleClickButton = () => {
-    setSelecterOpenState((prevValue) => !prevValue);
+    setSelecterOpenState(true);
   };
 
   const handleSelect = (event: React.MouseEvent<HTMLLIElement>) => {
+    console.log("test");
+    console.log(event.currentTarget.value);
     onSelect(event.currentTarget.value);
     setSelecterOpenState(false);
   };
 
-  const handleClickOutside = useCallback((event: MouseEvent) => {
-    if (
-      dropdownRef.current &&
-      !dropdownRef.current.contains(event.target as Node)
-    ) {
-      setSelecterOpenState(false);
-    }
-  }, []);
-
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [handleClickOutside]);
-
   return (
-    <div className="relative">
+    <div className="dropdown dropdown-top">
       <button
         onClick={handleClickButton}
-        className="join-item btn btn-sm btn-ghost"
+        className="rounded-none btn btn-sm btn-ghost"
       >
         {children ?? "Select a value"}
       </button>
-      {
-        <ul
-          className="absolute bg-white border border-gray-300 mt-1 rounded shadow"
-          hidden={!isSelecterOpened}
-          ref={dropdownRef}
-        >
-          {valueList.length > 0 ? (
-            valueList.map((value) => (
-              <li
-                key={value}
-                value={value}
-                className="p-2 hover:bg-gray-100 cursor-pointer"
-                onClick={handleSelect}
-              >
-                {value}
-              </li>
-            ))
-          ) : (
-            <li className="p-2 hover:bg-gray-100 cursor-pointer">
-              {emptyListMessage}
+
+      <ul
+        className="menu dropdown-content bg-primary rounded-box z-[1] w-52 p-2 m-2 shadow"
+        hidden={!isSelecterOpened}
+        ref={dropdownRef}
+      >
+        {valueList.length > 0 ? (
+          valueList.map((value) => (
+            <li key={value} value={value} onClick={handleSelect}>
+              <button>{value}</button>
             </li>
-          )}
-        </ul>
-      }
+          ))
+        ) : (
+          <li>{emptyListMessage}</li>
+        )}
+      </ul>
     </div>
   );
 };
