62223680ada00009f5aff757e5075951630f9406
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2024-10-31 15:09:15 +0100

    correctif du titre d'une action

--- front/src/views/group/group-home.tsx
+++ front/src/views/group/group-home.tsx
@@ -82,7 +82,10 @@ const GroupHome = () => {
             isLoading={isLoading}
             isDisabled={!(idsList.length > 0)} // disabled si la liste a une longueur de 0
             onRefreshData={onRefreshData}
-            onDeleteUsers={onDeleteSelectedGroups}
+            delete={{
+              actionTitle: "Supprimer les groupes selectionnés",
+              onDelete: onDeleteSelectedGroups,
+            }}
           />,
           // bottom
           <TablePagination
