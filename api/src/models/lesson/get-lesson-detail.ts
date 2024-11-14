7dad86edb9e5faeb12c7840c3856e5851a34c5ff
CyrilPonsan <cyril.ponsan@gmail.com>
2024-11-14 13:58:37 +0100

    activités wip presque ok

--- api/src/models/lesson/get-lesson-detail.ts
+++ api/src/models/lesson/get-lesson-detail.ts
@@ -21,6 +21,8 @@ export default async function getLessonDetail(lessonId: number) {
           url: true,
           title: true,
           description: true,
+          createdAt: true,
+          updatedAt: true,
         },
         orderBy: { order: "asc" },
       },
