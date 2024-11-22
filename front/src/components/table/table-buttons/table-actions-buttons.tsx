831203b7bc11d01e7dfe48387ae13131488b220e
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2024-11-22 14:25:46 +0100

    nombre de groupes pour la suppression ajoutés

--- front/src/components/table/table-buttons/table-actions-buttons.tsx
+++ front/src/components/table/table-buttons/table-actions-buttons.tsx
@@ -32,7 +32,7 @@ const TableActionsButtons = (props: TableButtonsProps) => {
       {showModal ? (
         <TableActionsModal
           title="Confirmation de suppression"
-          description="Les groupes suivants vont être supprimés :"
+          description={`${descList?.length} groupes vont être supprimés :`}
           descList={descList}
           onConfirm={props.delete.onDelete}
           onCancel={handleCloseModal}
