db31ae534c9b9f94eee92b16537cd51f3665c6be
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2024-11-25 16:11:39 +0100

    ajout d'un endpoint api pour les stats du parcours et des modules destines a un affichage cote apprenant

---
+++ api/src/models/stats/get-parcours-stats.ts
@@ -0,0 +1,8 @@
+/**
+ * Récupérer les informations d'un parcours destiné à l'affichage pour l'étudiant.
+ * - Diplome du parcours
+ * - Nombre de semaines de cours du parcours
+ * - Nombre d'heures de cours dans le parcours
+ * - Nombre de modules dans le parcours
+ */
+export default async function getParcoursStats() {}
--- api/src/routes/v1/stats.router.ts
+++ api/src/routes/v1/stats.router.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import checkToken from "../../middleware/check-token";
 import httpGetLessonsStats from "../../controllers/stats/http-lessons-stats";
+import httpGetParcoursStats from "../../controllers/stats/http-parcours-stats";
 
 const statsRouter = express.Router();
 
 statsRouter.get("/lessons-read", checkToken, httpGetLessonsStats);
+statsRouter.get("/parcours", checkToken, httpGetParcoursStats);
 
 export default statsRouter;
