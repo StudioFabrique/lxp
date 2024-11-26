1857a723177b3dd80d828ad81c5587fed20b5712
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2024-11-26 10:13:08 +0100

    validation supplémentaire

--- api/src/controllers/stats/http-parcours-stats.ts
+++ api/src/controllers/stats/http-parcours-stats.ts
@@ -4,6 +4,12 @@ import getParcoursStats from "../../models/stats/get-parcours-stats";
 export default async function httpParcoursStats(req: Request, res: Response) {
   const parcoursId = parseInt(req.params.id);
 
+  if (isNaN(parcoursId)) {
+    return res.status(400).json({
+      error: "Le parcours id est incorrect",
+    });
+  }
+
   const parcours = await getParcoursStats(parcoursId);
 
   return res.json({ data: parcours });
