e3709d8ab3e9ef1b5e4c6a3f71fa9501d5519cf0
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2024-11-26 10:11:55 +0100

    les stats sont dynamiques

--- api/src/routes/v1/stats.router.ts
+++ api/src/routes/v1/stats.router.ts
@@ -2,10 +2,16 @@ import express from "express";
 import checkToken from "../../middleware/check-token";
 import httpGetLessonsStats from "../../controllers/stats/http-lessons-stats";
 import httpGetParcoursStats from "../../controllers/stats/http-parcours-stats";
+import { param } from "express-validator";
 
 const statsRouter = express.Router();
 
 statsRouter.get("/lessons-read", checkToken, httpGetLessonsStats);
-statsRouter.get("/parcours", checkToken, httpGetParcoursStats);
+statsRouter.get(
+  "/parcours/:id",
+  checkToken,
+  param("id").isMongoId().withMessage("ID de groupe invalide"),
+  httpGetParcoursStats,
+);
 
 export default statsRouter;
