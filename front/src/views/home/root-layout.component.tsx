09363faeac238d3341399c7ea0ec784f37408a8e
CyrilPonsan <cyril.ponsan@gmail.com>
2024-11-26 20:41:08 +0100

    page reload ok

--- front/src/views/home/root-layout.component.tsx
+++ front/src/views/home/root-layout.component.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useContext, useEffect } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 
 import { Context } from "../../store/context.store";
 import FadeWrapper from "../../components/UI/fade-wrapper/fade-wrapper";
@@ -13,17 +13,20 @@ const RootLayout = () => {
   const { user, fetchRoles, initTheme, isLoggedIn, handshake } =
     useContext(Context);
   const nav = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     if (isLoggedIn && user) {
       fetchRoles(user!.roles[0]);
-      if (user && user.roles[0].rank < 3) {
-        nav("/admin");
-      } else if (user && user.roles[0].rank > 2) {
-        nav("/student");
+      if (location.pathname.split("/").length < 2) {
+        if (user && user.roles[0].rank < 3) {
+          nav("/admin");
+        } else if (user && user.roles[0].rank > 2) {
+          nav("/student");
+        }
       }
     }
-  }, [fetchRoles, nav, user, isLoggedIn]);
+  }, [fetchRoles, nav, user, isLoggedIn, location.pathname]);
 
   useEffect(() => {
     initTheme();
