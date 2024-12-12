593c3de9d744c09b64d4827699a813fde65aa604
CyrilPonsan <cyril.ponsan@gmail.com>
2024-12-12 11:59:52 +0100

    mediatheque images wip

--- front/src/views/mediatheque/mediatheque-home-page.tsx
+++ front/src/views/mediatheque/mediatheque-home-page.tsx
@@ -1,7 +1,39 @@
+import { useCallback, useEffect, useState } from "react";
+import useHttp from "../../hooks/use-http";
+import CardMedia from "../../components/mediatheque/card-media";
+
 function MediathequeHomePage() {
+  const { sendRequest } = useHttp();
+  const [medias, setMedias] = useState([]);
+
+  const getData = useCallback(() => {
+    const applyData = (data: any) => {
+      setMedias(data.medias);
+    };
+    sendRequest(
+      {
+        path: "/media/images",
+      },
+      applyData
+    );
+  }, [sendRequest]);
+
+  useEffect(() => {
+    getData();
+  }, [getData]);
+
   return (
     <div>
       <h1>Mediathèque</h1>
+      {medias && medias.length > 0 ? (
+        <ul className="flex flex-wrap gap-4">
+          {medias.map((media: any) => (
+            <li key={media.id}>
+              <CardMedia media={media} />
+            </li>
+          ))}
+        </ul>
+      ) : null}
     </div>
   );
 }
