c93223fd0723ebd284f4bf64228e98fac6c5dec7
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2024-12-12 12:18:34 +0100

    correction style groupe

--- front/src/components/role/permissions-list/ressources-by-action.tsx
+++ front/src/components/role/permissions-list/ressources-by-action.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { FC } from "react";
 import PermissionItem from "./permission-item";
 
@@ -33,36 +32,38 @@ const RessourcesByAction: FC<{
   );
 
   return (
-    <div className="flex flex-col gap-y-5 items-center w-full">
-      <p className="bg-primary-focus text-secondary-content p-2 rounded-lg w-[95%] text-center font-bold">
+    <div className="flex flex-col gap-y-5 items-stretch w-full px-1">
+      <p className="bg-secondary text-secondary-content p-2 rounded-lg text-center font-semibold">
         {title}
       </p>
-      {ressources?.ressources.map((res, i) => (
-        <PermissionItem
-          key={res}
-          item={res}
-          color={i % 2 > 0}
-          roundedLeft={roundedLeft}
-          roundedRight={roundedRight}
-          isDefaultChecked={actionPermissions.includes(`${action}:${res}`)}
-          onChangePermission={(ressourceName, isChecked) =>
-            onChangePermission(ressourceName, isChecked, action)
-          }
-        />
-      ))}
-      <hr className="border-black w-[110%]" />
-      {ressources?.roles.map((res, i) => (
-        <PermissionItem
-          key={res}
-          item={res}
-          color={i % 2 > 0}
-          isDefaultChecked={actionPermissions.includes(`${action}:${res}`)}
-          disabled={disabled}
-          onChangePermission={(ressourceName, isChecked) =>
-            onChangePermission(ressourceName, isChecked, action)
-          }
-        />
-      ))}
+      <div className="flex flex-col gap-y-5 flex-1">
+        {ressources?.ressources.map((res, i) => (
+          <PermissionItem
+            key={res}
+            item={res}
+            color={i % 2 > 0}
+            roundedLeft={roundedLeft}
+            roundedRight={roundedRight}
+            isDefaultChecked={actionPermissions.includes(`${action}:${res}`)}
+            onChangePermission={(ressourceName, isChecked) =>
+              onChangePermission(ressourceName, isChecked, action)
+            }
+          />
+        ))}
+        <hr className="border-base-300 w-full" />
+        {ressources?.roles.map((res, i) => (
+          <PermissionItem
+            key={res}
+            item={res}
+            color={i % 2 > 0}
+            isDefaultChecked={actionPermissions.includes(`${action}:${res}`)}
+            disabled={disabled}
+            onChangePermission={(ressourceName, isChecked) =>
+              onChangePermission(ressourceName, isChecked, action)
+            }
+          />
+        ))}
+      </div>
     </div>
   );
 };
