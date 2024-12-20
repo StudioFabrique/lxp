53e81ba76e30c1a38d6ecc146fbd3edabd0c2bbc
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2024-12-20 15:11:56 +0100

    amélioration de la couleur lors du survol de certains éléments dans des listes

--- front/src/components/role/permissions/permission-item.tsx
+++ front/src/components/role/permissions/permission-item.tsx
@@ -7,17 +7,17 @@ type PermissionItemProps = {
 
 const PermissionItem = ({ name, onDelete }: PermissionItemProps) => {
   return (
-    <div className="flex items-center justify-between p-3 rounded-lg bg-base-100 text-base-content gap-2 hover:bg-base-200 transition-colors duration-200 cursor-pointer">
+    <div className="flex items-center justify-between p-3 rounded-lg bg-base-100 text-base-content gap-2 hover:bg-base-100/60 transition-colors duration-200 cursor-pointer">
       <div className="flex items-center gap-2">
         <Shield className="w-5 h-5 stroke-warning" />
         <p className="font-medium text-base-content">{name}</p>
       </div>
       <button
         onClick={onDelete}
-        className="p-2 hover:text-error-content transition-all duration-200"
+        className="p-2 "
         aria-label="Delete permission"
       >
-        <MinusCircle className="w-5 stroke-error" />
+        <MinusCircle className="w-5 stroke-error hover:stroke-error-content transition-all duration-200" />
       </button>
     </div>
   );
