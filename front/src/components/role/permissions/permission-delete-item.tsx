382746a13cf19cdadf0ac3bcb5f615054c92f54e
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2025-01-03 11:22:21 +0100

    ajout tooltip item delete perms

--- front/src/components/role/permissions/permission-delete-item.tsx
+++ front/src/components/role/permissions/permission-delete-item.tsx
@@ -2,22 +2,32 @@ import { Shield, MinusCircle } from "lucide-react";
 
 type PermissionItemProps = {
   name: string;
+  description?: string;
   onDeleteItem: (name: string) => void;
 };
 
-const PermissionDeleteItem = ({ name, onDeleteItem }: PermissionItemProps) => {
+const PermissionDeleteItem = ({
+  name,
+  description,
+  onDeleteItem,
+}: PermissionItemProps) => {
   const handleClick = () => {
     onDeleteItem(name);
   };
 
   return (
-    <div className="flex items-center justify-between px-2 py-1 rounded-md bg-base-100 text-base-content gap-2 hover:bg-base-100/60 transition-colors duration-200 cursor-pointer">
+    <div className="group relative flex items-center justify-between px-2 py-1 rounded-md bg-base-100 text-base-content gap-2 hover:bg-base-100/60 transition-colors duration-200 cursor-pointer">
       <div className="flex items-center gap-1">
         <Shield className="w-4 h-4 stroke-warning" />
         <p className="font-medium text-sm text-base-content capitalize">
           {name}
         </p>
       </div>
+      {description && (
+        <div className="z-10 absolute invisible group-hover:visible opacity-0 group-hover:opacity-100 transition-all duration-200 top-full mt-2 left-0 bg-neutral text-neutral-content px-3 py-2 rounded text-sm whitespace-normal shadow-lg w-64">
+          {description}
+        </div>
+      )}
       <button
         onClick={handleClick}
         className="p-1"
