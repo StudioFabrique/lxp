72aa17f2dfae15fb0321afa766ec493b6fcf2b98
CyrilPonsan <cyril.ponsan@gmail.com>
2025-01-15 13:47:32 +0100

    email envoyé à la bonne adresse après modifications des données personnelles ok

--- api/src/helpers/get-mail-template.ts
+++ api/src/helpers/get-mail-template.ts
@@ -7,6 +7,10 @@ export const getTemplate = (template: string, token: string) => {
     case "reset":
       link = `${process.env.FRONT_URL}reset-update?id=${token}`;
       return `<b>Bonjour, vous avez demandé la réinitialisation de votre mot de passe, pour enregistrer votre nouveau mot de passe veuillez suivre ce lien.</b><br/><a href=${link}>Lien pour réinitialiser votre mot de passe</a><br/><p>A bientôt !</p>`;
+    case "updated-user":
+      return `<b>Bonjour, votre compte a été mis à jour avec succès.</b><br/><p>Si vous n'êtes pas à l'origine de la demande de modification, veuillez contacter un administrateur.</p>
+      <br/>
+      <p>A bientôt !</p>`;
     default:
       break;
   }
