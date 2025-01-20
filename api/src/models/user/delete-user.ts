ad15151892bf62450853f86c007d327fee3aaf27
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2025-01-20 14:18:57 +0100

    le calendrier peut etre desormais affiché en tant que admin

--- api/src/models/user/delete-user.ts
+++ api/src/models/user/delete-user.ts
@@ -3,7 +3,6 @@ import User from "../../utils/interfaces/db/user";
 
 export default async function deleteUser(userId: string) {
   await prisma.student.deleteMany({ where: { idMdb: userId } });
-  await prisma.teacher.deleteMany({ where: { idMdb: userId } });
   await prisma.admin.deleteMany({ where: { idMdb: userId } });
 
   await User.deleteOne().where({ _id: userId });
