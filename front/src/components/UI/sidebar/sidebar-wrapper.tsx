6cd4a25bdf3b9969eacd5c9dbcc08f9e02ceec94
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2025-02-11 15:12:26 +0100

    suppression placeholder temporaire logo sur branche dev

--- front/src/components/UI/sidebar/sidebar-wrapper.tsx
+++ front/src/components/UI/sidebar/sidebar-wrapper.tsx
@@ -1,18 +1,18 @@
 import { ReactNode, useContext } from "react";
 import { Context } from "../../../store/context.store";
-import logo from "./logo.png";
+// import logo from "./logo.png";
 
 const SidebarWrapper = ({ children }: { children: ReactNode }) => {
   const { theme } = useContext(Context);
 
   return (
     <nav className="w-24 h-screen fixed p-4 z-10 flex flex-col gap-2">
-      <div className="w-full bg-slate-100 rounded-lg">
+      {/* <div className="w-full bg-slate-100 rounded-lg">
         <img
           className="h-full w-full object-contain rounded-lg border-slate-700 border-2 p-1"
           src={logo}
         />
-      </div>
+      </div> */}
       <div
         className={`flex flex-col gap-y-4 p-4 pt-6 rounded-lg h-full relative ${
           theme === "dark"
