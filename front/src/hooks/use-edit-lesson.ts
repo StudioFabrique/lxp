897e403659ec63e52d4ecfc7d3f45914892d1666
CyrilPonsan <cyril.ponsan@gmail.com>
2025-02-12 10:55:57 +0100

    edition de lecon ok

--- front/src/hooks/use-edit-lesson.ts
+++ front/src/hooks/use-edit-lesson.ts
@@ -3,8 +3,15 @@ import { regexGeneric, regexOptionalGeneric } from "../utils/constantes";
 import Lesson from "../utils/interfaces/lesson";
 import useInput from "./use-input";
 import Tag from "../utils/interfaces/tag";
+import useHttp from "./use-http";
+import toast from "react-hot-toast";
+import { useNavigate, useParams } from "react-router-dom";
 
 const useEditLesson = () => {
+  const { lessonId } = useParams<{ lessonId: string }>();
+  const [lesson, setLesson] = useState<Lesson | null>(null);
+  const { sendRequest, error, isLoading } = useHttp();
+  const navigate = useNavigate();
   const { value: title, newProps: newTitle } = useInput((value) =>
     regexGeneric.test(value)
   );
@@ -26,7 +33,49 @@ const useEditLesson = () => {
     [newTitle, newDescription]
   );
 
+  const handleUpdateLesson = () => {
+    const applyData = (data: Lesson) => {
+      console.log({ data });
+      navigate(`/admin/lesson`);
+      toast.success("Leçon mise à jour");
+    };
+    sendRequest(
+      {
+        path: `/lesson/update`,
+        method: "put",
+        body: {
+          id: lesson!.id,
+          title: title.value,
+          description: description.value,
+          tagId: tag!.id,
+          modalite: mode,
+        },
+      },
+      applyData
+    );
+  };
+
+  const getLesson = useCallback(() => {
+    const applyData = (data: { success: boolean; lesson: Lesson }) => {
+      setLesson(data.lesson);
+      setLessonValues(data.lesson);
+    };
+    sendRequest({ path: `/lesson/edit/${lessonId}` }, applyData);
+  }, [lessonId, sendRequest, setLessonValues]);
+
+  useEffect(() => {
+    getLesson();
+  }, [getLesson]);
+
+  useEffect(() => {
+    console.log("toto");
+
+    if (error.length > 0) toast.error(error);
+  }, [error]);
+
   return {
+    lesson,
+    isLoading,
     title,
     description,
     mode,
@@ -35,6 +84,7 @@ const useEditLesson = () => {
     setTag,
     tagsList,
     setLessonValues,
+    handleUpdateLesson,
   };
 };
 
