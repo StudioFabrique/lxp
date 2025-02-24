e203139d95caca6d68ed848fc76b2b15982a71b1
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2025-02-24 15:35:44 +0100

    calendrier des modules des parcours mis a jour

--- front/src/components/UI/sidebar/sidebar-parts/roles.tsx
+++ front/src/components/UI/sidebar/sidebar-parts/roles.tsx
@@ -10,20 +10,22 @@ const Roles = ({ currentRoute }: { currentRoute: string[] }) => {
     <Can action="write" object="role">
       <li>
         <div className="flex items-center">
-          <Link
-            to={`/${currentRoute[0]}/roles`}
-            className="tooltip tooltip-right w-6 h-6 z-10"
-            data-tip="Roles et permissions"
-          >
-            <div className="flex hover justify-center items-center">
-              <UserCheck />
-              <span
-                className={`absolute p-5 rounded-lg hover:bg-primary/50 ${
-                  isCurrentPathActive && "bg-primary/50"
-                }`}
-              />
-            </div>
-          </Link>
+          <Can action="read" object="user">
+            <Link
+              to={`/${currentRoute[0]}/roles`}
+              className="tooltip tooltip-right w-6 h-6 z-10"
+              data-tip="Roles et permissions"
+            >
+              <div className="flex hover justify-center items-center">
+                <UserCheck />
+                <span
+                  className={`absolute p-5 rounded-lg hover:bg-primary/50 ${
+                    isCurrentPathActive && "bg-primary/50"
+                  }`}
+                />
+              </div>
+            </Link>
+          </Can>
         </div>
       </li>
     </Can>
