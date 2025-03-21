ceec27b794175ccf574e76d39fe40b5e60a9d007
CyrilPonsan <cyril.ponsan@gmail.com>
2025-03-21 11:12:33 +0100

    Update http-delete-activity.old.ts

--- api/src/controllers/activity/http-delete-activity.old.ts
+++ api/src/controllers/activity/http-delete-activity.old.ts
@@ -1,17 +1,19 @@
 import { Request, Response } from "express";
 import { serverIssue } from "../../utils/constantes";
-import deleteActivity from "../../models/activity/delete-activity.old";
+//import deleteActivity from "../../models/activity/delete-activity.old";
 
 export default async function httpDeleteActivity(req: Request, res: Response) {
-  try {
-    const { activityId } = req.params;
-    const response = await deleteActivity(+activityId);
-    return res.status(200).json(response);
-  } catch (error: any) {
-    console.log({ error });
-
-    return res
-      .status(error.statusCode ?? 500)
-      .json({ message: error.message ?? serverIssue });
-  }
+  /* try {
+     const { activityId } = req.params;
+     const response = await deleteActivity(+activityId);
+     return res.status(200).json(response);
+   } catch (error: any) {
+     console.log({ error });
+ 
+     return res
+       .status(error.statusCode ?? 500)
+       .json({ message: error.message ?? serverIssue });
+   }
+ }*/
+  return;
 }
