e7006473425be934c3985a47a95bc02c1780f508
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2025-03-25 14:31:37 +0100

    amélioration des tooltips

--- front/src/components/lessons-preview/sidebar/course-item.tsx
+++ front/src/components/lessons-preview/sidebar/course-item.tsx
@@ -53,51 +53,57 @@ const CourseItem = ({
       >
         <div className="bg-secondary/80 p-4 rounded-xl flex flex-col gap-4">
           {/* Titre du cours + tooltip */}
-          <div className="capitalize">
-            <div className="relative">
-              <span
-                data-tip={`Titre : ${course.title}`}
-                className="w-full h-10 tooltip tooltip-right absolute"
-              />
-            </div>
-            <div className="flex justify-between items-center gap-1">
-              <div className="truncate">
-                <h3 className="text-secondary-content/80">{course.title}</h3>
-              </div>
 
-              <div className="flex gap-2">
-                <Can action="update" object="lesson">
-                  <div
-                    className="tooltip"
-                    data-tip="Modifier les informations du cours"
+          <div className="flex justify-between items-center gap-1 min-w-0">
+            <span
+              data-tip={`Titre : ${course.title}`}
+              className="flex items-center w-[75%] h-12 tooltip tooltip-right capitalize min-w-0"
+            >
+              <h3 className="text-secondary-content/80 capitalize truncate min-w-0">
+                {course.title}
+              </h3>
+            </span>
+
+            <div className="flex gap-2 flex-shrink-0">
+              <Can action="update" object="lesson">
+                <div
+                  className="tooltip"
+                  data-tip="Modifier les informations du cours"
+                >
+                  <Link to={`/admin/course/edit/${course.id}`}>
+                    <Edit className="stroke-base-100 w-7 h-7 hover:bg-primary px-1 rounded-lg" />
+                  </Link>
+                </div>
+              </Can>
+              <Can action="write" object="lesson">
+                <div className="tooltip" data-tip="Ajouter une leçon">
+                  <Link
+                    to="/admin/lesson/add"
+                    // state={{ parcoursId, moduleId, courseId }}
                   >
-                    <Link to={`/admin/course/edit/${course.id}`}>
-                      <Edit className="stroke-base-100 w-7 h-7 hover:bg-primary px-1 rounded-lg" />
-                    </Link>
-                  </div>
-                </Can>
-                <Can action="write" object="lesson">
-                  <div className="tooltip" data-tip="Ajouter une leçon">
-                    <Link
-                      to="/admin/lesson/add"
-                      // state={{ parcoursId, moduleId, courseId }}
-                    >
-                      <ListPlus className="stroke-base-100 w-7 h-7 hover:bg-primary px-1 rounded-lg" />
-                    </Link>
-                  </div>
-                </Can>
-              </div>
+                    <ListPlus className="stroke-base-100 w-7 h-7 hover:bg-primary px-1 rounded-lg" />
+                  </Link>
+                </div>
+              </Can>
             </div>
           </div>
-          <div className="flex justify-between gap-5">
-            <p className="text-secondary-content font-semibold text-sm w-[80%] max-h-10 break-words overflow-y-clip">
-              {course.description}
-            </p>
-            {isCourseOpen ? (
-              <ArrowDown className="stroke-primary-content" />
-            ) : (
-              <ArrowRight className="stroke-primary-content" />
-            )}
+
+          <div className="flex justify-between items-center gap-5 pr-2 min-w-0">
+            <span
+              data-tip={`Description : ${course.description}`}
+              className="tooltip tooltip-right flex-1 min-w-0"
+            >
+              <p className="text-secondary-content font-semibold text-sm w-[80%] max-h-10 break-words overflow-y-clip min-w-0">
+                {course.description}
+              </p>
+            </span>
+            <div className="flex-shrink-0 ">
+              {isCourseOpen ? (
+                <ArrowDown className="stroke-primary-content" />
+              ) : (
+                <ArrowRight className="stroke-primary-content" />
+              )}
+            </div>
           </div>
         </div>
         <Can action="component" object="progression">
