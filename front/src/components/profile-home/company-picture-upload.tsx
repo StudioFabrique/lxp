02ff55535d6c0c7f1a46c969acd0142158962b13
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2025-04-03 09:57:26 +0200

    correction liste de themes

--- front/src/components/profile-home/company-picture-upload.tsx
+++ front/src/components/profile-home/company-picture-upload.tsx
@@ -2,14 +2,21 @@ import ProfileImageFileUpload from "../UI/image-file-upload/profile-image-file-u
 
 const CompanyPictureUpload = () => {
   return (
-    <div>
-      <p>Téléverser une image de l'entreprise/organisame de formation</p>
-      <ProfileImageFileUpload
-        temporaryAvatar={{ file: null, url: null }}
-        onSetTemporaryAvatar={() => {}}
-        maxSize={100}
-        existingAvatar={""}
-      />
+    <div className="card bg-base-200 shadow-lg hover:shadow-xl transition-shadow duration-300">
+      <div className="card-body flex flex-col gap-4 p-6 rounded-lg">
+        <p className="text-base-content text-lg font-medium">
+          Téléverser un nouveau logo de l'organisme de formation
+        </p>
+        <ProfileImageFileUpload
+          temporaryAvatar={{ file: null, url: null }}
+          onSetTemporaryAvatar={() => {}}
+          maxSize={100}
+          existingAvatar={""}
+        />
+        <p className="ml-2 text-sm text-gray-600">
+          Formats acceptés : .png, .jpg
+        </p>
+      </div>
     </div>
   );
 };
