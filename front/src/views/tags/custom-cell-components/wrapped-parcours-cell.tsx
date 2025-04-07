97977368ce90e50c559c00ccae17bd9e9e36a092
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2025-04-07 18:54:01 +0200

    liste tags complete

--- front/src/views/tags/custom-cell-components/wrapped-parcours-cell.tsx
+++ front/src/views/tags/custom-cell-components/wrapped-parcours-cell.tsx
@@ -1,22 +1,26 @@
 import { Link } from "react-router-dom";
 
 export type WrappedParcoursCellProps = {
-  data: { id: number; name: string }[];
+  data: { id: number; title: string }[];
 };
 
 const WrappedParcoursCell = ({ data }: WrappedParcoursCellProps) => {
   return (
     <td className="p-2">
       <div className="flex flex-wrap gap-2">
-        {data.map((p) => (
-          <Link
-            key={p.id}
-            to={`/admin/parcours/view/${p.id}`}
-            className="badge badge-primary badge-outline p-3 hover:badge-primary transition-colors"
-          >
-            {p.name}
-          </Link>
-        ))}
+        {data.length > 0 ? (
+          data.map((p) => (
+            <Link
+              key={p.id}
+              to={`/admin/parcours/view/${p.id}`}
+              className="badge badge-primary badge-outline p-3 hover:badge-primary transition-colors"
+            >
+              {p.title}
+            </Link>
+          ))
+        ) : (
+          <span className="w-full">-</span>
+        )}
       </div>
     </td>
   );
