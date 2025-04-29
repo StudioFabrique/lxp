503f399e736699350a5538d36605f614ba42511c
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2025-04-29 14:20:42 +0200

    changement de place des boutons et réalisation du nouveau bouton d'insertion d'image

--- front/src/components/lessons-preview/writing/tiptap-simple-editor/components/EditYoutubeLinkPopover.tsx
+++ front/src/components/lessons-preview/writing/tiptap-simple-editor/components/EditYoutubeLinkPopover.tsx
@@ -4,17 +4,20 @@ import { Icon } from "./ui/Icon";
 import { YoutubeLinkEditorPanel } from "./YoutubeLinkEditorPanel";
 
 export type EditYoutubeLinkPopoverProps = {
+  title?: string;
   onSetLink: (url: string, size?: "small" | "medium" | "large") => void;
 };
 
 export const EditYoutubeLinkPopover = ({
+  title,
   onSetLink,
 }: EditYoutubeLinkPopoverProps) => {
   return (
     <Popover.Root>
       <Popover.Trigger asChild>
-        <Toolbar.Button tooltip="Insérer une vidéo Youtube">
+        <Toolbar.Button className="flex items-center gap-3 p-1.5 text-sm font-medium text-neutral-500 dark:text-neutral-400 text-left bg-transparent w-full rounded hover:bg-neutral-100 hover:text-neutral-800 dark:hover:bg-neutral-900 dark:hover:text-neutral-200">
           <Icon name="Youtube" />
+          {title}
         </Toolbar.Button>
       </Popover.Trigger>
       <Popover.Content>
