7d28aad96482ab16177de439deca419b8d474d0e
CyrilPonsan <cyril.ponsan@gmail.com>
2025-07-31 16:01:08 +0200

    test ??

--- api/src/utils/services/db/mongo-connect.ts
+++ api/src/utils/services/db/mongo-connect.ts
@@ -10,8 +10,6 @@ const MONGO_URL = process.env.MONGO_LOCAL_URL;
 
 mongoose.connection.once("open", () => {
   console.log("MongoDB connection ready!");
-  console.log("ENVIRONMENT", process.env.NODE_ENV);
-  console.log("ENVIRONMENT", MONGO_URL);
 });
 
 mongoose.connection.on("error", (err) => {
@@ -19,8 +17,6 @@ mongoose.connection.on("error", (err) => {
 });
 
 async function mongoConnect() {
-  console.log(MONGO_URL);
-
   await mongoose.connect(MONGO_URL!);
 }
 
