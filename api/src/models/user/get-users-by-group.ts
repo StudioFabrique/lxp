41db7e3124cb1c168f507a61003a214934aace52
CyrilPonsan <cyril.ponsan@gmail.com>
2025-09-09 12:37:48 +0200

    formulaire new resource ok

--- api/src/models/user/get-users-by-group.ts
+++ api/src/models/user/get-users-by-group.ts
@@ -3,16 +3,36 @@ import mongoose from "mongoose";
 
 async function getUsersByGroup(groupsIds: string[]) {
   const ids = groupsIds.map(
-    (item: string) => new mongoose.Types.ObjectId(item),
+    (item: string) => new mongoose.Types.ObjectId(item)
   );
 
-  const group = await Group.find({ _id: { $in: ids } }).populate("users", {});
+  const group = await Group.find({ _id: { $in: ids } }).populate("users", {
+    _id: 1,
+    firstname: 1,
+    lastname: 1,
+    avatar: 1,
+  });
 
   if (!group) {
     const error = { message: "Groupe inexistant", statusCode: 404 };
     throw error;
   } else {
-    return group;
+    let updatedGroups: any = [];
+    for (const item of group) {
+      updatedGroups = [
+        ...updatedGroups,
+        {
+          _id: item._id,
+          name: item.name,
+          users: item.users.map((user: any) => ({
+            ...user.toObject(),
+            avatar: user.avatar ? user.avatar.toString("base64") : null,
+          })),
+        },
+      ];
+    }
+
+    return updatedGroups;
   }
 }
 
