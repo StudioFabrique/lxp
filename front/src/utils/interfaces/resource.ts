1e9e4f2ad8a5b50533bdc58eb6eeaf623b213d63
CyrilPonsan <cyril.ponsan@gmail.com>
2025-09-10 15:41:43 +0200

    mise a jour ok ? a verifier une fois qu'on affichera la liste des activités

--- front/src/utils/interfaces/resource.ts
+++ front/src/utils/interfaces/resource.ts
@@ -5,4 +5,18 @@ export default interface Resource {
   title: string;
   description: string;
   tags: Tag[];
+  bonusActivities: bonusActivity[];
+  imageUrl: string | null;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface bonusActivity {
+  id: number;
+  title: string;
+  description: string;
+  url: string;
+  order: number;
+  createdAt: string;
+  updatedAt: string;
 }
