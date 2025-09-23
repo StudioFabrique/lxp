5ec3073ce7e3288e20246b4a3506513fdd6e3a30
CyrilPonsan <cyril.ponsan@gmail.com>
2025-09-23 11:38:49 +0200

    probleme d'affichage des données ok

--- api/src/models/user/edit-user.ts
+++ api/src/models/user/edit-user.ts
@@ -7,55 +7,43 @@ export default async function editUser(
   user: IUser,
   roleId: string
 ) {
-  try {
-    // Vérifier si l'utilisateur existe
-    const userToUpdate = await User.findOne({ _id: userId });
-    if (!userToUpdate) {
-      throw {
-        statusCode: 404,
-        message: "Utilisateur non trouvé.",
-      };
-    }
-
-    // Vérifier si le rôle existe
-    const newRole = await Role.findOne({ _id: roleId });
-    if (!newRole) {
-      throw { statusCode: 404, message: "Le rôle n'existe pas." };
-    }
-
-    // Mettre à jour l'utilisateur dans MongoDB
-    const updatedUser = await User.findByIdAndUpdate(
-      userId,
-      {
-        email: user.email.toLowerCase(),
-        firstname: user.firstname.toLowerCase(),
-        lastname: user.lastname.toLowerCase(),
-        nickname: user.nickname?.toLowerCase(),
-        description: user.description?.toLowerCase(),
-        address: user.address?.toLowerCase(),
-        city: user.city?.toLowerCase(),
-        postCode: user.postCode?.toLowerCase(),
-        birthDate: user.birthDate,
-        phoneNumber: user.phoneNumber?.toLowerCase(),
-        avatar: user.avatar,
-        roles: newRole,
-      },
-      { new: true }
-    );
-
-    await sendUpdatedUserEmail(userToUpdate.email);
-
-    if (!updatedUser) {
-      throw {
-        statusCode: 404,
-        message: "Impossible de mettre à jour l'utilisateur.",
-      };
-    }
-
-    // Retourner l'utilisateur mis à jour et le rang du rôle
-    return { updatedUser, role: newRole.rank };
-  } catch (error) {
-    // Gérer les erreurs en les relayant
-    throw error;
+  // Vérifier si l'utilisateur existe
+  const userToUpdate = await User.findOne({ _id: userId }).populate("roles");
+  if (!userToUpdate) {
+    throw {
+      statusCode: 404,
+      message: "Utilisateur non trouvé.",
+    };
   }
+
+  // Vérifier si le rôle existe
+  const newRole = await Role.findOne({ _id: roleId });
+  if (!newRole) {
+    throw { statusCode: 404, message: "Le rôle n'existe pas." };
+  }
+
+  // Mettre à jour l'utilisateur dans MongoDB
+  const updatedUser = await User.findByIdAndUpdate(
+    userId,
+    {
+      email: user.email.toLowerCase(),
+      firstname: user.firstname.toLowerCase(),
+      lastname: user.lastname.toLowerCase(),
+      nickname: user.nickname?.toLowerCase(),
+      description: user.description?.toLowerCase(),
+      address: user.address?.toLowerCase(),
+      city: user.city?.toLowerCase(),
+      postCode: user.postCode?.toLowerCase(),
+      birthDate: user.birthDate,
+      phoneNumber: user.phoneNumber?.toLowerCase(),
+      avatar: user.avatar,
+      roles: newRole,
+    },
+    { new: true }
+  );
+
+  await sendUpdatedUserEmail(userToUpdate.email);
+
+  // Retourner l'utilisateur mis à jour et le rang du rôle
+  return { updatedUser, role: newRole.rank };
 }
