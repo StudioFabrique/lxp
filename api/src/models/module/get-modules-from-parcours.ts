e79452f74e58e560744388e0be6337362fb249df
CyrilPonsan <cyril.ponsan@gmail.com>
2025-10-02 15:17:44 +0200

    avatar details user ok

--- api/src/models/module/get-modules-from-parcours.ts
+++ api/src/models/module/get-modules-from-parcours.ts
@@ -4,11 +4,20 @@ async function getModulesFromParcours(parcoursId: number) {
   const existingParcours = await prisma.parcours.findUnique({
     where: { id: +parcoursId },
     select: {
-      modules: true,
+      modules: { select: { id: true, module: { select: { title: true } } } },
     },
   });
 
-  return existingParcours?.modules ?? [];
+  if (!existingParcours)
+    throw { statusCode: 404, message: "Parcours introuvable." };
+
+  const modules =
+    existingParcours?.modules.map((mod) => ({
+      id: mod.id,
+      title: mod.module.title,
+    })) ?? [];
+
+  return modules;
 }
 
 export default getModulesFromParcours;
