342e26fa724f411cc985797037ebf066c3192475
CyrilPonsan <cyril.ponsan@gmail.com>
2025-10-08 15:01:50 +0200

    objectifs et compétences retirés de l'interface de création de cours

--- front/src/components/edit-course/preview/hook/use-validate-course.tsx
+++ front/src/components/edit-course/preview/hook/use-validate-course.tsx
@@ -1,8 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useSelector } from "react-redux";
 import Course from "../../../../utils/interfaces/course";
-import Objective from "../../../../utils/interfaces/objective";
-import Skill from "../../../../utils/interfaces/skill";
 import Lesson from "../../../../utils/interfaces/lesson";
 import CourseDates from "../../../../utils/interfaces/course-dates";
 import { testStep } from "../../../../helpers/course-steps-validation";
@@ -12,12 +10,6 @@ const useValidateCourse = () => {
   const course = useSelector(
     (state: any) => state.courseInfos.course
   ) as Course;
-  const objectives = useSelector(
-    (state: any) => state.courseObjectives.courseObjectives
-  ) as Objective[];
-  const skills = useSelector(
-    (state: any) => state.courseSkills.courseSkills
-  ) as Skill[];
   const lessons = useSelector(
     (state: any) => state.courseScenario.courseLessons
   ) as Lesson[];
@@ -31,12 +23,10 @@ const useValidateCourse = () => {
       description: course.description,
       tags: course.tags,
       contacts: course.contacts,
-      objectives: objectives,
-      skills: skills,
       lessons: lessons,
       dates: dates,
     });
-  }, [course, objectives, lessons, dates, skills]);
+  }, [course, lessons, dates]);
 
   return { validateCourse };
 };
