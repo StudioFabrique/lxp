a37c83f756b4bb550b80bba1fe235ef07f536c81
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2025-11-18 11:27:47 +0100

    correction de style

--- front/src/components/parcours-view/quick-statistiques/quick-stat-item.tsx
+++ front/src/components/parcours-view/quick-statistiques/quick-stat-item.tsx
@@ -4,7 +4,7 @@ type QuickStatItemProps = {
 };
 
 const QuickStatItem = ({ title, item }: QuickStatItemProps) => (
-  <div className="bg-secondary card w-full h-24 flex flex-col justify-center items-center text-base-100">
+  <div className="bg-secondary card w-full h-24 flex flex-col justify-center items-center text-base-content">
     <div className="card-body text-center">
       <h2 className="card-title justify-center text-xl font-bold">{title}</h2>
       <p className="text-lg font-bold">{item}</p>
