ec4966fd2823e9735f8829433aa8c113772a2480
CyrilPonsan <91895995+CyrilPonsan@users.noreply.github.com>
2025-11-19 14:20:38 +0100

    wip

--- front/src/views/module/add/module-add.tsx
+++ front/src/views/module/add/module-add.tsx
@@ -1,15 +1,14 @@
 //import { useRef } from "react";
 import Header from "../../../components/UI/header";
 import Wrapper from "../../../components/UI/wrapper/wrapper.component";
-import ModuleMetadatas from "./module-metadatas";
-import Selecter from "../../../components/UI/selecter/selecter.component";
-import useNewModule from "./use-new-module";
-import ModuleToParcours from "./module-to-parcours";
+import useNewModule from "./useAddModule";
 import { useMemo } from "react";
-import FieldNumber from "../../../components/UI/forms/field-number";
 import defaultImage from "../../../assets/images/module-default.jpg";
 import bgImageGradient from "../../../utils/bg-image-gradient";
 import TwoButtonsModal from "../../../components/UI/modal/two-buttons-modal";
+import ModuleCreateForm from "../../../components/module-add/ModuleCreateForm";
+import AssociateToParcours from "../../../components/module-add/AssociateToParcours";
+import ButtonButton from "../../../components/module-add/ButtonButton";
 
 export default function ModuleAdd() {
   const {
@@ -85,112 +84,45 @@ export default function ModuleAdd() {
         <Wrapper>
           <div className="grid grid-cols-1 lg:grid-cols-13 gap-2">
             <span className="col-span-5 flex flex-col gap-y-4">
-              <h2 className="text-sm">
-                Choisissez une formation à laquelle attacher le module
-              </h2>
-              <Selecter
-                defaultItem={{ id: formationId ?? 0, title: "" }}
-                list={formationList}
-                title="Choisissez une formation"
-                onSelectItem={handlePickFormation}
+              <ModuleCreateForm
+                formationId={formationId}
+                formationList={formationList}
+                data={data}
+                onSubmit={handleSubmit}
+                onPickFormation={handlePickFormation}
+                onSetFile={handleSetFile}
+                setImageBase64={setImageBase64}
+                toggleModal={toggleModal}
+                newModuleData={newModuleData}
               />
-
-              <form onSubmit={handleSubmit} ref={null}>
-                <ModuleMetadatas
-                  data={data}
-                  thumb={null}
-                  onSetFile={handleSetFile}
-                  mode="create"
-                  onSetImageBase64={setImageBase64}
-                />
-                <div className="flex justify-end">
-                  <span className="flex items-center gap-x-4 mt-4">
-                    <button
-                      className="btn btn-outline btn-secondary"
-                      type="button"
-                      onClick={toggleModal}
-                      disabled={!!newModuleData}
-                    >
-                      Annuler
-                    </button>
-                    <button
-                      className="btn btn-primary"
-                      type="submit"
-                      disabled={!!newModuleData}
-                    >
-                      Enregistrer
-                    </button>
-                  </span>
-                </div>
-              </form>
             </span>
             <div className="col-span-1 lg:col-span-0 divider sm:my-auto lg:hidden" />
             <div className="col-span-1 divider divider-horizontal lg:mx-auto hidden lg:divider " />
             <span className="col-span-6 mx-auto flex flex-col gap-y-4">
               {showMetadataForm && newModuleData && parcoursList ? (
-                <>
-                  <h2 className="text-sm">
-                    Choisissez un parcours auquel attacher le module afin de
-                    pouvoir mettre à jour les compétences et ressources
-                    pédagogiques liées
-                  </h2>
-                  <Selecter
-                    defaultItem={{ id: parcoursId ?? 0, title: "" }}
-                    list={parcoursList}
-                    title="Choisissez un parcours auquel attacher le module"
-                    onSelectItem={handlePickParcours}
-                  />
-                  <FieldNumber
-                    label="Durée du module en heures *"
-                    name="duration"
-                    placeholder="Ex : 12"
-                    min={0}
-                    data={data}
-                  />
-                  <ModuleToParcours
-                    currentContacts={currentContacts ?? []}
-                    currentSkills={currentSkills ?? []}
-                    contacts={contacts ?? []}
-                    skills={skills ?? []}
-                    isLoading={isLoading}
-                    setCurrentContacts={setCurrentContacts}
-                    setCurrentSkills={setCurrentSkills}
-                    isDisabled={!parcoursId}
-                  />
-                  <div className="flex justify-end gap-x-4">
-                    <button
-                      className="btn btn-outline btn-secondary"
-                      onClick={toggleModal}
-                    >
-                      Annuler
-                    </button>
-                    <button
-                      className="btn btn-primary"
-                      disabled={cantSubmit}
-                      type="button"
-                      onClick={handleMetadataSubmit}
-                    >
-                      Enregistrer
-                    </button>
-                  </div>
-                </>
+                <AssociateToParcours
+                  parcoursId={parcoursId}
+                  parcoursList={parcoursList}
+                  onPickParcours={handlePickParcours}
+                  data={data}
+                  contacts={contacts}
+                  currentContacts={currentContacts}
+                  skills={skills}
+                  currentSkills={currentSkills}
+                  isLoading={isLoading}
+                  onMetadataSubmit={handleMetadataSubmit}
+                  setCurrentContacts={setCurrentContacts}
+                  setCurrentSkills={setCurrentSkills}
+                  toggleModal={toggleModal}
+                  cantSubmit={cantSubmit}
+                />
               ) : (
                 <div className="h-full flex justify-center items-center gap-x-4">
-                  <button
-                    className="btn btn-secondary"
-                    type="button"
-                    onClick={toggleModal}
-                  >
-                    Retour à la liste des modules
-                  </button>
-                  <button
-                    className="btn btn-info"
-                    disabled={!newModuleData}
-                    type="button"
-                    onClick={toggleShowMetadataForm}
-                  >
-                    Attacher à un parcours
-                  </button>
+                  <ButtonButton
+                    toggleModal={toggleModal}
+                    toggleShowMetadataForm={toggleShowMetadataForm}
+                    newModuleData={newModuleData}
+                  />
                 </div>
               )}
             </span>
