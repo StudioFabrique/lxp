38ce9942ed0e5eeeafa0cd255ffff8363783e1e0
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2025-11-24 16:30:06 +0100

    suppression icon eye

--- front/src/views/module-content-explorer/hooks/use-module-explorer.tsx
+++ front/src/views/module-content-explorer/hooks/use-module-explorer.tsx
@@ -20,8 +20,9 @@ const useModuleExplorerContent = () => {
   // Params et states de la route active
   // ------------
   const { moduleId } = useParams();
-  const { state: stateFromUrl }: { state: { lessonId?: number } } =
-    useLocation();
+  const {
+    state: stateFromUrl,
+  }: { state: { lessonId?: number; activityId?: number } } = useLocation();
   const [stateFromUrlCalled, setStateFromUrlCalled] = useState(false);
   // ------------
   const navigate = useNavigate();
