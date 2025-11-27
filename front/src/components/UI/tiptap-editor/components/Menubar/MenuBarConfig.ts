fbfa831b207ba5b6e18f819a339612c11a97824b
Martin Dhollande <martin.dhlle@gmail.com>
2025-11-27 11:36:31 +0100

    ajout classes manquantes

--- front/src/components/UI/tiptap-editor/components/Menubar/MenuBarConfig.ts
+++ front/src/components/UI/tiptap-editor/components/Menubar/MenuBarConfig.ts
@@ -1,7 +1,7 @@
 export const TIPTAP_MENU_BAR_COLORS = {
   background: "bg-slate-200",
-  backgroundActive: "bg-slate-600",
-  backgroundSelected: "bg-slate-600",
+  backgroundActive: "bg-slate-500",
+  backgroundSelected: "bg-slate-500",
   text: "text-slate-500 fill-slate-500",
   textActive: "text-slate-100 fill-slate-100",
   textSelected: "text-slate-100 fill-slate-100",
