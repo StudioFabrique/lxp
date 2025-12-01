940ebc6bd361b83222f83f9dd657b6ae42065d0e
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2025-12-01 11:25:13 +0100

    amelioration sidebar admin

--- front/src/components/UI/avatar/avatar.component.tsx
+++ front/src/components/UI/avatar/avatar.component.tsx
@@ -3,21 +3,24 @@ import User from "../../../utils/interfaces/user";
 
 export const AvatarSmall: FC<{
   user: Partial<User> & { firstname: string; lastname: string };
-}> = ({ user }) => {
+  className?: string;
+  noImgClassName?: string;
+  imgClassName?: string;
+}> = ({
+  user,
+  noImgClassName = "text-xs flex justify-center items-center p-4 w-6 h-6 rounded-full bg-accent text-base-200",
+  imgClassName = "w-8 h-8 rounded-full object-cover",
+}) => {
   console.log(user);
 
   return (
     <>
       {!user.avatar || user.avatar === undefined || user.avatar === "" ? (
-        <p className="text-xs flex justify-center items-center p-4 w-6 h-6 rounded-full bg-accent text-base-200">
+        <p className={noImgClassName}>
           {(user.firstname[0] + user.lastname[0]).trim().toUpperCase()}
         </p>
       ) : (
-        <img
-          src={user.avatar}
-          alt="avatar"
-          className="w-8 h-8 rounded-full object-cover"
-        />
+        <img src={user.avatar} alt="avatar" className={imgClassName} />
       )}
     </>
   );
