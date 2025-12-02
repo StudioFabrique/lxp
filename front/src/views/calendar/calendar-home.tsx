7607bbe662d4e30d80205215cac24517b5a563e8
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2025-12-02 14:03:17 +0100

    integration nouveau compo calendrier

--- front/src/views/calendar/calendar-home.tsx
+++ front/src/views/calendar/calendar-home.tsx
@@ -1,5 +1,55 @@
-import Timeline from "../../components/student-home/timeline/timeline";
+import NewCalendarTimeline, {
+  CalendarEvent,
+} from "../../components/student-home/timeline/new-calendar-timeline";
 import Header from "../../components/UI/header";
+
+const myEvents: CalendarEvent[] = [
+  {
+    id: 1,
+    title: "HTML & Sémantique Web",
+    subtitle: "Cours",
+    dayIndex: 0,
+    start: "08:30",
+    end: "10:00",
+    type: "primary",
+  },
+  {
+    id: 2,
+    title: "CSS et Design Web Responsive",
+    subtitle: "Cours",
+    dayIndex: 0,
+    start: "10:30",
+    end: "12:30",
+    type: "secondary",
+  },
+  {
+    id: 3,
+    title: "Versionnement avec Git & GitHub",
+    subtitle: "Cours",
+    dayIndex: 0,
+    start: "12:30",
+    end: "13:30",
+    type: "neutral",
+  },
+  {
+    id: 4,
+    title: "JavaScript Moderne (ES6+)",
+    subtitle: "Cours",
+    dayIndex: 1,
+    start: "14:15",
+    end: "16:00",
+    type: "danger",
+  },
+  {
+    id: 5,
+    title: "UI/UX : Conception d'Interfaces Utilisateur",
+    subtitle: "Cours",
+    dayIndex: 2,
+    start: "8:30",
+    end: "12:00",
+    type: "accent",
+  },
+];
 
 const CalendarHome = () => {
   return (
@@ -8,7 +58,13 @@ const CalendarHome = () => {
         title="Calendrier"
         description="Consulter le calendrier des prochains cours."
       />
-      <Timeline title="Mon emploi du temps pour cette semaine" />
+      <NewCalendarTimeline
+        title="Développeur Web"
+        events={myEvents}
+        startHour={8}
+        endHour={18}
+        defaultView="week"
+      />
     </div>
   );
 };
