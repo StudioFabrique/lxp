4e718f5ebbd9e63ede306d2cab5f8f2220abfdba
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2025-12-18 16:58:47 +0100

    amélioration layout

--- front/src/components/UI/sidebar/sidebar-wrapper.tsx
+++ front/src/components/UI/sidebar/sidebar-wrapper.tsx
@@ -50,14 +50,14 @@ const SidebarWrapper = ({
 
   return (
     <nav
-      className={`h-full xl:w-[20rem] max-sm:w-[10rem] flex flex-col justify-between gap-y-4 px-2 py-4 rounded-xl gap-2 ${
+      className={`h-full xl:w-[20rem] flex flex-col justify-between gap-y-4 xl:px-2 xl:pt-2 xl:pb-4 pb-2 rounded-xl gap-2 ${
         theme === "dark" ? "text-white bg-slate-500" : "text-white bg-slate-800"
       }`}
     >
       <div>
         {showLogo && (
           <img
-            className="self-start h-[50px] w-[50px] rounded-full border-slate-700 border-1 object-contain p-1 m-2 mb-3 bg-white"
+            className="self-start xl:h-[50px] xl:w-[50px] h-8 w-8 rounded-full border-slate-700 border-1 object-contain p-1 m-2 mb-3 bg-white"
             src={COMPANY_LOGO}
             style={{ backgroundColor: logoBgColor }}
             alt="Company logo"
