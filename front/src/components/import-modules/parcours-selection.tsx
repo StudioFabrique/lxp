a381f856e5559dbf63145e832183acdc6a69f2a9
Martin Dhollande <martin.dhlle@gmail.com>
2026-01-21 15:18:37 +0100

    gestion erreur des fichiers manquants partiels

--- front/src/components/import-modules/parcours-selection.tsx
+++ front/src/components/import-modules/parcours-selection.tsx
@@ -16,7 +16,20 @@ const ParcoursSelection = ({ onGoBack }: Props) => {
         title="Seconde étape"
         description="Selectionner le parcours auquels les modules seront rattachés"
         alternateBgColor
-      />
+      >
+        <button className="btn btn-success" disabled>
+          Lancer le processus d'importation
+        </button>
+      </Header>
+      <div className="flex flex-col gap-5 items-center">
+        <div className="flex gap-5 items-center">
+          <span>Parcours</span>
+          <button className="btn btn-outline w-fit">Choisir le parcours</button>
+        </div>
+        <button className="btn btn-sm w-fit">
+          Continuer sans rattacher les modules à un parcours
+        </button>
+      </div>
     </div>
   );
 };
