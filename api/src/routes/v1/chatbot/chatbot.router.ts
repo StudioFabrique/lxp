a4a7f7cf4a63a15525a538f5dff1d8f892688186
CyrilPonsan <cyril.ponsan@gmail.com>
2026-01-26 16:59:04 +0100

    wip

--- api/src/routes/v1/chatbot/chatbot.router.ts
+++ api/src/routes/v1/chatbot/chatbot.router.ts
@@ -2,9 +2,30 @@ import Router from "express";
 import { http } from "winston";
 import httpPostPrompt from "../../../controllers/chatbot/http-post-prompt";
 import checkToken from "../../../middleware/check-token";
+import { fastApiAgent } from "../../../server";
 
 const chatbotRouter = Router();
 
 chatbotRouter.post("/prompt", checkToken, httpPostPrompt);
+chatbotRouter.get("/test-mongo", async (req, res) => {
+  console.log("HELLO MONGO!");
+
+  try {
+    const result = await fetch("https://localhost:8443/test-mongo", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // Ajouter l'agent mTLS si configuré pour HTTPS
+      ...(fastApiAgent ? { dispatcher: fastApiAgent } : {}),
+    });
+    const data = await result.json();
+    return res.json(data);
+  } catch (error) {
+    console.log({ error });
+
+    res.status(500).json({ error: "Failed to fetch data from FastAPI" });
+  }
+});
 
 export default chatbotRouter;
