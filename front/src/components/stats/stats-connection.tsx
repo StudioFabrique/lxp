16e7199ae082c107a9174675f1f1145d905b02d6
CyrilPonsan <cyril.ponsan@gmail.com>
2026-02-04 17:02:58 +0100

    stats tokens student ok

--- front/src/components/stats/stats-connection.tsx
+++ front/src/components/stats/stats-connection.tsx
@@ -1,5 +1,6 @@
 import { localeDate } from "../../helpers/locale-date";
 import VerticalBars from "../UI/vertical-bars";
+import { TokenStat } from "./user-connection";
 
 interface StatsConnectionProps {
   connectionTime: Array<{
@@ -29,6 +30,37 @@ export default function StatsConnection({
           },
         ]}
         label="Durée des sessions"
+        type="bar"
+        warning={2}
+        width="100%"
+        height="200px"
+      />
+    </div>
+  );
+}
+
+type Props = {
+  tokenStats: TokenStat[];
+};
+
+export function TokensUsed(props: Props) {
+  const categories = props.tokenStats.map((item) => localeDate(item.date));
+  const series = props.tokenStats.map((item) => item.tokensUsed);
+
+  console.log({ props });
+
+  return (
+    <div className="w-full h-[100%]">
+      <h2 className="text-xs font-bold">Tokens utilisés</h2>
+      <VerticalBars
+        categories={categories}
+        series={[
+          {
+            name: "heures",
+            data: series,
+          },
+        ]}
+        label="Qté de tokens utilisés"
         type="bar"
         warning={2}
         width="100%"
--- front/src/components/stats/user-connection.tsx
+++ front/src/components/stats/user-connection.tsx
@@ -1,9 +1,15 @@
 import { ReactNode } from "react";
 import Wrapper from "../UI/wrapper/wrapper.component";
-import StatsConnection from "./stats-connection";
+import StatsConnection, { TokensUsed } from "./stats-connection";
 import StudentCard from "../teacher/student-data/student-card";
 import Parcours from "../../utils/interfaces/parcours";
 import User from "../../utils/interfaces/user";
+
+export type TokenStat = {
+  _id: string;
+  date: string;
+  tokensUsed: number;
+};
 
 interface UserConnectionProps {
   connectionInfos: Array<{ lastConnection: string; duration: number }>;
@@ -12,6 +18,7 @@ interface UserConnectionProps {
   student: User;
   parcours: Parcours | null;
   totalTokens: number;
+  tokenStats?: TokenStat[];
 }
 
 export default function UserConnection({
@@ -21,6 +28,7 @@ export default function UserConnection({
   student,
   parcours,
   totalTokens,
+  tokenStats,
 }: UserConnectionProps) {
   const style = {
     "--value": parcoursCompletion,
@@ -28,27 +36,12 @@ export default function UserConnection({
     "--thickness": "8px",
   } as React.CSSProperties;
 
+  console.log({ tokenStats });
+
   return (
     <article className="flex-1">
       <div className="flex flex-col gap-y-4">
-        <span className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-          <StatsUser label="Complétion du parcours">
-            <div
-              className="flex items-center radial-progress text-primary md:mt-0 mt-2"
-              style={style}
-              role="progressbar"
-            >
-              <p className="font-bold text-xs">{parcoursCompletion} %</p>
-            </div>
-          </StatsUser>
-          <StatsUser label="Temps de connexion">
-            {totalConnectionTime} heures
-          </StatsUser>
-          <StatsUser label="Token utilisés">
-            {totalTokens} tokens utilisés
-          </StatsUser>
-        </span>
-        <span className="flex gap-4 flex-col lg:flex-row">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
           <Wrapper>
             <StudentCard
               avatar={student?.avatar}
@@ -60,13 +53,37 @@ export default function UserConnection({
               status={student.isActive ? "Actif" : "Inactif"}
             />
           </Wrapper>
-          <div className=" md:flex-1">
-            <Wrapper>
-              {connectionInfos ? (
-                <StatsConnection connectionTime={connectionInfos!} />
-              ) : null}
-            </Wrapper>
+          <div className="col-span-2">
+            <StatsUser label="Complétion du parcours" positionY="top">
+              <span className="flex justify-center items-center">
+                <div
+                  className="radial-progress text-primary md:mt-0 mt-2"
+                  style={style}
+                  role="progressbar"
+                >
+                  <p className="font-bold text-xs">{parcoursCompletion} %</p>
+                </div>
+              </span>
+            </StatsUser>
           </div>
+        </div>
+        <span className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+          <StatsUser label="Temps de connexion">
+            {totalConnectionTime} heures
+          </StatsUser>
+          <StatsUser label="Token utilisés">
+            {totalTokens} tokens utilisés
+          </StatsUser>
+        </span>
+        <span className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          <Wrapper>
+            {connectionInfos ? (
+              <StatsConnection connectionTime={connectionInfos!} />
+            ) : null}
+          </Wrapper>
+          <Wrapper>
+            {tokenStats ? <TokensUsed tokenStats={tokenStats!} /> : null}
+          </Wrapper>
         </span>
       </div>
     </article>
@@ -76,14 +93,31 @@ export default function UserConnection({
 interface StatsUserProps {
   label: string;
   children: ReactNode;
+  positionY?: "top" | "center" | "bottom";
 }
 
-export function StatsUser({ label, children }: StatsUserProps) {
+export function StatsUser({
+  label,
+  children,
+  positionY = "center",
+}: StatsUserProps) {
   return (
     <Wrapper>
-      <div className="h-full md:flex-row flex flex-col md:justify-between items-center gap-x-2 gap-y-2">
+      <div
+        className={`h-full md:flex-row flex flex-col md:justify-between ${
+          positionY === "top"
+            ? "items-start"
+            : positionY === "bottom"
+              ? "items-end"
+              : "items-center"
+        } gap-x-2 gap-y-2`}
+      >
         <h2 className="font-bold text-xl">{label}</h2>
-        <div className="h-full flex items-center">{children}</div>
+        <div
+          className={`h-full flex ${positionY === "top" ? "items-start" : positionY === "bottom" ? "items-end" : "items-center"}`}
+        >
+          {children}
+        </div>
       </div>
     </Wrapper>
   );
