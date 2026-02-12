06dac64bbd79d1ba2dcb735a07c4c6ce68c6af87
CyrilPonsan <cyril.ponsan@gmail.com>
2026-02-12 09:46:59 +0100

    complétion des modules ok, style mis à jour

--- front/src/components/stats/stats-progression.tsx
+++ front/src/components/stats/stats-progression.tsx
@@ -6,12 +6,6 @@ type Props = {
 };
 
 export default function StatsProgression(props: Props) {
-  const style = {
-    "--value": props.parcoursCompletion,
-    "--size": "2.5rem",
-    "--thickness": "4px",
-    "--font-size": "8px",
-  } as React.CSSProperties;
   return (
     <>
       {props.completionModules.map((module) => (
@@ -19,27 +13,36 @@ export default function StatsProgression(props: Props) {
           key={module.id}
           className="flex flex-col md:flex-row md:justify-between gap-y-42 overflow-auto pr-2"
         >
-          <div className="flex gap-x-4 justify-between items-center w-full mb-2">
-            <span className="flex gap-x-4 items-center">
+          <div className="flex gap-x-4 items-center w-full mb-2">
+            <span className="w-full flex gap-x-4 items-center">
               <img
                 src={`data:image/jpeg;base64,${module.thumb}`}
                 alt={`thumb de ${module.title}`}
                 className="w-8 h-8 rounded-lg object-cover"
               />
-              <span className="capitalize font-bold">{module.title}</span>
+              <span className="flex capitalize font-bold flex-1">
+                {module.title}
+              </span>
             </span>
-            <span className="flex justify-end items-start">
-              <div
-                className={"radial-progress text-primary md:mt-0 mt-2"}
-                style={style}
-                role="progressbar"
-              >
-                <p style={{ fontSize: "10px" }}>{module.stats.progress} %</p>
-              </div>
-            </span>
+            <StatsProgressionChart value={module.stats.progress} />
           </div>
         </div>
       ))}
     </>
+  );
+}
+
+export function StatsProgressionChart(props: { value: number }) {
+  return (
+    <span className="flex gap-x-2 items-center w-full">
+      <progress
+        className="progress progress-primary w-96"
+        value={props.value - Math.random() * 100}
+        max="100"
+      ></progress>
+      <p className="text-primary font-semibold">
+        {Math.floor(props.value - Math.random() * 100)}%
+      </p>
+    </span>
   );
 }
