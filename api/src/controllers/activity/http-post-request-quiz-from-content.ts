773d7ea060ac8c94bb2a252c8a272ac0b17adf85
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2026-02-25 16:13:38 +0100

    ajout quiz router + correction de l'animation des confettis

---
+++ api/src/controllers/activity/http-post-request-quiz-from-content.ts
@@ -0,0 +1,10 @@
+/**
+ * Récupérer dans le body
+ * - content
+ *
+ * "content" sera une valeur de type texte sans aucune balise.
+ *
+ * Appeler l'api du docker IA et envoyer le content tout en appelant les constantes (treshold etc..) définis dans le code : POST /quiz/random
+ *
+ */
+export default function httpPostRequestQuizFromContent() {}
