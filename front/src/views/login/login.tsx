4761b8d015fd2adfe8727b7540b9828fb7d8283f
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2026-05-04 11:19:56 +0200

    refactor(front):reorganisation du code de connexion

---
+++ front/src/views/login/login.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "../../components/login/login-form";
+import LoginLayout from "./login-layout";
+
+const Login = () => {
+  return (
+    <LoginLayout>
+      <LoginForm />
+    </LoginLayout>
+  );
+};
+
+export default Login;
