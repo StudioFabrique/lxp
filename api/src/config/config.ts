08c08c09f489b041cb922ccbe6949b55e2ffb894
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2026-05-26 08:47:54 +0200

    chore(config):amélioration de la configuration concernant le démarrage du serveur backend

--- api/src/config/config.ts
+++ api/src/config/config.ts
@@ -1,3 +1,7 @@
+export const PORT = process.env.PORT || 5001;
+export const HTTPS_ENABLED = process.env.HTTPS_ENABLED === "true";
+export const MTLS_TO_FASTAPI = process.env.MTLS_TO_FASTAPI === "true";
+
 export const tokensMaxAge = {
   accessToken: 20 * 60 * 1000,
   refreshToken: 2 * 60 * 60 * 1000,
@@ -5,3 +9,19 @@ export const tokensMaxAge = {
 
 export const accessExpire = "20min";
 export const refreshExpire = "2h";
+
+export const corsOrigins =
+  process.env.ENVIRONMENT === "production"
+    ? []
+    : [
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "http://localhost:5175",
+        ...(HTTPS_ENABLED
+          ? [
+              "https://localhost:5173",
+              "https://localhost:5174",
+              "https://localhost:5175",
+            ]
+          : []),
+      ];
