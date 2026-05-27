84a6a5c9a0db7d0614afb9073714ae4a6ad8bd16
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2026-05-27 12:10:50 +0200

    feat(chatbot-ui): remplacement d'une image d'avatar

--- front/src/components/chatbot/chatbot-parts/message-loader-chatbot.tsx
+++ front/src/components/chatbot/chatbot-parts/message-loader-chatbot.tsx
@@ -1,16 +1,10 @@
-import chatbot from "../../../assets/images/chatbot.png";
+import { BotMessageSquare } from "lucide-react";
 
 export default function MessageLoaderChatbot() {
   return (
     <div className="chat chat-start">
       <div className="chat-image avatar">
-        <div className="w-10 h-10 bg-white rounded-full p-1 shadow-sm">
-          <img
-            alt="Bot loading"
-            src={chatbot}
-            className="w-full h-full object-contain"
-          />
-        </div>
+        <BotMessageSquare />
       </div>
       <div className="chat-bubble chat-bubble-base-200 bg-base-100 text-base-content border border-base-300 shadow-sm flex items-center h-10">
         <span className="loading loading-dots loading-sm text-primary"></span>
