adaad1c59ac60a95cc4ddee3b52e6f00e0666007
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2026-05-29 13:23:57 +0200

    feat(suivi-tokens): mise en place du vraie suivi de token pour les appels api

--- api/src/controllers/chatbot/http-post-prompt.ts
+++ api/src/controllers/chatbot/http-post-prompt.ts
@@ -2,6 +2,8 @@ import { Response } from "express";
 import CustomRequest from "../../utils/interfaces/express/custom-request";
 import { fastApiAgent } from "../../server";
 import { fetch } from "undici";
+import postDialogs from "../../models/chatbot/post-dialogs";
+import { trackTokens } from "../../models/stats/trackTokens";
 
 export default async function httpPostPrompt(
   req: CustomRequest,
@@ -28,6 +30,7 @@ export default async function httpPostPrompt(
         user_id: userId,
         question: req.body.prompt,
         course_slug: courseSlug,
+        threshold: 0.68,
         student_profile: {
           user_id: userId,
           course_id: courseSlug,
@@ -40,12 +43,10 @@ export default async function httpPostPrompt(
       }),
     };
 
-    // Ajouter l'agent mTLS si configuré pour HTTPS
     if (fastApiAgent && dockerIa.startsWith("https://")) {
       fetchOptions.dispatcher = fastApiAgent;
     }
 
-    // Appel direct à /ask
     const response = await fetch(`${dockerIa}/ask`, fetchOptions);
 
     if (!response.ok) {
@@ -54,19 +55,34 @@ export default async function httpPostPrompt(
         .json({ error: "Erreur provenant de FastAPI" });
     }
 
-    // Récupération de la réponse JSON complète
     const data = (await response.json()) as any;
 
-    if (data.answer?.mode === "model_knowledge") {
-      console.warn(
-        `[RAG] Mode fallback activé. Score max: ${data.meta?.retrieval?.best_score}`,
-      );
-    }
+    console.warn(
+      `[RAG] Mode (${data.answer?.mode}). Score max: ${data.meta?.retrieval?.best_score}`,
+    );
 
     const markdownContent =
       data.answer?.text || "Désolé, aucune réponse n'a pu être générée.";
 
-    // Envoi propre du JSON attendu par ton hook Front-End 'useChatbot'
+    // Evite de polluer la BDD si l'étudiant est anonyme
+    if (userId && userId !== "anonymous_student") {
+      const lastDialogs = [
+        { origin: "user" as const, message: req.body.prompt, date: new Date() },
+        { origin: "bot" as const, message: markdownContent, date: new Date() },
+      ];
+
+      // Récupérer les tokens si FastAPI les inclut dans sa réponse JSON
+      const aiTokens = (data.meta.usage.total_tokens as number) || 0;
+
+      console.log({ aiTokens });
+
+      if (aiTokens) {
+        await trackTokens(userId, aiTokens);
+      }
+
+      await postDialogs(userId, lastDialogs);
+    }
+
     res.setHeader("Content-Type", "application/json; charset=utf-8");
     return res.status(200).json({ text: markdownContent });
   } catch (error) {
