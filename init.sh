a1271ea96eea0cd94a8edbdc1c89703fe3b68206
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2026-06-15 11:48:55 +0200

    docs(init.sh):amelioration de la documentation

--- init.sh
+++ init.sh
@@ -16,10 +16,10 @@ echo "Exécution des migrations..."
 npx prisma migrate deploy || { echo -e "\033[1;31m Échec: Migrations"; exit 1; }
 
 echo "Restauration des données..."
-docker cp ./dumps/dump.sql lxp-prisma:. || { echo -e "\033[1;31m Échec: Copie du dump SQL"; exit 1; }
+docker cp ./dumps/dump-pgsq.sql lxp-prisma:. || { echo -e "\033[1;31m Échec: Copie du dump SQL"; exit 1; }
 docker cp ./dumps/dump-mongo lxp-mongo:. || { echo -e "\033[1;31m Échec: Copie du dump Mongo"; exit 1; }
-docker exec -it lxp-prisma PGPASSWORD=$POSTGRES_PASSWORD psql -U postgres -d lxp < dump.sql || { echo -e "\033[1;31m Échec: Import SQL"; exit 1; }
-docker exec -it lxp-mongodb mongorestore --db lxp dump-mongo || { echo -e "\033[1;31m Échec: Import MongoDB"; exit 1; }
+docker exec -it lxp-prisma PGPASSWORD=$POSTGRES_PASSWORD psql -U postgres -d lxp < /dump-pgsq.sql || { echo -e "\033[1;31m Échec: Import SQL"; exit 1; }
+docker exec -it lxp-mongodb mongorestore --db lxp /dump-mongo || { echo -e "\033[1;31m Échec: Import MongoDB"; exit 1; }
 
 echo -e "\033[0;32m Configuration du projet ANDRIA terminée avec succès."
 echo "Prochaine étape => Lancer la commande \`npm run dev\` à la racine du projet"
