ea705c7c1b0ffeeefcf30a3029ffe2c4d527fcc9
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2026-06-16 10:37:53 +0200

    docs: amélioration du script de dump

--- init-scripts/dump-data.sh
+++ init-scripts/dump-data.sh
@@ -12,7 +12,7 @@ else
 fi
 
 # Création du dossier de destination s'il n'existe pas
-mkdir -p ./dumps
+mkdir -p ./api/dumps
 
 # SAUVEGARDE POSTGRESQL
 echo "Sauvegarde de PostgreSQL en cours..."
@@ -23,17 +23,17 @@ echo -e "\033[0;32m Dump PostgreSQL réussi.\033[0m"
 echo "Sauvegarde de MongoDB en cours..."
 
 # Nettoyage de l'ancien dump local s'il existe pour éviter les conflits
-rm -rf ./dumps/dump-mongo
+rm -rf ./api/dumps/dump-mongo
 
 # Création du dump à l'intérieur du conteneur
 docker exec -i lxp-mongo mongodump --db "${POSTGRES_DB:-lxp}" --out /dump-mongo || { echo -e "\033[1;31m ❌ Échec: Génération du dump MongoDB"; exit 1; }
 
 # Copie du dump vers le repo
-docker cp lxp-mongo:/dump-mongo ./dumps/ || { echo -e "\033[1;31m ❌ Échec: Copie du dump MongoDB"; exit 1; }
+docker cp lxp-mongo:/dump-mongo ./api/dumps/ || { echo -e "\033[1;31m ❌ Échec: Copie du dump MongoDB"; exit 1; }
 
 # Nettoyage du dossier temporaire dans le conteneur pour libérer de l'espace
 docker exec -i lxp-mongo rm -rf /dump-mongo
 
 echo -e "\033[0;32m Dump MongoDB réussi.\033[0m"
 
-echo -e "\033[1;32m Les données ont été sauvegardées avec succès dans le dossier ./dumps/ \033[0m"
+echo -e "\033[1;32m Les données ont été sauvegardées avec succès dans le dossier ./api/dumps/ \033[0m"
