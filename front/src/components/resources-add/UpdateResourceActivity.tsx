a1cf0ab3e1fed2d3ede8002023fc151ef48c7786
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2026-07-01 16:13:56 +0200

    chore: nettoyage des fichiers inutilisés

--- front/src/components/resources-add/UpdateResourceActivity.tsx
+++
@@ -1,9 +0,0 @@
-import { Activity } from "../../utils/interfaces/activity";
-
-type Props = {
-  activity: Activity;
-};
-
-export default function UpdateResourceActivity(props: Props) {
-  return <div>Update Resource Activity - {props.activity.type}</div>;
-}
