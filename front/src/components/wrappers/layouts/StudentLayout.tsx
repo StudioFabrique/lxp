6eef876fb530c0c7d1f072f9a266736ea3b2b8e6
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2026-07-08 15:03:28 +0200

    refactor: optimisation du code du routeur

---
+++ front/src/components/wrappers/layouts/StudentLayout.tsx
@@ -0,0 +1,22 @@
+import { isAiDisabled } from "../../../config/ai/ai";
+import { ChatbotProvider } from "../../../store/ChatbotProvider";
+import { ROLES_RANKS } from "../../../utils/helpers/roles-rank";
+import Chatbot from "../../chatbot/chatbot";
+import RouteGuard from "../../guards/RouteGuard";
+import Loader from "../../loaders/Loader";
+import Sidebar from "../../sidebar/Sidebar";
+import AppWrapper from "../AppWrapper";
+import ConfettiWrapper from "../ConfettiWrapper";
+
+const StudentLayout = () => (
+  <ChatbotProvider>
+    <ConfettiWrapper>
+      <AppWrapper sidebar={<Sidebar />} loader={<Loader />}>
+        <RouteGuard allowedRanks={[ROLES_RANKS.STUDENT]} />
+      </AppWrapper>
+    </ConfettiWrapper>
+    {!isAiDisabled && <Chatbot />}
+  </ChatbotProvider>
+);
+
+export default StudentLayout;
