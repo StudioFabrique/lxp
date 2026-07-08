3a1ab13b8af0524c28a724b0fe6c4b52f6db9ac4
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2026-07-08 16:24:41 +0200

    feat: correction fade wrapper

--- front/src/features/module-preview/components/preview/module-explorer-preview.tsx
+++ front/src/features/module-preview/components/preview/module-explorer-preview.tsx
@@ -6,6 +6,7 @@ import ActivityTypeSelection from "./activity-type-selection";
 import LessonReaderAndEditor from "./lesson-reader-and-editor";
 import NoActivityPlaceholder from "./no-activity-placeholder";
 import { ExplorerStore } from "../../views/ModuleContentExplorer";
+import FadeWrapper from "../../../../components/wrappers/FadeWrapper";
 
 const ModuleExplorerPreview = ({
   store,
@@ -60,83 +61,85 @@ const ModuleExplorerPreview = ({
   }
 
   return (
-    <LessonReaderAndEditor
-      isLessonCompleted={computed.isLessonCompleted}
-      canEdit={canEditSelectedLesson}
-      mode={mode}
-      textActivityContent={textActivityContent}
-      textActivityTitle={
-        mode === "write" ? state.newActivityTitle : selectedActivity?.title
-      }
-      textActivityTitleError={mode !== "read" ? state.titleError : undefined}
-      selectedActivity={selectedActivity}
-      activityType={
-        selectedActivity?.type ||
-        (mode === "write" && state.activityType) ||
-        "text"
-      }
-      iframeActivitySrc={
-        mode === "write" ? state.newActivitySrc : selectedActivity?.url
-      }
-      selectedLesson={selectedLesson}
-      showDeleteModal={modalVisibility === "deletionModal"}
-      isLoading={isLoading}
-      onOpenDeleteModal={() =>
-        dispatch({
-          type: "set_modal_visibility",
-          modalVisibility: "deletionModal",
-        })
-      }
-      onCloseDeleteModal={() =>
-        dispatch({ type: "set_modal_visibility", modalVisibility: "none" })
-      }
-      onEditActivity={() => dispatch({ type: "select_mode", mode: "edit" })}
-      onEditTitle={editTitle}
-      onEditContent={editContent}
-      onEditIframeSrc={editIframeSrc}
-      onRateActivity={lessonActions.rateContent}
-      onDeleteActivity={activityActions.deleteActivity}
-      onClose={() =>
-        mode === "write"
-          ? dispatch({ type: "select_last_activity_from_current_lesson" })
-          : dispatch({ type: "select_mode", mode: "read" })
-      }
-      onBack={() =>
-        dispatch({ type: "select_mode", mode: "activity_type_selection" })
-      }
-      onSaveActivity={activityActions.saveActivity}
-    >
-      {mode === "read" && (
-        <ActivityBottomNavigation
-          modalVisibility={modalVisibility}
-          isLessonCompleted={computed.isLessonCompleted}
-          isFirstActivitySelected={computed.isFirstActivitySelected}
-          isLastActivitySelected={computed.isLastActivitySelected}
-          isLastLessonSelected={computed.isLastLessonSelected}
-          onPrevious={() => dispatch({ type: "go_to_previous_activity" })}
-          onNext={smartQuizState.handleNextActivity}
-          onCompleteLesson={() =>
-            computed.isLessonCompleted
-              ? lessonActions.nextLesson()
-              : dispatch({
-                  type: "set_modal_visibility",
-                  modalVisibility: "lessonCompletionModal",
-                })
-          }
-        >
-          {computed.isLastActivitySelected &&
-            computed.isLastLessonOfCurrentCourse && (
-              <button
-                className="btn btn-secondary btn-outline"
-                onClick={quizState.onLoadQuizzes}
-              >
-                <BadgeQuestionMark />
-                Je veux me tester
-              </button>
-            )}
-        </ActivityBottomNavigation>
-      )}
-    </LessonReaderAndEditor>
+    <FadeWrapper>
+      <LessonReaderAndEditor
+        isLessonCompleted={computed.isLessonCompleted}
+        canEdit={canEditSelectedLesson}
+        mode={mode}
+        textActivityContent={textActivityContent}
+        textActivityTitle={
+          mode === "write" ? state.newActivityTitle : selectedActivity?.title
+        }
+        textActivityTitleError={mode !== "read" ? state.titleError : undefined}
+        selectedActivity={selectedActivity}
+        activityType={
+          selectedActivity?.type ||
+          (mode === "write" && state.activityType) ||
+          "text"
+        }
+        iframeActivitySrc={
+          mode === "write" ? state.newActivitySrc : selectedActivity?.url
+        }
+        selectedLesson={selectedLesson}
+        showDeleteModal={modalVisibility === "deletionModal"}
+        isLoading={isLoading}
+        onOpenDeleteModal={() =>
+          dispatch({
+            type: "set_modal_visibility",
+            modalVisibility: "deletionModal",
+          })
+        }
+        onCloseDeleteModal={() =>
+          dispatch({ type: "set_modal_visibility", modalVisibility: "none" })
+        }
+        onEditActivity={() => dispatch({ type: "select_mode", mode: "edit" })}
+        onEditTitle={editTitle}
+        onEditContent={editContent}
+        onEditIframeSrc={editIframeSrc}
+        onRateActivity={lessonActions.rateContent}
+        onDeleteActivity={activityActions.deleteActivity}
+        onClose={() =>
+          mode === "write"
+            ? dispatch({ type: "select_last_activity_from_current_lesson" })
+            : dispatch({ type: "select_mode", mode: "read" })
+        }
+        onBack={() =>
+          dispatch({ type: "select_mode", mode: "activity_type_selection" })
+        }
+        onSaveActivity={activityActions.saveActivity}
+      >
+        {mode === "read" && (
+          <ActivityBottomNavigation
+            modalVisibility={modalVisibility}
+            isLessonCompleted={computed.isLessonCompleted}
+            isFirstActivitySelected={computed.isFirstActivitySelected}
+            isLastActivitySelected={computed.isLastActivitySelected}
+            isLastLessonSelected={computed.isLastLessonSelected}
+            onPrevious={() => dispatch({ type: "go_to_previous_activity" })}
+            onNext={smartQuizState.handleNextActivity}
+            onCompleteLesson={() =>
+              computed.isLessonCompleted
+                ? lessonActions.nextLesson()
+                : dispatch({
+                    type: "set_modal_visibility",
+                    modalVisibility: "lessonCompletionModal",
+                  })
+            }
+          >
+            {computed.isLastActivitySelected &&
+              computed.isLastLessonOfCurrentCourse && (
+                <button
+                  className="btn btn-secondary btn-outline"
+                  onClick={quizState.onLoadQuizzes}
+                >
+                  <BadgeQuestionMark />
+                  Je veux me tester
+                </button>
+              )}
+          </ActivityBottomNavigation>
+        )}
+      </LessonReaderAndEditor>
+    </FadeWrapper>
   );
 };
 
