ecca18434e1b3c96b513229afe6cffca92d7fa7c
Martin Dhollande <martin.dhlle@gmail.com>
2026-07-15 14:21:34 +0200

    feat: improve general networks requests to api and compression added

--- front/src/features/auth/routes.tsx
+++ front/src/features/auth/routes.tsx
@@ -1,9 +1,8 @@
 import { lazy } from "react";
 import { RouteObject } from "react-router";
-import AuthLayout from "./components/AuthLayout";
 import { withSuspense } from "../../utils/helpers/router-helpers";
-import LoginGuard from "../../components/guards/LoginGuard";
 
+const AuthLayout = lazy(() => import("./components/AuthLayout"));
 const Init = lazy(() => import("./views/AppInit"));
 const Login = lazy(() => import("./views/Login"));
 const RegisterHome = lazy(() => import("./views/Register"));
@@ -12,13 +11,9 @@ const ResetPasswordUpdate = lazy(() => import("./views/ResetPasswordUpdate"));
 
 export const authRoutes: RouteObject[] = [
   {
-    element: (
-      <AuthLayout>
-        <LoginGuard />
-      </AuthLayout>
-    ),
+    element: withSuspense(AuthLayout),
     children: [
-      {path: "/init", element: withSuspense(Init)},
+      { path: "/init", element: withSuspense(Init) },
       { path: "/login", element: withSuspense(Login) },
       { path: "/register", element: withSuspense(RegisterHome) },
       { path: "/reset-password", element: withSuspense(ResetPasswordHome) },
