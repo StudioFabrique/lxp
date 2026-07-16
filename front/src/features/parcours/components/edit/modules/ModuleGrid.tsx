d386d203559c07686249d487af1a843fcd91c943
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2026-07-16 16:35:22 +0200

    feat: correction ajustement module cards

--- front/src/features/parcours/components/edit/modules/ModuleGrid.tsx
+++ front/src/features/parcours/components/edit/modules/ModuleGrid.tsx
@@ -18,7 +18,7 @@ export default function ModuleGrid({
   }
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 items-stretch w-full">
+    <div className="flex flex-wrap gap-6 items-stretch w-full">
       {modules.map((module) => (
         <ModuleCard
           module={module}
