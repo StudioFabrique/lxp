4f74b18b8f8e9377f4ec8a8fb793e1cf127c78d7
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2026-07-20 10:33:26 +0200

    refactor: delete duplicated code

--- front/src/utils/helpers/date-helpers.ts
+++ front/src/utils/helpers/date-helpers.ts
@@ -1,4 +1,4 @@
-export const monthsList = [
+const monthsList = [
   "janvier",
   "février",
   "mars",
@@ -11,25 +11,6 @@ export const monthsList = [
   "octobre",
   "novembre",
   "décembre",
-];
-
-export function getMaxYear() {
-  return new Date().getFullYear();
-}
-
-export const monthItems = [
-  { month: "janvier", maxDays: 31 },
-  { month: "février", maxDays: 29 },
-  { month: "mars", maxDays: 31 },
-  { month: "avril", maxDays: 30 },
-  { month: "mai", maxDays: 31 },
-  { month: "juin", maxDays: 30 },
-  { month: "juillet", maxDays: 31 },
-  { month: "août", maxDays: 31 },
-  { month: "septembre", maxDays: 30 },
-  { month: "octobre", maxDays: 31 },
-  { month: "novembre", maxDays: 30 },
-  { month: "décembre", maxDays: 31 },
 ];
 
 export function getMonth(monthNumber: number) {
