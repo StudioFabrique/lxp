82feeec6a00a26ab96f3409f4014524c271de62b
Martin Dhollande <martin.dhlle@gmail.com>
2026-07-21 22:26:29 +0200

    feat: debut integration de la conformité au modèle du docker IA

---
+++ api/src/helpers/slugify.ts
@@ -0,0 +1,6 @@
+export function slugify(input: string): string {
+  return (input || "")
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, "-")
