8c6ae89d73133e7410e047d9148464053cadd723
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2026-08-24 15:02:25 +0200

    fix: date locale fr

--- front/src/utils/helpers/locale-date.ts
+++ front/src/utils/helpers/locale-date.ts
@@ -1,5 +1,5 @@
 export function localeDate(date: string | Date) {
-  return new Date(date).toLocaleDateString();
+  return new Date(date).toLocaleDateString("fr");
 }
 
 export function localeTime(date: string | Date) {
