c0ab1dcdae2306946b2d544c67554cd29f9a8f23
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2026-09-16 11:18:31 +0200

    feat: override regle css pour le remplissage auto du navigateur dans login

--- front/src/features/auth/views/Login.tsx
+++ front/src/features/auth/views/Login.tsx
@@ -53,11 +53,12 @@ const Login = () => {
           <input
             name="email"
             type="email"
+            autoComplete="email"
             value={email.value}
             onChange={email.valueChangeHandler}
             onBlur={email.valueBlurHandler}
             placeholder="Adresse mail"
-            className="input input-lg text-sm px-5 w-full bg-base-200 text-base-content placeholder-base-content/50 border-none focus:outline-none focus:ring-2 focus:ring-primary rounded-lg"
+            className="auth-login-input input input-lg text-sm px-5 w-full bg-base-200 text-base-content placeholder-base-content/50 border-none focus:outline-none focus:ring-2 focus:ring-primary rounded-lg"
           />
         </div>
 
@@ -66,11 +67,12 @@ const Login = () => {
           <input
             name="password"
             type={inputType}
+            autoComplete="current-password"
             value={password.value}
             onChange={password.valueChangeHandler}
             onBlur={password.valueBlurHandler}
             placeholder="Mot de passe"
-            className="input input-lg text-sm px-5 w-full bg-base-200 text-base-content placeholder-base-content/50 border-none focus:outline-none focus:ring-2 focus:ring-primary rounded-lg pr-12"
+            className="auth-login-input input input-lg text-sm px-5 w-full bg-base-200 text-base-content placeholder-base-content/50 border-none focus:outline-none focus:ring-2 focus:ring-primary rounded-lg pr-12"
           />
           <div className="absolute right-1 top-1/2 -translate-y-1/2 cursor-pointer flex items-center">
             <PasswordVisibilityToggle
